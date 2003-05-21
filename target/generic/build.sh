0bba66219fc45c740013d19cc1a65efcb7f6c5aa
René Rebe <rene@t2-project.org>
2003-05-21 21:50:33 +0000

    checks after Built-Target: - for missing binary packagse - for invalid binary packages (like duplicate version)
    git-svn-id: https://svn.exactcode.de/t2/rock-trunk@344 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/generic/build.sh
+++ target/generic/build.sh
@@ -7,6 +7,30 @@ pkgloop
 
 echo_header "Finishing build."
 
+grep "^X" config/$config/packages | cut -d ' ' -f 5-6 | \
+	sed "s/ /-/" > /tmp/$$-gem-files
+
+echo_status "Searching for missing binary packages ..."
+for x in `cat /tmp/$$-gem-files` ; do
+   if [ ! -e build/$ROCKCFG_ID/pkgs/$x.* ] ; then
+	echo "$x binary package is missing!"
+   fi
+done
+
+if [ ! $ROCKCFG_PKGFILE_VER = 1 ] ; then
+   echo_status "Checking for binary package files with old version ..."
+
+   rm -f invalid-files.lst 2> /dev/null
+
+   for file in `cd build/$ROCKCFG_ID/pkgs/ ; ls * ` ; do
+	x=${file/.gem/} ; x=${x/.tar.bz2//}
+	if ! grep -q $x /tmp/$$-gem-files ; then
+		echo "$file should not be present (now in invalid-files.lst)!"
+		echo $file >> invalid-files.lst
+	fi
+   done
+fi
+
 echo_status "Creating package database ..."
 admdir="build/${ROCKCFG_ID}/root/var/adm"
 create_package_db $admdir > build/${ROCKCFG_ID}/packages.db
