9760b9ab0eb89830f54cfb124e1fb4ce4dd54137
René Rebe <rene@t2-project.org>
2003-05-25 19:54:31 +0000

    Alejandro Mery Pellegrini <amery@cbs.cl> big/fat cleanup to mnemoc's
    git-svn-id: https://svn.exactcode.de/t2/rock-trunk@390 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ package/mnemoc/lvs/kernel.conf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo CONFIG_NETFILTER=y >> $1
+echo CONFIG_IP_VS=y >> $1
--- package/mnemoc/vquota/kernel.conf.sh
+++ package/mnemoc/vquota/kernel.conf.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-CONFIG=$1
-
-echo "Appending VQuota Configuration to $CONFIG ... "
-[ -z "$CONFIG" ] && exit 1
-
-echo -e "\n# VQuota" >> $CONFIG
-
-for x in QUOTA BLK_DEV_VROOT
-do
-	echo "CONFIG_$x=y" >> $CONFIG
-done
+echo "CONFIG_QUOTA=y" >> $1
+echo "CONFIG_BLK_DEV_VROOT=y" >> $1
