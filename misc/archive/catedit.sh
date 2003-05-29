a1fb377053e63a9d1d92d521304da6e87e252785
René Rebe <rene@t2-project.org>
2003-05-29 09:50:30 +0000

    merged Niklaus next attempt to destroy catedit even further much cleanup and debugging to get it working (to some basic degree) again ... :-(((((((((((((((
    git-svn-id: https://svn.exactcode.de/t2/rock-trunk@398 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/archive/catedit.sh
+++ misc/archive/catedit.sh
@@ -4,37 +4,59 @@
 # for editing the categories of your packages !!!
 #
 # Usage: sh misc/archive/catedit.sh package/base/*/*.desc
+#    or: sh misc/archive/catedit.sh -a
 #
 
-item='' title="Choose the package you want to edit"
+item='' 
 tmp=$( mktemp )
 
+if [ $(dialog --version 2>&1| grep -c "0.9") -eq 0 ]; then
+    echo "dialog's version at least 0.9 needed"
+    exit 1
+fi
+
+if [ "$1" = "-a" -o "$1" = "--all" ]
+then files="$(find package/ -name *.desc | sort -t '/' -k 4)"
+else files="$@"
+fi
+
 until
-	cmd="dialog ${item:+--default-item} $item --menu '$title' 20 74 13  \
-		$( grep '^\[C\]' "$@" | \
-		   sed 's,^[^:]*/,,; s,\.[^ ]* , ",; s,$,",;' | tr '\n' ' '
-		)"
-	eval "$cmd 2> $tmp"
+	grep '^\[C\]' $files | sed -e 's,^[^:]*/,,;' -e 's,\.[^ ]* , ",;' \
+	     -e 's,$,",;' | sed -e 's, "$,",' | tr '\n' ' ' > $tmp
+
+
+	cat $tmp
+
+	dialog --backtitle "ROCK Linux package category editor" \
+	       ${item:+--default-item} $item --cancel-label \
+	       Quit --menu "Choose the package you want to edit" \
+	       42 120 35 $( cat $tmp ) 2> $tmp
 	item="$( cat $tmp )"
+	cat $tmp
 	[ -z "$item" ]
 do
-	for file; do
-		[ "${file##*/}" = "$item.desc" ] && break
+	for file in $files; do
+		[[ $file = */$item.desc ]] && break
 	done
+	
+	(for category in $(awk '/^[^# ]/ {print $1}' < Documentation/Developers/PKG-CATEGORIES); do
+		echo -n $category $category
+		if [ -n "$(grep "^\[C\].*$category" $file )" ]
+		then echo -e " on "
+		else echo -e " off "
+		fi
+	done ) > $tmp
 
-	dialog --inputbox "Modify categories for $item" 8 70 \
-		"$( grep '^\[C\]' "$file" | \
-		    sed 's,[^ ]* ,,' | tr '\n' ' ' | sed 's, *$,,'
-		)" 2> $tmp
-	value="$( cat $tmp )"
+	dialog --cancel-label Back --backtitle " categories for $item " \
+	       --checklist "$(grep "^\[I\]" $file)" 42 80 35 \
+	       $(cat $tmp) 2> $tmp
+	value=$( cat $tmp | tr -d '"' )
 
-	if [ "$value" ]; then 
-		grep -v '^\[C\]' "$file" >  $tmp
-		echo -e '\n'"[C] $value" >> $tmp
+	if [ "$value" ] ; then 
+		cat $file | sed -e "s,\[C\]\(.*\),\[C\] $value,g" > $tmp
 		cat $tmp > $file
 		./scripts/Create-DescPatch $item | patch -p1
 	fi
 done
 
 rm -f $tmp
-
