7a9fc8da2b1b1061287371f4dd7819a3bcb3082e
René Rebe <rene@t2-project.org>
2003-06-07 08:58:30 +0000

    this patch introduce gcc33 as the stable release. in much cases it's considered as equal to gcc3. support for gccx is also added.
    (tested default=gcc33 until stage 5)
    
    hope1: hope it wont break gcc[23].
    hope2: hope you like it.
    
    
    git-svn-id: https://svn.exactcode.de/t2/rock-trunk@459 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/gcc3/apply-protector.sh
+++ package/base/gcc3/apply-protector.sh
@@ -1,20 +1,20 @@
 
-# exclude some version - since we do not have a stack-protector for them
-if [ $2 = gccx ] ; then
-	echo "No stack-protector available for $2 ..."
-else
-	read x pfile x < <( echo "$3" | grep protector; )
-
+# only try to apply protector if present
+read x pfile x < <( echo "$3" | grep protector )
+if [ "$pfile" ] ; then
 	tar --use-compress-program=bzip2 \
 	  -xf $1/download/base/$2/${pfile/.gz/.bz2}
 
 	# Patch protector.dif a bit to apply against current gcc-3
 	#[ $2 = gcc3 ] && patch -p1 < $1/package/base/$2/protector-hotfix.diff
 
+	# be careful if you enable this, you have to respect $pkg.
 	# Set -fstack-protector as default?
 	# [ $ROCKCFG_PKG_GCC[23]_STACKPRO = 1 ] && patch -p0 < protectonly.dif
 
 	patch -p0 < protector.dif
 	mv protector.{c,h} gcc/
+else
+	echo "No stack-protector available for $2 ..."
 fi
 
