915cfe24e6782fb5828a519b81bc626890b98fac
René Rebe <rene@t2-project.org>
2003-06-19 15:10:09 +0000

    added the last missing devpts bit and changelog
    git-svn-id: https://svn.exactcode.de/t2/rock-trunk@568 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/stone_mod_setup.sh
+++ package/base/sysfiles/stone_mod_setup.sh
@@ -29,6 +29,7 @@ make_fstab() {
 /dev/root / auto defaults 0 0
 none /proc proc defaults 0 0
 none /dev devfs defaults 0 0
+none /dev/pts devpts defaults 0 0
 none /dev/shm ramfs defaults 0 0
 #none /tmp tmpfs defaults 0 0
 EOT
