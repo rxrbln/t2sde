d010130e5a52ba0ecbe632ea3a586073560f4864
Stefan Paletta <stefanp@cabal1.com>
2003-08-27 16:02:57 +0000

    I was bored and tired ... - a hello-world in C# ...
    git-svn-id: https://svn.exactcode.de/t2/trunk@1214 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ misc/archive/hello.cs
@@ -0,0 +1,12 @@
+// Hello World in C#
+// Compile into an assembly with  'mcs hello.cs'.
+// Run with the Mono Interpreter  'mint hello.exe' or
+//     with the Mono JIT Compiler 'mono hello.exe'.
+
+class Hello 
+{
+   static void Main() 
+   {
+      System.Console.WriteLine("Hello World! (A simple C# program.)");
+   }
+}
