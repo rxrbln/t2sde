e4d9f9c884b21f4fed3696c4641549541bb3157b
René Rebe <rene@t2-project.org>
2003-09-03 12:52:38 +0000

    ^M fix
    git-svn-id: https://svn.exactcode.de/t2/trunk@1301 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/archive/fmnewpackage.sh
+++ misc/archive/fmnewpackage.sh
@@ -37,7 +37,7 @@
 #
 
 extract_xml_name() {
-    local tmp="`tr -d "\012" < $2 | grep $3 | sed "s,.*<$3>\([^<]*\)<.*,\1," | sed 's,,\n[T] ,g' | sed 's,^\[T\] $,,'`"
+    local tmp="`tr -d "\012" < $2 | grep $3 | sed "s,.*<$3>\([^<]*\)<.*,\1," | sed 's,,\n[T] ,g' | sed 's,^\[T\] $,,'`"
     eval "$1=\"\$tmp\""
 }
 
