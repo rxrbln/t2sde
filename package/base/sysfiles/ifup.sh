12e6d2c7fe5e23f056418f8bf83d5a4b5ee0a0ee
René Rebe <rene@t2-project.org>
2003-11-21 15:09:40 +0000

    improved ifup.sh and added the code to install it
    git-svn-id: https://svn.exactcode.de/t2/trunk@1832 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/ifup.sh
+++ package/base/sysfiles/ifup.sh
@@ -7,12 +7,14 @@ useage() {
 
 [ "$1" ] || useage
 
-if [ "$2"  = "" ] ; then
-	profile="`cat /etc/network/profile`"
+if [ "$2" = "" ] ; then
+	profile="`cat /etc/network/profile 2> /dev/null`"
 else
 	profile="$2"
 fi
 
-/etc/network/rocknet "$1" "$2" "${x#if}"
-echo "$profile" > /etc/network/proflie
+profile=${profile:-default}
+
+/etc/network/rocknet "$profile" "$1" "${0#/sbin/if}"
+echo "$profile" > /etc/network/profile
 
