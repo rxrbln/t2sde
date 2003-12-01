c3e2bd9b7744e1ff6ded3a2209a418014d724e70
René Rebe <rene@t2-project.org>
2003-12-01 20:19:36 +0000

    powerpc boot cleanups and adaptions
    git-svn-id: http://svn.exactcode.de/t2/trunk@1893 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/bootdisk/powerpc/build.sh
+++ target/bootdisk/powerpc/build.sh
@@ -11,7 +11,7 @@ then
 	echo_header "Creating yaboot setup:"
 	#
 	echo_status "Extracting yaboot boot loader images."
-	mkdir -p boot
+	mkdir -p boot etc
 	tar --use-compress-program=bzip2 \
 	    -xf $base/build/${ROCKCFG_ID}/pkgs/yaboot.tar.bz2 \
 	    usr/lib/yaboot/yaboot -O > boot/yaboot
@@ -22,6 +22,9 @@ then
 	echo_status "Creating yaboot config file."
 	cp -v $base/target/$target/powerpc/{yaboot.conf,boot.msg,ofboot.b} \
 	  boot
+	echo_status "Creating the IBM RS/6k yaboot config file."
+	cp -v $base/target/$target/powerpc/yaboot.conf etc
+	echo "Duplicates of /boot used on IBM RS/6k hardware." > etc/README
 	#
 	echo_status "Moving image (initrd) to yaboot directory."
 	mv -v initrd.img boot/
@@ -34,7 +37,7 @@ then
 		BOOT	-hfs -part -map $datdir/mapping -hfs-volid "ROCK_Linux_CD"
 		BOOTx	-hfs-bless boot -sysid PPC
 		DISK1	$datdir/boot/ boot/
-		SCRIPT	./target/bootdisk/powerpc/bless-rs6k.sh
+		SCRIPT	sh $base/target/bootdisk/powerpc/bless-rs6k.sh $disksdir
 	EOT
 fi
 
