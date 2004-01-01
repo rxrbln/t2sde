a2de8f68046b842a8978353680069458a37151fe
René Rebe <rene@t2-project.org>
2004-01-01 18:30:40 +0000

    mounting sysfs when available (e.g. 2.5/2.6) (untested ...)
    git-svn-id: https://svn.exactcode.de/t2/trunk@2088 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/stone_mod_setup.sh
+++ package/base/sysfiles/stone_mod_setup.sh
@@ -31,6 +31,7 @@ none /proc proc defaults 0 0
 none /dev devfs defaults 0 0
 none /dev/pts devpts defaults 0 0
 none /dev/shm ramfs defaults 0 0
+none /sys sysfs defaults 0 0
 #none /tmp tmpfs defaults 0 0
 EOT
 
