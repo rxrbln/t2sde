24dd36005b88bfa34f46a76dee0898e13ec023b2
René Rebe <rene@t2-project.org>
2004-01-20 21:48:56 +0000

    Stefan Fiedler <stefan.fiedler@students.jku.at>: fixed desktop sound module disabling for linux26 ALSA modules
    git-svn-id: http://svn.exactcode.de/t2/trunk@2228 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/desktop/kernel.conf.sh
+++ target/desktop/kernel.conf.sh
@@ -1,7 +1,7 @@
 # here we disable all OSS modules - because they suck
 
-echo "desktop target -> disabling sound modules ..."
+echo "desktop target -> disabling oss sound modules ..."
 
-sed "s/CONFIG_SOUND_\(.*\)=./# CONFIG_SOUND_\1 is not set/" $1 > .config.desktop
+sed "s/CONFIG_SOUND_OSS=./# CONFIG_SOUND_OSS is not set/" $1 > .config.desktop
 mv .config.desktop $1
 
