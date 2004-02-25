497b7e8a6b9a67044c0258aa5a2bd2b69faa7000
René Rebe <rene@t2-project.org>
2004-02-25 15:51:32 +0000

    merged the kde32 branch: (http://svn.rocklinux-consulting.de/rock-linux/branches/kde32/ -r1681:HEAD)
    git-svn-id: https://svn.exactcode.de/t2/trunk@2379 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/kde31/update-kde.sh
+++ package/kde31/update-kde.sh
@@ -5,10 +5,9 @@ if [ ! "$1" -o ! "$2" ] ; then
 	exit -1
 fi
 
-for x in arts* kde* quanta* ; do
+for x in kde* quanta* ; do
 	echo "Updateing $x ..."
-	sed 	-e s/$1/$2/g \
-		-e s,unstable/kde-$1,stable/kde-$2, \
+	sed 	-e s,$1,$2,g \
 		-e "s/\[D\] [0-9]* /\[D\] 0 /" $x/$x.desc > $x/$x.desc.new
 	mv $x/$x.desc.new $x/$x.desc
 done
