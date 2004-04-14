82cf9d8a898c20abe7a06a8b6778693fd7e7324f
Clifford Wolf <clifford@clifford.at>
2004-04-14 09:07:39 +0000

    Rene Rebe: 	use mkisofs to implant the partition table on for PReP / RS/6k 	some sparc bits
    [2004040715425731947] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@2732 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ target/bootdisk/sparc/build.sh
@@ -0,0 +1,33 @@
+
+use_silo=1
+
+cd $disksdir
+
+echo_header "Creating cleaning boot directory:"
+rm -rfv boot/*-rock boot/System.map boot/kconfig*
+
+if [ $use_silo -eq 1 ]
+then
+	echo_header "Creating silo setup:"
+	#
+	echo_status "Extracting silo boot loader images."
+	mkdir -p boot
+	tar --use-compress-program=bzip2 \
+	    -xf $base/build/${ROCKCFG_ID}/pkgs/silo.tar.bz2 \
+	    boot/second.b -O > boot/second.b
+	#
+	echo_status "Creating silo config file."
+	cp -v $base/target/$target/sparc/{silo.conf,boot.msg} \
+	  boot
+	#
+	# echo_status "Moving image (initrd) to yaboot directory."
+	# mv -v initrd.img boot/
+	#
+	buildroot="build/${ROCKCFG_ID}/root"
+	cat > ../isofs_arch.txt <<- EOT
+		BOOT	-G $buildroot/boot/isofs.b -B ...
+		DISK1	$datdir/boot/ boot/
+		SCRIPT	sh $base/target/bootdisk/powerpc/bless-rs6k.sh $disksdir
+	EOT
+fi
+
