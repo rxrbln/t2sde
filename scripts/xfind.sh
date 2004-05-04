30807371230dba9f952777b14b5bee4a4baadd62
Clifford Wolf <clifford@clifford.at>
2004-05-04 09:32:22 +0000

    Clifford Wolf: 	Fixed bug in new xfind.sh
    [2004042915423617378] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@2870 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- scripts/xfind.sh
+++ scripts/xfind.sh
@@ -5,7 +5,7 @@
 # in various places. Never use this with -depth! Instead pipe the output thru
 # "tac" or "sort -r".
 
-dirs=""; while [ -n "${1##[-\(]*}" ]; do dirs="$dirs $1"; shift; done
+dirs=""; while [ -n "${1##[-\(\!]*}" ]; do dirs="$dirs $1"; shift; done
 if [ $# -eq 0 ]; then set -- -print; fi; 
 action=") -print"; if [ "${*#-print}" != "$*" ]; then action=""; fi
 find $dirs '(' -path '*/.svn' -o -path '*/CVS' ')' -prune -o ${action:+\(} "$@" $action
