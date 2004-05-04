404d7692c72051bc7ddfeebbee409e83a13ae037
Clifford Wolf <clifford@clifford.at>
2004-05-04 09:29:35 +0000

    Dimitar Zhekov: 	reduced the bootdisk 2.6 kernel by disabling some stuff (from ml)
    [2004042813324904700] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@2862 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/bootdisk/kernel.conf.sh
+++ target/bootdisk/kernel.conf.sh
@@ -1,6 +1,6 @@
-# here we disable driver we'll most probably not need on the bootdisk
+# here we disable settings we'll most probably not need on the bootdisk
 
-echo "bootdisk target -> disabling some modules ..."
+echo "bootdisk target -> disabling some settings ..."
 
 sed -e "s/CONFIG_SOUND\(.*\)=./# CONFIG_SOUND\1 is not set/" \
     -e "s/CONFIG_VIDEO\(.*\)=./# CONFIG_VIDEO\1 is not set/" \
@@ -12,6 +12,13 @@ sed -e "s/CONFIG_SOUND\(.*\)=./# CONFIG_SOUND\1 is not set/" \
     -e "s/CONFIG_SMP\(.*\)=./# CONFIG_SMP\1 is not set/" \
     -e "s/CONFIG_PCI_NAMES\(.*\)=./# CONFIG_PCI_NAMES\1 is not set/" \
     -e "s/CONFIG_INPUT_JOYDEV\(.*\)=./# CONFIG_INPUT_JOYDEV\1 is not set/" \
+    -e "s/CONFIG_ACPI\(.*\)=./# CONFIG_ACPI\1 is not set/" \
+    -e "s/CONFIG_GAMEPORT\(.*\)=./# CONFIG_GAMEPORT\1 is not set/" \
+    -e "s/CONFIG_IEEE1394\(.*\)=./CONFIG_IEEE1394\1=m/" \
+    -e "s/CONFIG_IP_NF\(.*\)=./CONFIG_IP_NF\1=m/" \
+    -e "s/# CONFIG_EMBEDDED is not set/CONFIG_EMBEDDED=y/" \
+    -e "s/CONFIG_KALLSYMS=./# CONFIG_KALLSYMS is not set/" \
+    -e "s/CONFIG_IOSCHED_AS=./# CONFIG_IOSCHED_AS is not set/" \
     $1 > .config.boot
 
 mv .config.boot $1
