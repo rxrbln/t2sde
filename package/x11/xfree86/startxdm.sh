615272edeac3d0205d5109c2ea25d3d3c2346f0a
Claire Xenia Wolf <claire@clairexen.net>
2004-05-16 19:20:28 +0000

    Chris Efant: 	xfree86: this fixes the respawn issue that happened when running 	g/k/xdm out of init.  Unlike the previous patch, this only 	passes -nodaemon to the display managers that support it. Clifford Wolf: 	Some cleanups to the original patch
    [2004051108200513424] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@2986 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/x11/xfree86/startxdm.sh
+++ package/x11/xfree86/startxdm.sh
@@ -4,5 +4,15 @@ XDM=/usr/X11/bin/xdm
 
 [ -e /etc/conf/xdm ] && . /etc/conf/xdm
 
-$XDM
+function check_dm() {
+	echo $XDM | grep -q $1
+}
+
+if [ "$1" = "-nodaemon" ] &&
+		! check_dm gdm && ! check_dm kdm && ! check_dm xdm
+then
+	shift
+fi
+
+$XDM "$@"
 
