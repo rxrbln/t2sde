ab8d6cc3848882c9e21baab3fb1ed1b43a4ba12b
Clifford Wolf <clifford@clifford.at>
2004-05-20 09:10:23 +0000

    Rene Rebe: 	first patchset to make ROCK Net more useable by addind error detections 	(such as diagnostics if the interface/profile combination was not know 	 and so nothing done) 	fixed typos in ifup (useage -> usage ...)
    [2004051216163501246] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://svn.exactcode.de/t2/trunk@3027 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/rocknet_modules_base.sh
+++ package/base/sysfiles/rocknet_modules_base.sh
@@ -30,6 +30,7 @@ public_interface() {
 		for x in $prof; do
 			[ "$x" = "$profile" ] && ignore=0
 		done
+		[ "$ignore" = 0 ] && pmatched=1
 	fi
 
 	if [ "$ignore" = 0 ]; then
@@ -41,6 +42,11 @@ public_interface() {
 		else
 			[ "$if" = "$interface" ] || ignore=1
 		fi
+	fi
+
+	if [ "$ignore" = 0 ] ; then
+		imatched=1
+		status "Interface / profile matched: $if($prof)"
 	fi
 }
 
