9819c7ca88337f07649ed3c4d5618ce736b9a762
Claire Xenia Wolf <claire@clairexen.net>
2004-05-20 09:13:28 +0000

    Alejandro Mery: 	sendmail (and friends): add compatibility symlink to /usr/sbin
    [2004051523004619005] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@3044 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sendmail/setmailer.sh
+++ package/base/sendmail/setmailer.sh
@@ -5,5 +5,9 @@ for x in sendmail mailq newaliases; do
 	echo -e "#!/bin/sh\nexec -a $x ${x}_@mailer@ \"\$@\"" > /usr/bin/$x
 	chmod +x /usr/bin/$x
 done
+
+# add compatibility symlink
+ln -sf /usr/bin/sendmail /usr/sbin/sendmail
+
 exit 0
 
