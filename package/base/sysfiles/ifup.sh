82f8fa6182cd1b6834e3ab57cfceb5c8ae1479ee
Claire Xenia Wolf <claire@clairexen.net>
2004-05-20 09:14:18 +0000

    Rene Rebe: 	fixed the ROCK Net imrpovement patch + stray ; removal
    [2004051601303009856] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@3049 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/ifup.sh
+++ package/base/sysfiles/ifup.sh
@@ -4,9 +4,9 @@ rocknet_tmp_base="/var/run/rocknet"
 [ -d $rocknet_tmp_base ] || mkdir -p $rocknet_tmp_base
 
 var_contains() {
-  local tmp;
+  local tmp
   eval "tmp=\"\$$1\""
-  
+
   [ "${tmp/$3$2/}" != "$tmp" ]
 }
 
@@ -36,14 +36,15 @@ profile=${profile:-default}
 
 if [ $force -eq 0 ] ; then
   active_interfaces="`cat $rocknet_tmp_base/active-interfaces 2>/dev/null`"
-  if test $action = "up" && var_contains active_interfaces ',' "$1($profile)"
+
+  if test $action = "up" && var_contains active_interfaces ',' "$if($profile)"
     then
 	echo "Interface $if($profile) is already listed active, it is probably a good idea to"
 	echo "take it down before activating it. Use -force to suppress this warning."
 	exit 2
   fi
 
-  if test $action = "down" && ! var_contains active_interfaces ',' "$1($profile)"
+  if test $action = "down" && ! var_contains active_interfaces ',' "$if($profile)"
     then
         echo "Interface $if($profile) is not listed active, it is probably a good idea to"
         echo "activate it before deactivating it. Use -force to suppress this warning."
