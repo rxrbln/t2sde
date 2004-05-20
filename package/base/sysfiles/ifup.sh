4e2421cd15670896e5341020f53a32af59f48bc8
Claire Xenia Wolf <claire@clairexen.net>
2004-05-20 09:10:40 +0000

    Rene Rebe: 	make ROCK Net a new experience: 	- speedup 	- register active interfaces in /var/run/rocknet/active-interfaces 	- make if{up,down} warn when the interface is active/unactive 	  (supressable with -force)
    [2004051319074012257] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@3029 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/ifup.sh
+++ package/base/sysfiles/ifup.sh
@@ -1,20 +1,56 @@
 #!/bin/sh
 
+rocknet_tmp_base="/var/run/rocknet"
+[ -d $rocknet_tmp_base ] || mkdir -p $rocknet_tmp_base
+
+var_contains() {
+  local tmp;
+  eval "tmp=\"\$$1\""
+  
+  [ "${tmp/$3$2/}" != "$tmp" ]
+}
+
 usage() {
-	echo "Usage: $0 interface [ profile ]"
+	echo "Usage: $0 interface [ profile ] [ -force ]"
 	exit 1
 }
 
-[ "$1" ] || usage
+force=0
 
-if [ "$2" = "" ] ; then
-	profile="`cat /etc/network/profile 2> /dev/null`"
-else
-	profile="$2"
-fi
+if="$1" ; shift ; [ "$if" ] || usage
+profile=""
+action="${0/#*\/if}"
 
+while [ "$1" ] ; do
+	case $1 in
+		-force) force=1 ;;
+		*) [ "$profile" = "" ] && profile="$1" || usage ;;
+	esac
+	shift
+done
+
+[ "$profile" ] || profile="`cat /etc/network/profile 2> /dev/null`"
 profile=${profile:-default}
 
-/etc/network/rocknet "$profile" "$1" "${0#/sbin/if}"
+# sanity checks (...)
+
+if [ $force -eq 0 ] ; then
+  active_interfaces="`cat $rocknet_tmp_base/active-interfaces 2>/dev/null`"
+  if test $action = "up" && var_contains active_interfaces ',' "$1($profile)"
+    then
+	echo "Interface $if($profile) is already listed active, it is probably a good idea to"
+	echo "take it down before activating it. Use -force to suppress this warning."
+	exit 2
+  fi
+
+  if test $action = "down" && ! var_contains active_interfaces ',' "$1($profile)"
+    then
+        echo "Interface $if($profile) is not listed active, it is probably a good idea to"
+        echo "activate it before deactivating it. Use -force to suppress this warning."
+        exit 2
+  fi
+fi
+
+/etc/network/rocknet "$profile" "$if" "$action"
 echo "$profile" > /etc/network/profile
 
