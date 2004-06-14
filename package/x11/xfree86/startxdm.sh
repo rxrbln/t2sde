3f367f116c3e6f7a4991f423d8134089baebb24e
René Rebe <rene@t2-project.org>
2004-06-14 21:54:48 +0000

    Rene Rebe: 	* reverted r2986: 	  * fixes the respawn issue 	* reimplemented r2986: 	  * fixed the respawn issue in cleaner code
    [2004061423275422639] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: https://svn.exactcode.de/t2/trunk@3214 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/x11/xfree86/startxdm.sh
+++ package/x11/xfree86/startxdm.sh
@@ -4,15 +4,12 @@ XDM=/usr/X11/bin/xdm
 
 [ -e /etc/conf/xdm ] && . /etc/conf/xdm
 
-function check_dm() {
-	echo $XDM | grep -q $1
-}
-
-if [ "$1" = "-nodaemon" ] &&
-		! check_dm gdm && ! check_dm kdm && ! check_dm xdm
-then
-	shift
-fi
+[ "$1" = "-nodeamon" ] && case $XDM in
+	gdm*) ;;
+	kdm*) ;;
+	xdm*) ;;
+	*) shift ;;
+esac
 
 $XDM "$@"
 
