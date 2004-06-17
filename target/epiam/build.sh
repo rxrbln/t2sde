ce136f0cf67b779a16bc8ac1f3aa49ee5bfc7360
Clifford Wolf <clifford@clifford.at>
2004-06-17 10:53:06 +0000

    Andreas V. Meier: 	 add c3-improved for the old ezra c3 	 add target/epiam for Epia Boards with Ezra core
    [2004060913220106624] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://svn.exactcode.de/t2/trunk@3291 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ target/epiam/build.sh
@@ -0,0 +1,27 @@
+
+# This is the shortest possible target build.sh script. Some targets will
+# add code after calling pkgloop() or modify pkgloop's behavior by defining
+# a new pkgloop_action() function.
+#
+pkgloop
+
+echo_header "Finishing build."
+
+echo_status "Creating package database ..."
+admdir="build/${ROCKCFG_ID}/root/var/adm"
+create_package_db $admdir build/${ROCKCFG_ID}/pkgs \
+                  build/${ROCKCFG_ID}/packages.db
+
+echo_status "Creating isofs.txt file .."
+cat << EOT > build/${ROCKCFG_ID}/isofs.txt
+DISK1	$admdir/cache/			${ROCKCFG_SHORTID}/info/cache/
+DISK1	$admdir/cksums/			${ROCKCFG_SHORTID}/info/cksums/
+DISK1	$admdir/dependencies/		${ROCKCFG_SHORTID}/info/dependencies/
+DISK1	$admdir/descs/			${ROCKCFG_SHORTID}/info/descs/
+DISK1	$admdir/flists/			${ROCKCFG_SHORTID}/info/flists/
+DISK1	$admdir/md5sums/		${ROCKCFG_SHORTID}/info/md5sums/
+DISK1	$admdir/packages/		${ROCKCFG_SHORTID}/info/packages/
+EVERY	build/${ROCKCFG_ID}/packages.db	${ROCKCFG_SHORTID}/packages.db
+SPLIT	build/${ROCKCFG_ID}/pkgs/	${ROCKCFG_SHORTID}/pkgs/
+EOT
+
