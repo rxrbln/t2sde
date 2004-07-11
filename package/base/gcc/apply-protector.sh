f33f8935182534c96c08ee43f5cf3e4f9d86721e
Clifford Wolf <clifford@clifford.at>
2004-07-11 20:47:38 +0000

    Clifford Wolf: 	Fixed gcc34 (stack protector)
    [2004070821461510441] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://svn.exactcode.de/t2/trunk@3567 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/gcc/apply-protector.sh
+++ package/base/gcc/apply-protector.sh
@@ -1,19 +1,23 @@
 
-# only try to apply protector if available
 pfile=$( echo "$3" | grep protector | tr ' ' '\t' | tr -s '\t' | cut -f2 )
-if [ "$pfile" ] ; then
-	tar --use-compress-program=bzip2 \
-	  -xf $archdir/${pfile/.gz/.bz2}
+
+if [ "$pfile" ]
+then
+	tar --use-compress-program=bzip2 -xf $archdir/${pfile/.gz/.bz2}
 
 	# Patch protector.dif a bit to apply against current gcc-3
-	#[ $2 = gcc3 ] && patch -p1 < $1/package/base/$2/protector-hotfix.diff
+	if test -f $1/package/base/gcc/$2/protector-hotfix.diff; then
+		patch -p1 < $1/package/base/$2/protector-hotfix.diff
+	fi
 
 	# be careful if you enable this, you have to respect $pkg.
 	# Set -fstack-protector as default?
 	# [ $ROCKCFG_PKG_GCC[23]_STACKPRO = 1 ] && patch -p0 < protectonly.dif
 
 	patch -p0 < protector.dif
-	mv protector.{c,h} gcc/
+	if [ $2 != gcc34 ]; then
+		mv protector.{c,h} gcc/
+	fi
 else
 	echo "No stack-protector available for $2 ..."
 fi
