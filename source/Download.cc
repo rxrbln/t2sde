b39a1e96f0937e7488b39ffefcafa653c936d32a
René Rebe <rene@t2-project.org>
2004-08-17 20:10:11 +0000

    * some typos, unimplemented stuff and Download code bits
    git-svn-id: https://svn.exactcode.de/t2/trunk@3703 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- source/Download.cc
+++ source/Download.cc
@@ -1,6 +1,17 @@
 
+#include <ostream>
+
 #include "desc-parser.hh"
 
 int main (int argc, char* argv[])
 {
+  Package package;
+  
+  for (int i = 1; i < argc; ++i)
+    {
+      package.Clear();
+      package.Parse (argv[i]);
+      
+      std::cout << package.download.value << std::endl;
+    }
 }
