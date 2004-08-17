94bfd529f9dc9d5193f9d785bfee0bc549284dec
Valentin Ziegler <valentin@t2-project.org>
2004-08-17 18:54:59 +0000

    * separated the tag-parsing part into separate baseclass
    git-svn-id: https://svn.exactcode.de/t2/trunk@3697 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- source/desc-parser.cc
+++ source/desc-parser.cc
@@ -1,42 +1,12 @@
 
-#include <vector>
-#include <iostream>
-#include <fstream>
+#include "tag-parser.hh"
 
 // some inteligent Version information cless, that contains
 // some logic to compare version numbers, including -beta and
 // -alpha suffixes
 
-class DescTag {
-public:
-  
-  DescTag(const std::string& i_short_name,
-	  const std::string& i_name,
-	  const std::string& i_long_name)
-    : short_name(i_short_name), name(i_name),
-      long_name(i_long_name)
-  {
-  };
-  
-  virtual ~DescTag(){};
-  
-  virtual std::ostream& Read(std::istream& is)
-  {
-  }
-  virtual std::ostream& Write(std::ostream& os)
-  {
-  }
-  
-  std::string short_name;
-  std::string name;
-  std::string long_name;
-  
-  std::string value;
-};
-
-
 // TODO: IMHO better tat bookkeeping ... -ReneR
-class Package
+class Package : public TagParser
 {
 public:
   Package()
@@ -65,19 +35,7 @@ public:
       sourcepackage("SRC","","SRCPACKAGE"),
       conf("O","","CONF")
   {
-  }
-  
-  ~Package()
-  {}
-  
-  bool Parse(const std::string& file)
-  {
-    int error = 0;
-    
-    std::vector<DescTag*> tags;
-    
     tags.push_back(&copyright);
-    
     tags.push_back(&title);
     tags.push_back(&text);
     tags.push_back(&url);
@@ -99,94 +57,40 @@ public:
     tags.push_back(&cv_url);
     tags.push_back(&sourcepackage);
     tags.push_back(&conf);
-    
-    // parse
-    std::fstream desc_file(file.c_str());
-    
-    std::string line;
-    std::string tag, value;
-    for (int linenr = 0; desc_file; ++linenr) {
-      std::getline(desc_file, line);
-      
-      if (line.size() == 0)
-	continue;
-      
-      // skip comments (maybe we also need to compress
-      // whitespaces? -ReneR
-      if (line[0] == '#' || line[0] == ' ')
-	continue;
-      
-      if (line.size() < 3) {
-	++error;
-	std::cout << "Error: Only garbage found in line "
-		  << linenr << ":" << std::endl
-		  << "  " << line << std::endl;
-      }
-      
-      std::string::size_type idx = line.find(' ');
-      if (idx == std::string::npos)
-	idx = line.size();
-      
-      if (line[0] != '[' || line[idx - 1] != ']') {
-	++error;
-	std::cout << "Error: No tag found in line "
-		  << linenr << ":" << std::endl
-		  << "  " << line << std::endl;
-	continue;
-      }
-      
-      tag.erase();
-      tag.append(line, 1, idx - 2);
-      value.erase();
-      value.append(line, idx, std::string::npos);
-      
-      // search thru "registered" tags
-      bool tag_found = false;
-      for (int i = 0; i < tags.size(); ++i) {
-	if (tag == tags[i]->short_name ||
-	    tag == tags[i]->long_name ||
-	    tag == tags[i]->name)
-	  {
-	    tags[i]->value.append (value + '\n');
-	    tag_found = true;
-	    break;
-	  }
-      }
-      
-      if (!tag_found) {
-	++error;
-	std::cout << line << std::endl;
-	std::cout << "Error: Unknown tag: " << tag << " in line "
-		  << linenr << std::endl;
-      }
-    }
-    return error == 0;
   }
   
-  DescTag copyright;
+  ~Package()
+  {}
+  
+  bool ParsePakage(const std::string& file)
+  {
+    return Parse(file);
+  }
+  
+  Tag copyright;
 
-  DescTag title;
-  DescTag text;
-  DescTag url;
+  Tag title;
+  Tag text;
+  Tag url;
   
-  DescTag author;
-  DescTag maintainer;
+  Tag author;
+  Tag maintainer;
   
-  DescTag category;
-  DescTag flags;
-  DescTag arch;
-  DescTag dependency;
+  Tag category;
+  Tag flags;
+  Tag arch;
+  Tag dependency;
   
-  DescTag license;
-  DescTag status;
-  DescTag version;
-  DescTag priority;
+  Tag license;
+  Tag status;
+  Tag version;
+  Tag priority;
   
-  DescTag download;
-  DescTag cv_url;
+  Tag download;
+  Tag cv_url;
   
-  DescTag sourcepackage;
-  DescTag conf;
+  Tag sourcepackage;
+  Tag conf;
 };
 
 int main (int argc, char* argv[])
@@ -194,7 +98,7 @@ int main (int argc, char* argv[])
   Package p;
   
   if (argc > 1)
-    if (!p.Parse (argv[1]))
+    if (!p.ParsePakage (argv[1]))
       return 1;
   
   return 0;
