2edbdca98cb67d88797057077b40d1901d4b2974
René Rebe <rene@t2-project.org>
2004-08-18 00:47:09 +0000

    * more download work (now recognizes http and ftp, pares the [D] line 	  and could start the downloads - this is done after some sleep)
    git-svn-id: https://svn.exactcode.de/t2/trunk@3710 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- source/Download.cc
+++ source/Download.cc
@@ -1,5 +1,8 @@
 
 #include <ostream>
+#include <sstream>
+
+#include <boost/regex.hpp>
 
 #include "desc-parser.hh"
 
@@ -11,7 +14,52 @@ int main (int argc, char* argv[])
     {
       package.Clear();
       package.Parse (argv[i]);
+      std::stringstream sstr (package.download.value);
       
-      std::cout << package.download.value << std::endl;
+      std::cerr << package.download.value;
+      
+      std::string chksum, file, url, protocol;
+      while (sstr) {
+	sstr >> chksum >> file;
+	if (!sstr) // work around for trailing newlines
+	  continue;
+	std::getline(sstr, url);
+	
+	// url contains at least one leading ' ' ...
+	url.erase(0, 1);
+	bool overwrite_url = url[0] == '!';
+	if (overwrite_url)
+	  url.erase(0, 1);
+	
+	std::string::size_type idx = url.find(':');
+	if (idx == std::string::npos)
+	  protocol.clear();
+	else
+	  protocol = url.substr(0, idx);
+	if (protocol.size() > 0 && protocol[0] == '!')
+	  protocol.erase(0, 1);
+	
+	std::string down_url;
+	if (overwrite_url)
+	  down_url = url;
+	else
+	  down_url = url + file;
+	
+	// TODO: Choose type out of mirror and nodist. Not yet
+	// used in tree - and imperfectly implemented in rock ...
+	std::string store_url = "download/";
+	store_url += "mirror/";
+	store_url += file[0];
+	store_url += + "/";
+	store_url += file;
+	
+	if (protocol == "http" || protocol == "ftp") {
+	  std::cerr << "Would download: " << down_url
+		    << " -> " << store_url << std::endl;
+	}
+	else
+	  std::cerr << "Error: protocol '" << protocol
+		    << "' not yet implemented" << std::endl;
+      }
     }
 }
