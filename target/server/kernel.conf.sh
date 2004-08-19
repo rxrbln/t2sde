1633cdc63c7cc58abd50f7d46cc401e3075d7520
René Rebe <rene@t2-project.org>
2004-08-19 10:40:42 +0000

    * remove unmaintained (unnecesarry even) targets
    git-svn-id: https://svn.exactcode.de/t2/trunk@3735 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/server/kernel.conf.sh
+++
@@ -1,7 +0,0 @@
-# here we disable all OSS modules - because they suck
-
-echo "desktop target -> disabling sound modules ..."
-
-sed "s/CONFIG_SOUND_\(.*\)=./# CONFIG_SOUND_\1 is not set/" $1 > .config.desktop
-mv .config.desktop $1
-
