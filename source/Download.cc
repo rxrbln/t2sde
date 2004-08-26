7bdd30105fb1b01fe030c9a940602877294585e3
Valentin Ziegler <valentin@t2-project.org>
2004-08-26 12:08:00 +0000

    * moved download parsing into desc-parser 	* adapted Download.cc to new desc-parser
    git-svn-id: https://svn.exactcode.de/t2/trunk@3822 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- source/Download.cc
+++ source/Download.cc
@@ -1,6 +1,5 @@
 
 #include <ostream>
-#include <sstream>
 
 #include <boost/regex.hpp>
 
@@ -13,52 +12,27 @@ int main (int argc, char* argv[])
   for (int i = 1; i < argc; ++i)
     {
       package.Clear();
-      package.Parse (argv[i]);
-      std::stringstream sstr (package.download.value);
-      
-      std::cerr << package.download.value;
-      
-      std::string chksum, file, url, protocol;
-      while (sstr) {
-	sstr >> chksum >> file;
-	if (!sstr) // work around for trailing newlines
-	  continue;
-	std::getline(sstr, url);
-	
-	// url contains at least one leading ' ' ...
-	url.erase(0, 1);
-	bool overwrite_url = url[0] == '!';
-	if (overwrite_url)
-	  url.erase(0, 1);
-	
-	std::string::size_type idx = url.find(':');
-	if (idx == std::string::npos)
-	  protocol.clear();
-	else
-	  protocol = url.substr(0, idx);
-	if (protocol.size() > 0 && protocol[0] == '!')
-	  protocol.erase(0, 1);
-	
-	std::string down_url;
-	if (overwrite_url)
-	  down_url = url;
-	else
-	  down_url = url + file;
-	
+      package.ParsePackage (argv[i]);
+
+      unsigned int no_downloads = package.download.download_infos.size();
+
+      for (unsigned int dl=0; dl < no_downloads; dl++) {
+	DownloadInfo& info=package.download.download_infos[dl];
+
 	// TODO: Choose type out of mirror and nodist. Not yet
 	// used in tree - and imperfectly implemented in rock ...
 	std::string store_url = "download/";
 	store_url += "mirror/";
-	store_url += file[0];
+	store_url += info.file[0];
 	store_url += + "/";
-	store_url += file;
+	store_url += info.file;
 	
-	if (protocol == "http" || protocol == "ftp") {
-	  std::cerr << "Would download: " << down_url
+	if (info.protocol == "http" || info.protocol == "ftp") {
+	  std::cerr << "Would download: " << info.down_url
 		    << " -> " << store_url << std::endl;
 	}
 	else
-	  std::cerr << "Error: protocol '" << protocol
+	  std::cerr << "Error: protocol '" << info.protocol
 		    << "' not yet implemented" << std::endl;
       }
     }
