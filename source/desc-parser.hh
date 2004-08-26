d36a41930bdc63eac8cec15aec1260339f327184
Valentin Ziegler <valentin@t2-project.org>
2004-08-26 12:08:00 +0000

    * moved download parsing into desc-parser 	* adapted Download.cc to new desc-parser
    git-svn-id: https://svn.exactcode.de/t2/trunk@3822 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- source/desc-parser.hh
+++ source/desc-parser.hh
@@ -1,9 +1,65 @@
-
 #include "tag-parser.hh"
+#include <sstream>
 
 // some inteligent Version information class, that contains
 // some logic to compare version numbers, including -beta and
 // -alpha suffixes
+
+class DownloadInfo
+{
+public:
+  std::string chksum;
+  std::string file;
+  std::string url;
+  std::string down_url;
+  std::string protocol;
+};
+
+class DownloadTag : public Tag
+{
+public:
+  DownloadTag () : Tag ("D","DOWN","DOWNLOAD") {}
+
+  bool Parse ()
+  {
+    std::stringstream sstr (value);
+
+    // TODO: clean this mess up and check for errors !!
+
+    while (sstr) {
+      DownloadInfo info;
+      sstr >> info.chksum >> info.file;
+      if (!sstr) // work around for trailing newlines
+	continue;
+      std::getline(sstr, info.url);
+	
+      // url contains at least one leading ' ' ...
+      info.url.erase(0, 1);
+      bool overwrite_url = info.url[0] == '!';
+      if (overwrite_url)
+	info.url.erase(0, 1);
+	
+      std::string::size_type idx = info.url.find(':');
+      if (idx == std::string::npos)
+	info.protocol.clear();
+      else
+	info.protocol = info.url.substr(0, idx);
+      if (info.protocol.size() > 0 && info.protocol[0] == '!')
+	  info.protocol.erase(0, 1);
+	
+	if (overwrite_url)
+	  info.down_url = info.url;
+	else
+	  info.down_url = info.url + info.file;
+	download_infos.push_back(info);
+      }
+
+    return true;
+  }
+
+  std::vector<DownloadInfo> download_infos;
+};
+
 
 // TODO: IMHO better tag bookkeeping ... -ReneR
 class Package : public TagParser
@@ -28,7 +84,7 @@ public:
       version("V","VER","VERSION"),
       priority("P","","PRIORITY"),
       
-      download("D","DOWN","DOWNLOAD"),
+      download (),
       cv_url("","","CV-URL"),
       
       sourcepackage("SRC","","SRCPACKAGE"),
@@ -61,9 +117,9 @@ public:
   ~Package()
   {}
   
-  bool ParsePakage(const std::string& file)
+  bool ParsePackage(const std::string& file)
   {
-    return Parse(file);
+    return Parse(file) && download.Parse();
   }
   
   Tag copyright;
@@ -85,7 +141,7 @@ public:
   Tag version;
   Tag priority;
   
-  Tag download;
+  DownloadTag download;
   Tag cv_url;
   
   Tag sourcepackage;
