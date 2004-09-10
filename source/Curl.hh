55bd44eeb942d46b83e339b310448a3e2ab6ec35
Valentin Ziegler <valentin@t2-project.org>
2004-09-10 14:27:49 +0000

    * some updates for the updatelist generator
    git-svn-id: http://svn.exactcode.de/t2/trunk@3852 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- source/Curl.hh
+++ source/Curl.hh
@@ -7,9 +7,11 @@
 
 class CurlException {};
 class TimeoutException : public CurlException {};
-class UnsupportedProtocollException : public CurlException {};
+class UnsupportedProtocolException : public CurlException {};
 class MalformedUrlException : public CurlException {};
 class ConnectErrorException : public CurlException {};
+class AccessDeniedException : public CurlException {};
+class DoesNotExistException : public CurlException {};
 
 class CurlWrapper
 {
@@ -18,7 +20,8 @@ public:
     SetConnectTimeout(20);
     SetMaxTime(-1);
     char* templ=strdup("/tmp/curl_downloadXXXXXX");
-    filename = mktemp(templ);
+    mkstemp(templ);
+    filename=templ;
     free(templ);
   }
 
@@ -28,10 +31,15 @@ public:
   void Download (std::string url) {
     GenParamString ();
     params << " " << url;
-    std::cout << params.str () << std::endl;
+    //    std::cout << params.str () << std::endl;
+    ExecCurl ();
   }
 
   void Download (std::string url, unsigned int start, unsigned int end);
+
+  std::string GetCommand () {
+    return params.str();
+  }
 
   void SetFile (std::string name) {filename=name;}
 
@@ -45,11 +53,40 @@ public:
 
 private:
 
+  void ExecCurl () {
+    int ret=system(GetCommand().c_str());
+    if (ret != 0)
+      switch (ret) {
+      case 1:
+	throw UnsupportedProtocolException();
+      case 3:
+      case 4:
+      case 49:
+	throw MalformedUrlException();
+      case 5:
+      case 6:
+      case 7:
+      case 35:
+	throw ConnectErrorException();
+      case 9:
+      case 10:
+      case 46:
+	throw AccessDeniedException();
+      case 22:
+	throw DoesNotExistException();
+      case 28:
+	throw TimeoutException();
+      default:
+	throw CurlException();
+      }
+  }
+
   void GenParamString () {
-    params.clear();
+    std::string empty("");
+    params.rdbuf() -> str(empty);
     params << "curl -o " << filename;
     if (connect_timeout > 0)
-      params << " --conect-timeout " << connect_timeout;
+      params << " --connect-timeout " << connect_timeout;
     if (max_time > 0)
       params << " --max-time " << max_time;
   }
