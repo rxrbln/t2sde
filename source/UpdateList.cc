f82b9d3f667bb6416fb208ab494faaf71833e4a7
Valentin Ziegler <valentin@t2-project.org>
2004-09-10 14:27:49 +0000

    * some updates for the updatelist generator
    git-svn-id: https://svn.exactcode.de/t2/trunk@3852 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- source/UpdateList.cc
+++ source/UpdateList.cc
@@ -18,13 +18,38 @@ int main (int argc, char* argv[])
       dl.SetConnectTimeout(15);
       dl.SetMaxTime(30);
 
-      for (unsigned int dl=0; dl < no_downloads; dl++) {
-	DownloadInfo& info=package.download.download_infos[dl];
+      for (unsigned int dln=0; dln < no_downloads; dln++) {
+	DownloadInfo& info=package.download.download_infos[dln];
 
-	if (info.protocol=="http" || info.protocol=="ftp") {
+	try {
+	  std::cout << "trying to download listing of " << info.url << std::endl;
+	  std::cout.flush();
 	  dl.Download(info.url);
+	}
+	catch (TimeoutException e) {
+	  std::cout << "Operation timeout" << std::endl;
+	}
+	catch (UnsupportedProtocolException e) {
+	  std::cout << "Unsupported protocol : " << info.protocol << std::endl;
+	}
+	catch (MalformedUrlException e) {
+	  std::cout << "Malformed URL or invalid URL options" << info.url << std::endl;
+	}
+	catch (ConnectErrorException e) {
+	  std::cout << "Could not connect to host" << std::endl;
+	}
+	catch (AccessDeniedException e) {
+	  std::cout << "Access denied or invalid user/pass" << std::endl;
+	}
+	catch (DoesNotExistException e) {
+	  std::cout << "File does not exist" << std::endl;
+	}
+	catch (CurlException e) {
+	  std::cout << "operation canceled due to errors executing '" << dl.GetCommand() << "'" << std::endl;
 	}
 
       }
+
+      dl.RemoveFile();
     }
 }
