1370dcd48299c84d159b9aa8d9948099ec3567ca
René Rebe <rene@t2-project.org>
2004-09-12 22:40:04 +0000

    * use ... instead of * to shorten the filename in the Copyright note 	* fixed sysfiles to not use /*/ but /.../ (see above) in the shortened 	  filename since it ends the comment and obviously results in the C 	  compiler bailing out
    git-svn-id: https://svn.exactcode.de/t2/trunk@3891 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/rc.c
+++ package/base/sysfiles/rc.c
@@ -6,7 +6,7 @@
  * the ROCK-COPYRIGHT-NOTE-END tag. Otherwise it might get removed by
  * the ./scripts/Create-CopyPatch script. Do not edit this copyright text!
  * 
- * ROCK Linux: rock-src/package/*/sysfiles/rc.c
+ * ROCK Linux: rock-src/package/.../sysfiles/rc.c
  * Copyright (C) 1998 - 2003 ROCK Linux Project
  * 
  * This program is free software; you can redistribute it and/or modify
