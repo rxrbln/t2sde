73ecf6378820c0374e3f9d1ce740b7b6029e93c5
René Rebe <rene@t2-project.org>
2004-11-11 20:45:55 +0000

    * updated gcc (3.4.3) - tested on x86 	  * some protector applying cleanups on the way 	  * an private, updated protector patch 	  * removed obsolete patches
    git-svn-id: https://svn.exactcode.de/t2/trunk@4669 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/gcc/apply-protector.sh
+++ package/base/gcc/apply-protector.sh
@@ -1,24 +1,21 @@
 
 # only try to apply protector if available
-pfile=$( echo "$3" | grep protector | tr ' ' '\t' | tr -s '\t' | cut -f2 )
+pfile="$( echo "$desc_D" | tr ' ' '\n' | grep protector )"
 if [ "$pfile" ] ; then
+	echo "Inserting SPP ..."
 	tar --use-compress-program=bzip2 \
 	  -xf $archdir/${pfile/.gz/.bz2}
-
-	# be careful if you enable this, you have to respect $pkg.
-	# Set -fstack-protector as default?
-	# [ $ROCKCFG_PKG_GCC[23]_STACKPRO = 1 ] && patch -p0 < protectonly.dif
 
 	if [ -f protector.dif ] ; then
 		patch -p0 < protector.dif
 		mv protector.{c,h} gcc/
 	elif [ -f gcc_*.dif ] ; then
 		patch -p0 < gcc_*.dif
-		# the protector.* files already extract into gcc/ ...
+		# the protector.* files are already extracted into gcc/ ...
 	else
 		abord "Protector patch not found"
 	fi
 else
-	echo "No stack-protector available for $2 ..."
+	echo "No stack-protector available for $pkg ..."
 fi
 
