c7ab75b3851f6d82712490d4c93b81969a81a536
mnemoc <>
2004-12-01 22:19:32 +0000

    * Silly sync on mnemosyne and my personal unfinished port to T2
    git-svn-id: https://svn.exactcode.de/t2/trunk@4911 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/mnemosyne/build.sh
+++ target/mnemosyne/build.sh
@@ -1,14 +1,14 @@
 pkgloop_action() {
-	local rc
+	local rc=
+	local desc_M=$( grep -e '^\[M\]' ./package/*/$pkg_name/$pkg_name.desc | cut -d' ' -f2- )
+	local desc_P=$( grep -e '^\[P\]' ./package/*/$pkg_name/$pkg_name.desc | cut -d' ' -f2- )
 
         $cmd_buildpkg ; rc=$?
 
 	admdir="build/${ROCKCFG_ID}/var/adm"
 
-	echo_status "M: $desc_M P: $desc_P"
-	if [ $ROCKCFG_TRG_MNEMOSYNE_COPYCACHE -eq 1 ] && \
-	   [[ "$desc_M" == "Alejandro Mery*" ]] && [[ "$desc_P" == "O*" ]]; then
-		if [ -r $admdir/cache/$pkg_name ]; then
+	if [ $ROCKCFG_TRG_MNEMOSYNE_COPYCACHE -eq 1 -a -r $admdir/cache/$pkg_name ]; then
+		if grep -q -e '^\[M\] .*Alejandro Mery' -e '^\[P\] O' ./package/*/$pkg_name/$pkg_name.desc; then
 			echo_status "mnemosyne: catching cache file."
 			cp $admdir/cache/$pkg_name ./package/$pkg_tree/$pkg_name/$pkg_name.cache
 		fi
