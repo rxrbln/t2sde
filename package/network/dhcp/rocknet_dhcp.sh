19a0c04f0907419fdaaab572ad5a20cb71636d21
René Rebe <rene@t2-project.org>
2004-12-12 23:00:07 +0000

    * fixed the dhcp rocknet script to take the interface down
    git-svn-id: https://svn.exactcode.de/t2/trunk@5070 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/network/dhcp/rocknet_dhcp.sh
+++ package/network/dhcp/rocknet_dhcp.sh
@@ -1,6 +1,7 @@
 
 public_dhcp() {
 	addcode up   5 5 "/sbin/dhclient -q $if"
-	addcode down 5 5 "killall -15 dhclient"
+	addcode down 5 5 "killall -TERM dhclient"
+	addcode down 5 6 "sleep 2 ; ip link set $if down || ifconfig $if down"
 }
 
