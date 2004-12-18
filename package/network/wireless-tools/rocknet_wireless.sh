66fffc5ec20f8f4609f75811eb5862513ac8066e
Alejandro Mery Pellegrini <amery@geeks.cl>
2004-12-18 18:52:19 +0000

    * implement iwdetect on rocknet, to autoconfigure a detected network from the list. (lot too improve)
    git-svn-id: http://svn.exactcode.de/t2/trunk@5126 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/network/wireless-tools/rocknet_wireless.sh
+++ package/network/wireless-tools/rocknet_wireless.sh
@@ -1,3 +1,59 @@
+iwdetect_mangle_cells() {
+	local essid=none mode= encryption= quality=
+	local line=
+
+	mkdir -p $rocknet_tmp_base/$if/
+
+	iwlist $if scan | while read line; do
+		if [[ $line = *ESSID* ]]; then
+			[ -n "$essid" ] && echo -e "$essid\t$mode\t$encryption\t$quality"
+			essid="$( echo "$line" | sed -e 's,.*"\(.*\)",\1,g' )"
+			mode=
+			encryption=
+			quality=
+		elif [[ $line = Mode:* ]]; then
+			mode=${line#*:}
+		elif [[ $line = Encryption* ]]; then
+			encryption=${line#*:}
+		elif [[ $line = *Quality* ]]; then
+			quality=$( echo "$line" | sed -e 's,:\([^ ]*\),:\1\n,g' | sed -e 's,^.*:,,g' | head -n 2 | tr '\n' '\t' )
+		elif [ -z "$line" ]; then
+			[ "$essid" != "none" ] && echo -e "$essid\t$mode\t$encryption\t$quality"
+		fi
+	done > $rocknet_tmp_base/$if/detected_cells
+}
+iwdetect_select_cell() {
+	local networkfile=${1:-/etc/network/nettab}
+	local essid= key= extra=
+	local line=
+
+	if [ -f $networkfile ]; then
+	while read -r essid key extra; do
+		line="$( grep -e "^$essid" $rocknet_tmp_base/$if/detected_cells )"
+		if [ -n "$line" ]; then
+			echo "'$essid' selected..."
+			echo "$line" | ( read -r -d '\t' essid mode encryption quality signal;
+				iwconfig $if essid "$essid"
+				case "$mode" in
+					Master)	iwconfig $if mode Managed	;;
+					Ad-Hoc) iwconfig $if mode "Ad-Hoc"	;;
+					*)	echo "WARNING: Unknown mode '$mode'." ;;
+				esac
+				)
+			break
+		fi
+	done < $networkfile
+	else
+		echo "INFO: to use iwdetect you need to set a $networkfile."
+	fi
+}
+
+public_iwdetect() {
+	addcode up 4 2 "ip link set $if up"
+	addcode up 4 3 "iwdetect_mangle_cells" 
+	addcode up 4 4 "iwdetect_select_cell $1"
+	addcode down 4 5 "iwconfig $if essid any"
+}
 
 public_essid() {
 	addcode up 4 5 "iwconfig $if essid $*"
