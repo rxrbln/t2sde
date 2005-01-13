e6ec8949a0329cb33de24ca30a095b8ea0918d22
mnemoc <>
2005-01-13 17:04:11 +0000

    * sync fl_wrapper.c.sh step II (final). adding an special case for open* functions where last argument is '...'
    git-svn-id: https://svn.exactcode.de/t2/trunk@5552 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/tools-source/fl_wrapper.c.sh
+++ misc/tools-source/fl_wrapper.c.sh
@@ -97,6 +97,49 @@ add_wrapper()
 	p1="${p1%, }" ; p2="${p2%, }"
 
 	case ${function} in
+	open*)
+		# remove varg from $p2
+		p2=${p2%, ...}
+		echo ; cat << EOT
+extern $ret_type $function($p1);
+$ret_type (*orig_$function)($p1) = 0;
+
+$ret_type $function($p1)
+{
+	struct status_t status;
+	int old_errno=errno;
+	$ret_type rc;
+	mode_t b = 0;
+
+	handle_file_access_before("$function", f, &status);
+	if (!orig_$function) orig_$function = get_dl_symbol("$function");
+	errno=old_errno;
+
+#if DEBUG == 1
+	fprintf(stderr, "fl_wrapper.so debug [%d]: going to run original $function() at %p (wrapper is at %p).\n",
+		getpid(), orig_$function, $function);
+#endif
+
+	if (a & O_CREAT) {
+	  va_list ap;
+
+	  va_start(ap, a);
+	  b = va_arg(ap, mode_t);
+	  va_end(ap);
+
+	  rc = orig_$function($p2, b);
+	}
+	else
+	  rc = orig_$function($p2);
+
+	old_errno=errno;
+	handle_file_access_after("$function", f, &status);
+	errno=old_errno;
+
+	return rc;
+}
+EOT
+		;;
 	exec*)
 		echo ; cat << EOT
 extern $ret_type $function($p1);
