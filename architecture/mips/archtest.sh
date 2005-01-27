773786cb54383ee4e10ebdbbd99709963564a2df
René Rebe <rene@t2-project.org>
2005-01-27 07:35:20 +0000

    * substituted unknown with t2 for architecture triplets (better look 	  and feel and marketing / PR)
    git-svn-id: http://svn.exactcode.de/t2/trunk@5812 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- architecture/mips/archtest.sh
+++ architecture/mips/archtest.sh
@@ -2,8 +2,8 @@
 case "$ROCKCFG_MIPS_ENDIANESS" in
     EL)
     	arch_bigendian=no
-	arch_target="mipsel-unknown-linux-gnu" ;;
+	arch_target="mipsel-t2-linux-gnu" ;;
     EB)
     	arch_bigendian=yes
-	arch_target="mips-unknown-linux-gnu" ;;
+	arch_target="mips-t2-linux-gnu" ;;
 esac
