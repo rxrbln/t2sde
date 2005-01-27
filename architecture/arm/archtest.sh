e307226aeac8a25e69e88aec26f7fe725aff7eaf
René Rebe <rene@t2-project.org>
2005-01-27 07:35:20 +0000

    * substituted unknown with t2 for architecture triplets (better look 	  and feel and marketing / PR)
    git-svn-id: https://svn.exactcode.de/t2/trunk@5812 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- architecture/arm/archtest.sh
+++ architecture/arm/archtest.sh
@@ -2,8 +2,8 @@
 case "$ROCKCFG_ARM_ENDIANESS" in
     EL)
     	arch_bigendian=no
-	arch_target="arm-unknown-linux-gnu" ;;
+	arch_target="arm-t2-linux-gnu" ;;
     EB)
     	arch_bigendian=yes
-	arch_target="arm-unknown-linux-gnu" ;;
+	arch_target="arm-t2-linux-gnu" ;;
 esac
