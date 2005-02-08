860d84385d6ac90d64646dd68f53da9888acaf58
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-02-08 15:30:49 +0000

    * added dummy script IsBuilt.sh which see the build-state of svn-modified packages on a determited root dir
    git-svn-id: https://svn.exactcode.de/t2/trunk@6123 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ misc/archive/IsBuilt.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ "$1" == "-v" ]; then
+	VERBOSE=1; shift
+else
+	VERBOSE=
+fi
+LOGSDIR=$1/var/adm/logs
+
+if [ ! -d $LOGSDIR ]; then
+	echo "'${LOGSDIR%var/adm/logs}' is not a valid root."
+	exit 1
+fi
+
+( cd package; svn st | cut -d'/' -f2 | sort -u) | while read pkg; do
+	status=
+	for file in $( ls -1 $LOGSDIR/[0-9]-$pkg.{out,err,log} 2> /dev/null ); do
+		case $file in
+			*.err)	status=2	;;
+			*.log)	[ "$status" ] || status=1	;;
+		esac
+	done
+	if [ -z "$status" ]; then
+		[ "$VERBOSE" ] && echo "$pkg PENDING"
+	elif [ "$status" == 2 ]; then
+		echo "$pkg FAILED"
+	else
+		echo "$pkg OK"
+	fi
+done
