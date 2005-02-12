10855f83d1afebb9945d6a289415a2d95d457f3c
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-02-12 19:45:00 +0000

    * added runit rocknet module to let modules create supervised/logged processes. (the presence of CANUSESERVICE=1tells the functions service_{create,destroy} functions are available.)
    git-svn-id: http://svn.exactcode.de/t2/trunk@6396 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ package/base/runit/rocknet_runit.sh
@@ -0,0 +1,55 @@
+export CANUSESERVICE=1
+
+service_create() {
+	local service_name=$1 runstring="$2" finishstring="$3"
+
+	mkdir -p $rocknet_tmp_base/$service_name/log
+
+	# ./run
+	cat <<-EOT > $rocknet_tmp_base/$service_name/run
+	#!/bin/sh
+	exec 2>&1
+
+	$runstring
+	EOT
+
+	# ./log/run
+	cat <<-EOT > $rocknet_tmp_base/$service_name/log/run
+	#!/bin/sh
+	exec 2>&1
+
+	if [ ! -d /var/log/$service_name ]; then
+	    mkdir -p /var/log/$service_name
+	    chown log /var/log/$service_name
+	fi
+	exec chpst -ulog svlogd -tt /var/log/$service_name
+	EOT
+
+	# ./finish
+	if [ "$finishstring" ]; then
+		cat <<-EOT > $rocknet_tmp_base/$service_name/finish
+		#!/bin/sh
+		exec 2>&1
+
+		$finishstring
+		EOT
+		chmod +x $rocknet_tmp_base/$service_name/finish
+	fi
+        
+	chmod +x $rocknet_tmp_base/$service_name/{,log/}run
+	ln -nfs $rocknet_tmp_base/$service_name/ /service/$service_name
+}
+
+service_destroy() {
+	local service_name=$1 x=
+
+	for x in . log; do
+		if [ -d /service/$service_name/$x/supervice ]; then
+        		runsvctrl d /service/$service_name/$x
+        		svwaitdown /service/$service_name/$x
+		fi
+	done
+
+	rm -f /service/$service_name
+	rm -rf $rocknet_tmp_base/$service_name/
+}
