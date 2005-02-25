970f50ead40eae190b8a267363e98d1c0c05eae4
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-02-25 14:44:33 +0000

    * added 'nodns' argument for udhcp rocknet option
    git-svn-id: https://svn.exactcode.de/t2/trunk@6812 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/network/udhcp/rocknet_udhcp.sh
+++ package/network/udhcp/rocknet_udhcp.sh
@@ -1,10 +1,25 @@
 
 public_udhcp() {
+	local opt_nodns=
 	local HOSTNAME="`hostname`" cmdline=
 	[ "$HOSTNAME" == "(none)" ] && HOSTNAME=
 	
 	cmdline="/usr/sbin/udhcpc ${HOSTNAME:+-h $HOSTNAME} -i $if"
 	cmdline="$cmdline -s /etc/udhcp/t2-default.script"
+
+	while [ $# -ge 1 ]; do
+		case "$1" in
+			nodns)	opt_nodns=1	;;
+			*)	echo "WARNING: udhcp doesn't understand '$1'"
+				;;
+		esac
+		shift
+	done
+	
+	if [ "$opt_nodns" ]; then
+		cmdline="export UPDATE_RESOLVCONF=0;
+$cmdline"
+	fi
 
 	if [ "$CANUSESERVICE" == "1" ]; then
 		addcode up 5 1 "service_create $if '$cmdline -f'" \
