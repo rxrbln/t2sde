4745e8b2b7b204c28725afb558bf98d5fc6806a8
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-03-21 19:07:28 +0000

    * added RegressionsReport.sh script to generate a nice web output showing the state of a build and svn changes related
    git-svn-id: https://svn.exactcode.de/t2/trunk@7630 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ misc/archive/RegressionsReport.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+config=ref
+TARGET=regressions
+
+mkdir -p $TARGET
+
+sh misc/archive/AuditBuild.sh -w $TARGET -cfg $config --no-enabled-too -repository package/* \
+	| grep '\(CHANGED\|UPDATED\|ADDED\|FAILED\|PENDING\|NOQUEUED\)' > $TARGET/regressions.$config.$$
+
+
+cat <<EOT > $TARGET/regressions.$config.html
+<html>
+	<head><title>T2 r$( svn info | sed -n 's,^Revision: \(.*\),\1,p' ) - $( date )</title></head>
+<body><pre>
+$( ./scripts/Create-ErrList -cfg $config )
+</pre><hr><table>
+<tr><th>Package</th><th>SVN Status</th><th>Version</th><th>Audit</th><th>Status</th></tr>
+EOT
+grep -v NOQUEUED $TARGET/regressions.$config.$$ >> $TARGET/regressions.$config.html
+
+cat <<EOT >> $TARGET/regressions.$config.html
+</table><hr><table>
+<tr><th>Package</th><th>SVN Status</th><th>Version</th><th>Audit</th><th>Status</th></tr>
+EOT
+grep NOQUEUED $TARGET/regressions.$config.$$ >> $TARGET/regressions.$config.html
+cat <<EOT >> $TARGET/regressions.$config.html
+</table></body>
+</html>
+EOT
+
+rm -f $TARGET/regressions.$config.$$
+mv -f $TARGET/regressions.$config.html $TARGET/$config/regressions.html
+
