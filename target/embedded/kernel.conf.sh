8ef42cc73dc96a1f69830d7be2dfb54da5b6ee1f
René Rebe <rene@t2-project.org>
2005-04-12 07:47:27 +0000

    * disabled more kernel subsystems that are seldom needed on embedded 	  systems
    git-svn-id: https://svn.exactcode.de/t2/trunk@8199 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/embedded/kernel.conf.sh
+++ target/embedded/kernel.conf.sh
@@ -18,5 +18,11 @@ echo "desktop target -> disabling oss sound modules ..."
 sed -i -e "s/CONFIG_VIDEO_DEV=./# CONFIG_VIDEO_DEV is not set/" \
        -e "s/CONFIG_DVB=./# CONFIG_DVB is not set/" \
        -e "s/CONFIG_FB=./# CONFIG_FB is not set/" \
-       -e "s/CONFIG_SOUND=./# CONFIG_SOUND is not set/" $1
+       -e "s/CONFIG_SOUND=./# CONFIG_SOUND is not set/" \
+       -e "s/CONFIG_PHONE=./# CONFIG_PHONE is not set/" \
+       -e "s/CONFIG_FUSION=./# CONFIG_FUSION is not set/" \
+       -e "s/CONFIG_I2O=./# CONFIG I2O is not set/" \
+       -e "s/CONFIG_AGP=./# CONFIG AGP is not set/" \
+       -e "s/CONFIG_DRM=./# CONFIG DRM is not set/" \
+       -e "s/CONFIG_\(.*\)GAMEPORT=./# CONFIG_\1GAMEPORT is not set/" $1
 
