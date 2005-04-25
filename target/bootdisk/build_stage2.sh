d72178c752e7f31fc99bd6b38127bf6b3683bb7d
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-04-25 00:01:22 +0000

    * merged 8515 - uml_utilities (and skas) should not be part of bootdisk
    git-svn-id: https://svn.exactcode.de/t2/trunk@8516 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/bootdisk/build_stage2.sh
+++ target/bootdisk/build_stage2.sh
@@ -51,7 +51,7 @@ package_map='       +00-dirtree
 +netkit-base        +netkit-ftp         +netkit-telnet      +netkit-tftp
 +sysfiles           +libpcap            +iptables           +tcp_wrappers
 -kiss               +kbd		-syslinux           +ntfsprogs
--ethtool            -uml_utilities      -bdb
+-ethtool            -bdb
 +libol'
 
 if [ -f ../../pkgs/bize.tar.bz2 -a ! -f ../../pkgs/mine.tar.bz2 ] ; then
