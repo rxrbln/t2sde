dea3ef0f07d8e2cb20844a6ce9e99fafb7ef7625
René Rebe <rene@t2-project.org>
2005-05-30 13:40:18 +0000

    * added support for pkg-ver to Update.sh, to allow easier cut'n paste 	  of t2-updates notifications
    git-svn-id: https://svn.exactcode.de/t2/trunk@9233 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/archive/Update.sh
+++ misc/archive/Update.sh
@@ -16,8 +16,14 @@
 pkg="$1" ; shift
 ver="$1" ; shift
 
-if [ -z "$pkg" -o -z "$ver" ] ; then
+if [ -z "$ver" ]; then
+	ver=${pkg/*-/}
+	pkg=${pkg%-$ver}
+fi
+
+if [ -z "$pkg" -o -z "$ver" ]; then
 	echo "Usage: $0 pkg ver"
+	echo "   or: $0 pkg-ver"
 	exit
 fi
 
