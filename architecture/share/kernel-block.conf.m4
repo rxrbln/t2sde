rxrbln/t2sde

commit 1c09e3548e49130685580c8398594f5217d2a410

* cleaned shared kernel configuration to compile all the previously 	  non-vitial built-ins modular
git-svn-id: http://svn.exactcode.de/t2/trunk@8499 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

architecture/share/kernel-block.conf.m4
@@ -12,64 +12,42 @@ dnl the Free Software Foundation; version 2 of the License. A copy of the
 dnl GNU General Public License can be found in the file COPYING.
 dnl --- T2-COPYRIGHT-NOTE-END ---
 
-dnl Per partition statistics are rather useful ...
-dnl
-CONFIG_BLK_STATS=y
-
 dnl Activate SCSI discs and cd-roms - but not the verbose
 dnl SCSI error reporting (CONSTANTS)
 dnl
 CONFIG_SCSI=y
-CONFIG_BLK_DEV_SD=y
-CONFIG_BLK_DEV_SR=y
-CONFIG_CHR_DEV_SG=y
+CONFIG_BLK_DEV_SD=m
+CONFIG_BLK_DEV_SR=m
+CONFIG_CHR_DEV_SG=m
 # CONFIG_SCSI_CONSTANTS is not set
 
 dnl Some IDE stuff
 dnl
-CONFIG_BLK_DEV_IDESCSI=y
-CONFIG_BLK_DEV_IDECD=y
-# CONFIG_BLK_DEV_IDETAPE is not set
-CONFIG_BLK_DEV_IDEFLOPPY=y
+CONFIG_IDE=y
+CONFIG_BLK_DEV_IDE=y
+CONFIG_BLK_DEV_IDESCSI=m
+CONFIG_BLK_DEV_IDEDISK=m
+CONFIG_BLK_DEV_IDECD=m
+CONFIG_BLK_DEV_IDETAPE=m
+CONFIG_BLK_DEV_IDEFLOPPY=m
 # CONFIG_IDE_TASKFILE_IO is not set
+
+dnl Make sure the drivers are modular ...
+dnl
+CONFIG_IDE_GENERIC=m
+CONFIG_BLK_DEV_CMD640=m
+CONFIG_BLK_DEV_GENERIC=m
+CONFIG_BLK_DEV_RZ1000=m
+CONFIG_BLK_DEV_PIIX=m
+
+CONFIG_SCSI_SATA=m
+CONFIG_SCSI_QLA2XXX=m
 
 dnl Use multi-mode and DMA since this reduces the CPU load and
 dnl also increases the IDE I/O performance in general
 CONFIG_BLK_DEV_IDEDMA=y
 CONFIG_IDEDISK_MULTI_MODE=y
 CONFIG_IDEDMA_PCI_AUTO=y
-
-dnl Activate widest used IDE drivers (for proper DMA support)
-dnl
-CONFIG_BLK_DEV_ALI15X3=y
-CONFIG_BLK_DEV_AMD74XX=y
-CONFIG_BLK_DEV_CMD64X=y
-CONFIG_BLK_DEV_CS5530=y
-CONFIG_BLK_DEV_HPT34X=y
-CONFIG_BLK_DEV_HPT366=y
-CONFIG_BLK_DEV_NS87415=m
-CONFIG_BLK_DEV_OPTI621=m
-CONFIG_BLK_DEV_PDC202XX=y
-CONFIG_BLK_DEV_PDC202XX_OLD=y
-CONFIG_BLK_DEV_PDC202XX_NEW=y
-CONFIG_BLK_DEV_PIIX=y
-CONFIG_BLK_DEV_RZ1000=y
-CONFIG_BLK_DEV_SC1200=y
-CONFIG_BLK_DEV_SVWKS=y
-CONFIG_BLK_DEV_SIIMAGE=y
-CONFIG_BLK_DEV_SIS5513=y
-CONFIG_BLK_DEV_SLC90E66=y
-CONFIG_BLK_DEV_TRM290=y
-CONFIG_BLK_DEV_VIA82CXXX=y
-
-dnl Widest used non-on-board card
-dnl
-CONFIG_SCSI_AIC7XXX=y
-# CONFIG_AIC7XXX_TCQ_ON_BY_DEFAULT is not set
-
-dnl Widest used on-board card
-dnl
-CONFIG_SCSI_SYM53C8XX_2=y
 
 dnl Enable PCMCIA SCSI (drivers themself are modules)
 dnl
@@ -81,12 +59,5 @@ CONFIG_CD_NO_IDESCSI=y
 
 dnl Enable software-raid
 dnl
-CONFIG_MD=y
-CONFIG_BLK_DEV_MD=y
-CONFIG_MD_LINEAR=y
-CONFIG_MD_RAID0=y
-CONFIG_MD_RAID1=y
-CONFIG_MD_RAID5=y
-CONFIG_MD_MULTIPATH=m
-CONFIG_BLK_DEV_LVM=y
+CONFIG_MD=m
 

architecture/share/kernel-common.conf.m4
@@ -37,6 +37,10 @@ CONFIG_MODULE_UNLOAD=y
 # CONFIG_MODVERSIONS is not set
 CONFIG_KMOD=y
 
+dnl Firmware loader can always be useful
+dnl
+CONFIG_FW_LOADER=y
+
 dnl Loopback device can always be useful
 dnl
 CONFIG_BLK_DEV_LOOP=y

architecture/share/kernel-fs.conf.m4
@@ -16,65 +16,18 @@ dnl Enable Quota Support
 dnl
 CONFIG_QUOTA=y
 
-dnl FAT is still very importand
-dnl
-CONFIG_FAT_FS=y
-CONFIG_MSDOS_FS=y
-# CONFIG_UMSDOS_FS is not set
-CONFIG_VFAT_FS=y
-
-dnl ISO9660 support (including JOLIET and ZISOFS compression)
-dnl
-CONFIG_ISO9660_FS=y
 CONFIG_JOLIET=y
 CONFIG_ZISOFS=y
 
-dnl Enable IBM JFS
-dnl
-CONFIG_JFS_FS=m
-CONFIG_JFS_POSIX_ACL=y
-# CONFIG_JFS_DEBUG is not set
-# CONFIG_JFS_STATISTICS is not set
-
-dnl Enable SGI XFS
-dnl
-CONFIG_XFS_FS=m
-# CONFIG_XFS_RT is not set
-# CONFIG_XFS_QUOTA is not set
-CONFIG_XFS_POSIX_ACL=y
-
-dnl DevFS and Proc
-dnl in the past we had DEVPTS disabled since it is done in DevFS
-dnl unfortunately it got removed in 2.5 due to the code duplication.
-dnl So now even with devfs devpts must be enabled and used.
-dnl
-CONFIG_DEVFS_FS=y
-CONFIG_DEVFS_MOUNT=y
-# CONFIG_DEVFS_DEBUG is not set
 CONFIG_DEVPTS_FS=y
 CONFIG_PROC_FS=y
-
-dnl Enable ext2fs and ext3fs
-dnl
-CONFIG_EXT2_FS=y
-CONFIG_EXT2_FS_XATTR=y
-CONFIG_EXT2_FS_POSIX_ACL=y
-CONFIG_EXT3_FS=y
-CONFIG_EXT3_FS_XATTR=y
-CONFIG_EXT3_FS_POSIX_ACL=y
-
-dnl Reiser Filesystem
-dnl
-CONFIG_REISERFS_FS=y
-# CONFIG_REISERFS_CHECK is not set
-# CONFIG_REISERFS_PROC_INFO is not set
 
 dnl Network FS settings
 dnl Version 3 has several advantages ...
 dnl
 CONFIG_NFS_FS=y
 CONFIG_NFS_V3=y
-CONFIG_NFSD_V3=y
+CONFIG_NFSD_V3=m
 
 dnl ROMFS, RAMFS, CRAMFS and TMPFS (for initrd, install and /tmp)
 dnl
@@ -86,3 +39,4 @@ CONFIG_TMPFS=y
 dnl Squashfs (if patched in)
 dnl
 CONFIG_SQUASHFS=y
+

architecture/share/kernel-net.conf.m4
@@ -11,22 +11,21 @@ dnl it under the terms of the GNU General Public License as published by
 dnl the Free Software Foundation; version 2 of the License. A copy of the
 dnl GNU General Public License can be found in the file COPYING.
 dnl --- T2-COPYRIGHT-NOTE-END ---
-dnl Widest used non-on-board network card (3Com 905)
-dnl
-CONFIG_NET_VENDOR_3COM=y
-CONFIG_VORTEX=y
 
 dnl some more module which are not activated by default
-CONFIG_NET_VENDOR_SMC=y
-CONFIG_NET_VENDOR_RACAL=y
+dnl
 CONFIG_NET_ISA=y
 CONFIG_NET_POCKET=y
 
-dnl Widest used on-board network card (Tulip)
-dnl
 CONFIG_NET_ETHERNET=y
 CONFIG_NET_EISA=y
-CONFIG_DE4X5=y
+
+dnl Enable some vedor sections
+dnl
+CONFIG_NET_VENDOR_3COM=y
+CONFIG_NET_VENDOR_SMC=y
+CONFIG_NET_VENDOR_RACAL=y
+CONFIG_8139TOO=m
 
 dnl Enable some categories so drivers are enabled as modules
 dnl

commit 195a7f0725a0bef4f3167e6e1aaa90f16596c714

* fixed the Linux MD supoprt to built - bug introduced in r8499 	  (though the implementations are modular the config option must be 	   =Y to enable the subsystem - sigh)
git-svn-id: https://svn.exactcode.de/t2/trunk@9673 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

architecture/share/kernel-block.conf.m4
@@ -63,5 +63,5 @@ CONFIG_CD_NO_IDESCSI=y
 
 dnl Enable software-raid
 dnl
-CONFIG_MD=m
+CONFIG_MD=y
 