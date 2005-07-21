fc8bb6931c7aaf50cb99df359380ef740ff3843b
René Rebe <rene@t2-project.org>
2005-07-21 12:32:42 +0000

    * added custom network configuration
    git-svn-id: https://svn.exactcode.de/t2/trunk@10448 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ target/archivista/rootfs/home/archivista/network.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Simple custom network configuration.
+#
+# Copyright (C) 2005 Archivista GmbH
+# Copyright (C) 2005 Rene Rebe
+
+get_ip()
+{
+	x=$2
+        until [ "$set" ]; do
+                x=`Xdialog  --stdout --inputbox "$1" 10 38 $x`
+
+                # check ip
+                if [ `echo $x |
+                      sed 's,[0-9]\+.[0-9]\+.[0-9]\+.[0-9]\+,,'` ]
+                then
+                        Xdialog --infobox "IP not valid!" 8 28
+                else
+                        set=1
+                fi
+        done
+	echo $x
+}
+
+
+if [ -e /etc/conf/network ]; then
+	echo "Config exists - exiting."
+	exit
+fi
+
+if ! Xdialog --yesno "The network connection is not yet
+configured. Configure now?" 8 34; then
+	echo not yet
+	touch /etc/conf/network
+	exit
+fi
+
+if Xdialog --yesno "Use DHCP to obtain\nconfiguration?" 8 28; then
+	cat > /etc/conf/network <<-EOT
+interface eth0
+	dhcp
+EOT
+else
+	tip=192.168.0.100/24
+	until [ "$ip" ]; do
+		tip=`Xdialog  --stdout --inputbox "Internet address and network
+prefix in CIDR notation
+(e.g. 192.168.0.100/24 or 10.0.0.100/16):" 10 38 $tip`
+
+		# check ip
+		if [ `echo $tip |
+		      sed 's,[0-9]\+.[0-9]\+.[0-9]\+.[0-9]\+/[0-9]\+,,'` ]
+		then
+			Xdialog --infobox "IP not valid!" 8 28
+		else
+			ip=$tip
+		fi
+	done
+
+	gw=`get_ip "Gateway address
+(e.g. 192.168.0.1):" ${ip%.*}.1`
+	[ "$gw" ] && ns=`get_ip "Nameserver address
+(e.g. 192.168.0.1):" ${gw}`
+
+	echo IP: $ip GW: $gw NS: $ns
+fi
+
