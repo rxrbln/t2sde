873a35f80704667717aff6a93468888afa5059d1
René Rebe <rene@t2-project.org>
2005-07-22 09:15:40 +0000

    * added injecting the archivista.xpm.gz as t2.xpm.gz 	* added utilization of the inject_hook hack to inject a lot of CVS 	  code as well as a tar'ed mysql database to the archivist target 	  (yes this is a bit dirty, but there is no real release of the code 	   yet anyway and I got into holiday tomorrow and CD due today ...)
    git-svn-id: https://svn.exactcode.de/t2/trunk@10538 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/build_image.sh
+++ target/archivista/build_image.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
+# this is a prelemninary hack just because the archivsta stuff is not
+# available in nicer pieces so far
+inject_archivista()
+{
+	pushd .
+	# cvs code
+	cd $imagelocation/home
+	tar xvfz $base/target/$target/cvs.tar.gz
+
+	# configuration
+	cd cvs ; patch -p1 < $base/target/$target/config.patch ; cd ..
+
+	# example db
+	cd $imagelocation/home/data/archivista
+	tar xvfz $base/target/$target/mysql.tar.gz
+	popd
+}
+
+inject_hook=inject_archivista
+
 . $base/target/livecd/build_image.sh
 
