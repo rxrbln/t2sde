3135d625302b46471aaa366ff3029d88cd6f21aa
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-07-23 15:33:02 +0000

    * changed clip's getpatch.sh to use wget instead of curl - curl has troubles fetching files from their ftp
    git-svn-id: https://svn.exactcode.de/t2/trunk@10578 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/develop/clip/getpatch.sh
+++ package/develop/clip/getpatch.sh
@@ -20,7 +20,7 @@ echo "get: $location/patch.tgz"
 if [ -f patch.tgz ]; then
 	mv patch.tgz $tempfile
 else
-	curl -v $location/patch.tgz -o $tempfile
+	wget $location/patch.tgz -O $tempfile
 	if [ $? -ne 0 ]; then
 		rm -f $tempfile
 		exit
