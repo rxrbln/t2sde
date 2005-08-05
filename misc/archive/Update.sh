c74e0f8b58db61def295b62e9a6468229fdd00b6
Minto van der Sluis <Minto@MultiMach.com>
2005-08-05 19:17:20 +0000

    * Added additional arguments test + added comments.
    git-svn-id: https://svn.exactcode.de/t2/trunk@10833 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/archive/Update.sh
+++ misc/archive/Update.sh
@@ -13,32 +13,66 @@
 # GNU General Public License can be found in the file COPYING.
 # --- T2-COPYRIGHT-NOTE-END ---
 
+# Extract packagename and version number from the script arguments.
 pkg="$1" ; shift
-ver="$1" ; shift
+ver="$1" ; shift 
 
+# The script also supports the package name and version separated by a '-'
+# dash. In this case they have to be separated from each other. 
 if [ -z "$ver" ]; then
 	ver=${pkg/*-/}
 	pkg=${pkg%-$ver}
 fi
 
-if [ -z "$pkg" -o -z "$ver" ]; then
+# Check if both package name and version have been extracted from the 
+# commandline arguments. If not the script was called with invalid 
+# arguments, show the usage. 
+if [ -z "$pkg" -o -z "$ver" -o $pkg == $ver ]; then
 	echo "Usage: $0 pkg ver"
 	echo "   or: $0 pkg-ver"
+	echo ""
+	echo "Updating a package to a new versions."
+	echo ""
+	echo "  pkg	Packagename to be updated"
+	echo "  ver	New version number for the package"
+	echo "" 
+	echo "For detailed information on how to update packages"
+	echo "to new versions, please have a look at the online"
+	echo "T2 documentaion."
 	exit
 fi
 
+# Make sure any diff files from previous runs are removed.
 rm -f $$.diff
+
+# Only package names in lowercase are supported. As an additional
+# service any package names supplied in capitols will be converted
+# to lowercase.
 pkg=`echo $pkg | tr [A-Z] [a-z]`
 
+# Of course we can only update packages we know about.
 if [ -d package/*/$pkg ]; then
+	# The package exists so now update the package descriptor
+	# for the given package to the new version. Luckily we have
+	# a script for that too :-)
 	./scripts/Create-PkgUpdPatch $pkg $ver | tee $$.diff
 	patch -p1 < $$.diff
 	rm -f $$.diff
 
+	# Step 2: Use the modified package descriptor to download
+	# the package. Again, nothing more that calling an existing
+	# script.
 	./scripts/Download $pkg
+
+	# Third and final step is updating the checksum for the
+	# new download.
 	./scripts/Create-CkSumPatch $pkg | patch -p0
 else
+	# Oeps, the given package name does not exist.
 	echo "ERROR: package $pkg doesn't exist"
+	# As an extra service for the user a list possibilities is
+	# presented with packages closely matching the given package
+	# name.
 	pkg=`echo "$pkg" | tr '\-_.' '***'`
 	echo `echo package/*/*$pkg*/ | tr ' ' '\n' | grep -v '*' | cut -d'/' -f3`
 fi
