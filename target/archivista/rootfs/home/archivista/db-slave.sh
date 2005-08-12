cdf1528a8cc3c8c8b5b8e0cc87c019a241402591
René Rebe <rene@t2-project.org>
2005-08-12 15:56:30 +0000

    * added archivista target database master/slave setup scripts templates 	  and fluxbox menu entries
    git-svn-id: http://svn.exactcode.de/t2/trunk@10983 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ target/archivista/rootfs/home/archivista/db-slave.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ "$UID" -ne 0 ]; then
+	exec gnomesu -t "Database slave mode" \
+	-m "Please enter the system password (root user)^\
+in order to bring the database into slave mode." -c "/usr/X11/bin/xterm -e $0"
+fi
+
+# PATH and co
+. /etc/profile
+
+# get master ip
+until [ "$masterip" ]; do
+	tmasterip=`Xdialog --stdout --inputbox \
+	"Enter IP or hostname of master server:" 10 38 $tmasterip`
+
+	if ! ping -c 1 $tmasterip ; then
+		Xdialog --infobox 'Master not answering (pings)!' 8 28
+	else
+		export masterip=$tmasterip
+	fi
+done 
+
+rc mysql stop
+
+# copy the db and perform other needed tasks on the master
+echo "Please enter the master server system (root user) password in order to
+copy the initial database:"
+rsync -arve ssh $masterip:/home/data/archivista/mysql \
+                          /home/data/archivista/mysql
+echo Return code: $?
+
+if [ $? -ne 0 ]; then
+	Xdialog --infobox 'Error obtaining initial database from master!' 8 28
+	exit
+fi
+
+# bring into slave mode
+sed -i -e "s/.*server-id.*/server-id = 2/" \
+       -e "s/.*log-bin$/log-bin/" /etc/my.cnf
+
+rc mysql start
+
+echo "Press enter or close the window."
+read in
+
