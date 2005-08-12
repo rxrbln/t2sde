b2e7393a7a759a50dae11b61c2c8002cfefa675e
René Rebe <rene@t2-project.org>
2005-08-12 09:15:51 +0000

    * added backup.sh backend script and backup-restore frontend script 	  to the archivista target
    git-svn-id: https://svn.exactcode.de/t2/trunk@10969 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ target/archivista/rootfs/home/archivista/backup-restore.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+if [ "$UID" -ne 0 ]; then
+        exec gnomesu -t "Restore backup" \
+        -m "Please enter the system password (root user)^\
+in order to restore the backup." -c $0
+fi
+
+(
+	rc mysql stop
+	mkdir -p /home/data
+	rm -rf /home/data/*
+	cd /home/data
+        mt -f /dev/nst0 asf 1
+	flexbackup -extract
+	rc mysql start
+) | Xdialog --no-cancel --log - 20 60
+
