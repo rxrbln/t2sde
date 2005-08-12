145a26caa0c9f4af28b7bc2f54649c0bc4415374
René Rebe <rene@t2-project.org>
2005-08-12 14:57:06 +0000

    * improved archivista/baackup-setup.sh to allow disabling the backup
    git-svn-id: https://svn.exactcode.de/t2/trunk@10980 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/backup-setup.sh
+++ target/archivista/rootfs/home/archivista/backup-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$UID" -ne 0 ]; then
-        exec gnomesu -t "Setup backup" \
-        -m "Please enter the system password (root user)^\
+	exec gnomesu -t "Setup backup" \
+	-m "Please enter the system password (root user)^\
 in order to setup the backup." -c $0
 fi
 
@@ -20,13 +20,21 @@ ttime=`echo "$line" | cut -d ' ' -f 2`
 until [ "$days" ]; do
 	tdays=`Xdialog --stdout --inputbox "Days the backup should be run on.
 Allowed are ranges from 1 (Monday) to
-7 (Sunday):" 10 40 "$tdays"`
-	if [[ "$tdays" = [1-7]-[1-7] ]]; then
+7 (Sunday) and 0 to disable the backup:" 10 40 "$tdays"`
+	if [ "$tdays" = 0 ] || [ "$tdays" = [1-7]-[1-7] ]]; then
 		days=$tdays
 	else
 		Xdialog --infobox "Ragne not valid!" 8 28
 	fi
 done
+
+# disable?
+if [ "$tdays" = 0 ]; then
+	# remove previous line ...
+	sed -i "/archivista.*backup.sh/d" /etc/crontab
+	rc cron restart
+	exit
+fi
 
 until [ "$time" ]; do
 	ttime=`Xdialog --stdout --inputbox "Time the backup should be run on
