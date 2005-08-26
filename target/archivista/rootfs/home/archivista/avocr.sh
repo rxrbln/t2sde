804d10664c3e9282886304f5f8853ff94a3abb95
René Rebe <rene@t2-project.org>
2005-08-26 08:52:46 +0000

    * added a info box about font parsing to the archivista ocr start script
    git-svn-id: https://svn.exactcode.de/t2/trunk@11298 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/avocr.sh
+++ target/archivista/rootfs/home/archivista/avocr.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+tmp=`mktemp`
+
 cd ~/.wine/drive_c/Program\ Files/Av5e
-wine avocr $*
+
+# run wine in the background, to display a dialog if the fonts are processed
+wine avocr $* 2> $tmp &
+
+sleep 1
+
+if grep -q "Font metrics" $tmp; then
+	Xdialog --no-cancel --infobox \
+"The OCR is analyzing the system fonts. It may take up to
+30 seconds until the OCR dialog appears." 8 48 10000
+fi
+
+rm $tmp
 
