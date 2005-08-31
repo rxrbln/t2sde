c3be1fc3a1c3eda783376c3fe85f15d5f66c05aa
René Rebe <rene@t2-project.org>
2005-08-31 18:10:00 +0000

    * added CD creation date injection to target/archivista (untested)
    git-svn-id: https://svn.exactcode.de/t2/trunk@11447 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/build_image.sh
+++ target/archivista/build_image.sh
@@ -22,6 +22,10 @@ inject_archivista()
 	# wine config and archivista "rich-client" installation
 	tar xvfz $base/target/$target/wine-archivista.tar.gz
 
+	# inject versions we do not have immediate control over
+	sed -i "s/^\(title.*label.*\)/\1 - $build_date/" $imagelocation/etc/stone.d/mod_grub.sh
+	sed -i "s/^\(T2.*\)/\1 - $build_date/" $imagelocation/etc/VERSION
+
 	popd
 }
 
