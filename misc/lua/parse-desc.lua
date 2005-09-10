8b1a8b7c6e6b97699ee588d57cee234c490e4bbf
Valentin Ziegler <valentin@t2-project.org>
2005-09-10 11:24:59 +0000

    * created a t2_desc package containing the desc parsing facillitys from parse-desc.lua 	* rewrote parse-desc to use the functions from this package instead
    git-svn-id: https://svn.exactcode.de/t2/trunk@11766 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/lua/parse-desc.lua
+++ misc/lua/parse-desc.lua
@@ -8,6 +8,8 @@
 -- time time grep "^[[]" package/base/*/*.desc > /dev/null
 --
 
+require "t2_desc"
+
 if #arg < 1 then
    print("Usage: lua misc/lua/parse-desc.lua [path-to-desc-file]")
    os.exit(1)
@@ -17,50 +19,6 @@ function printf(...)
    io.write(string.format(unpack(arg)))
 end
 
--- parse PKG-DESC-FORMAT
-pkg_desc_format = {}
-for line in io.open("misc/share/PKG-DESC-FORMAT"):lines() do
-   local required=false
-   local tag
-
-   if string.match(line, "^[[]") ~= nil then
-      -- check if tag is required
-      if string.match(line, "([*])") ~= nil then required=true; end
-
-      -- use last tag definition as name
-      for t in string.gfind(line,"[[]([^]]*)[]]") do tag = t; end
-      tag = string.lower(tag)
-
-      -- sort into pkg_desc_format
-      for t in string.gfind(line,"([[][^]]*[]])") do 
-	 pkg_desc_format[t] = { name = tag; required = required }
-      end
-   end
-end
-
--- parse .desc file
-function parse_desc(fname)
-   local retval = {}
-
-   printf("Parsing %s...\n", fname)
-
-   local desc = io.open(fname):lines();
-
-   for line in desc do
-      local tag,cnt
-
-      _,_,tag,cnt = string.find(line, "([[][^]]*[]])[ ]+(.*)")
-      if tag then
-	 local fmt = pkg_desc_format[tag]
-	 if fmt then
-	    retval[fmt.name] = retval[fmt.name] or {}
-	    table.insert(retval[fmt.name], cnt)
-	 end
-      end
-   end
-
-   return retval
-end
 
 -- parse all files
 pkgs = {}
@@ -69,7 +27,7 @@ for i,file in ipairs(arg) do
       _,_,repo,pkg = string.find(file, "package/([^/]*)/([^/]*)/*");
 
       -- put all parsed files into a table
-      pkgs[pkg] = parse_desc(file)
+      pkgs[pkg] = t2_desc.parse_file(file)
    end
 end
 
