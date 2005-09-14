65507a1f837fb97f7f946cae325ed9ed6d945ca3
René Rebe <rene@t2-project.org>
2005-09-14 19:10:37 +0000

    * fixed archivista master - slave setup, including syncing the correct 	  data, setting, revoking the permissions, making sure mysqld is dead 	  and last but not least displaying a big warning that all data will 	  be lost on the slave box ...
    git-svn-id: https://svn.exactcode.de/t2/trunk@11866 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/db-master-disable.sh
+++ target/archivista/rootfs/home/archivista/db-master-disable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$UID" -ne 0 ]; then
-	exec gnomesu -t "Disable database master mode" \
+	exec gnomesu -p -t "Disable database master mode" \
 	-m "Please enter the system password (root user)^\
 in order to disable the database master mode." -c $0
 fi
@@ -9,8 +9,34 @@ fi
 # PATH and co
 . /etc/profile
 
+# get slave ip
+until [ "$slaveip" ]; do
+        tslaveip=`Xdialog --stdout --inputbox \
+        "Enter IP or hostname of slave server:" 10 38 $tslaveip` || exit
+
+        if ! ping -c 1 $tslaveip ; then
+                Xdialog --infobox 'Slave not answering (pings)!' 8 28
+        else
+                slaveip=$tslaveip
+        fi
+done
+
+# get account name
+
+user=""
+until [ "$user" ]; do
+        user=`Xdialog --stdout --inputbox \
+              "Name used for the replication account:" 10 38` || exit
+done
+
 sed -i -e "s/^log-bin$/# log-bin/" \
        -e "s/^max-binlog-size.*/# max-binlog-size = 300M/" /etc/my.cnf
+
+mysql -u root -p $PASSWD -h localhost <<-EOT
+revoke all on *.* from $user@$slaveip
+flush privileges
+EOT
+
 
 rc mysql restart
 
