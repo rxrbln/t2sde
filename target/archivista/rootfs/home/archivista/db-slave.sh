b00a919125d5a65269e86ad4e93fe65c26f38916
René Rebe <rene@t2-project.org>
2005-09-14 19:10:37 +0000

    * fixed archivista master - slave setup, including syncing the correct 	  data, setting, revoking the permissions, making sure mysqld is dead 	  and last but not least displaying a big warning that all data will 	  be lost on the slave box ...
    git-svn-id: https://svn.exactcode.de/t2/trunk@11866 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/db-slave.sh
+++ target/archivista/rootfs/home/archivista/db-slave.sh
@@ -22,6 +22,10 @@ until [ "$masterip" ]; do
 	fi
 done 
 
+Xdialog --yesno "Enabling slave mode will erase
+the current database!
+Are you sure you want to proceed?" 10 38 || exit
+
 user=""
 until [ "$user" ]; do
         user=`Xdialog --stdout --inputbox \
@@ -35,12 +39,16 @@ until [ "$passwd" ]; do
 done
 
 rc mysql stop
+sleep 2
+killall mysqld
+sleep 2
+killall -9 mysqld
 
 # copy the db and perform other needed tasks on the master
 echo "Please enter the master server system (root user) password in order to
 copy the initial database:"
-rsync -arve ssh $masterip:/home/data/archivista/mysql \
-                          /home/data/archivista/mysql
+rsync -arve ssh --delete $masterip:/home/data/archivista/mysql \
+                                   /home/data/archivista/
 error=$?
 
 if [ $error -ne 0 ]; then
