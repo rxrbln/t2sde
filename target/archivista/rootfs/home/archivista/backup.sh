ba887a0ea1bd2f39b19c2ebfe7a44991b713648a
René Rebe <rene@t2-project.org>
2005-09-16 14:52:55 +0000

    * hide mysql stop/start in archivista/backup.sh, since the escape 	  sequences leave ugly garbage in the Xdialog, rescan for scsi 	  devices in the same script
    git-svn-id: http://svn.exactcode.de/t2/trunk@11906 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/backup.sh
+++ target/archivista/rootfs/home/archivista/backup.sh
@@ -13,8 +13,9 @@ fi
 export DISPLAY=:0
 
 (
-	rc mysql stop
+	rc mysql stop > /dev/null
+	/home/archivista/rescan-scsi-bus.sh
 	flexbackup -set all 2>&1
-	rc mysql start
+	rc mysql start > /dev/null
 ) | Xdialog --no-cancel --log - 20 60
 
