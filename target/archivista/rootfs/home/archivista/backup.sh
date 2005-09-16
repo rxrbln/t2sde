a923d15037390073b0cdaec30ed093891c15cb4f
René Rebe <rene@t2-project.org>
2005-09-16 15:02:09 +0000

    * rewind and eject the tape after a backup in archivista/backup.sh
    git-svn-id: https://svn.exactcode.de/t2/trunk@11908 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/backup.sh
+++ target/archivista/rootfs/home/archivista/backup.sh
@@ -16,6 +16,7 @@ export DISPLAY=:0
 	rc mysql stop > /dev/null
 	/home/archivista/rescan-scsi-bus.sh
 	flexbackup -set all 2>&1
+	mt -f /dev/nst0 rewoffl
 	rc mysql start > /dev/null
 ) | Xdialog --no-cancel --log - 20 60
 
