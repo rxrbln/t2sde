69a806b1390dd9245063b9bdc102900d3ac73097
René Rebe <rene@t2-project.org>
2005-09-18 17:35:24 +0000

    * renamed archivista/network-status.sh to status.sh, adapted the menu 	  and added more info like: SSH running?, mysql replication mode, 	  backup enabled?, ...
    git-svn-id: https://svn.exactcode.de/t2/trunk@11964 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/status.sh
+++ target/archivista/rootfs/home/archivista/status.sh
@@ -20,6 +20,26 @@ ethtool eth0 >> $tmp
 
 	grep -e Speed -e Duplex -e "Link detect" $tmp
 
+	if ps ax | grep -q sshd; then
+		echo "Remote access (SSH) active"
+	else
+		echo "Remote access (SSH not active"
+	fi
+
+	read m h junk  < <(grep "archivista.backup" /etc/crontab)
+	if [ "$h" -a "$m" ]; then
+		echo "Tape Backup enabled at $h:$m"
+	else
+		echo "Tape Backup not enabled"
+	fi
+
+	read junk junk id < <(grep 'server-id' /etc/my.cnf)
+	case $id in
+	1) echo "Database in master mode" ;;
+	2) echo "Database in slave mode" ;;
+	*) echo "Databse in normal mode" ;;
+	esac
+
 	echo
 	cat /etc/VERSION
 ) | sed -e 's/^[[:space:]]\+//' -e 's/inet /Inet /' -e 's/HWaddr /HWaddr:/' \
