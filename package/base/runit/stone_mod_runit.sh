7762bcb4ad695abb18e3d9714fd209f707382f5e
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-09-22 02:23:48 +0000

    * reworked runit/stone_mod_runit.sh to detect the whole status menu on a sigle call to runsvstat
    git-svn-id: https://svn.exactcode.de/t2/trunk@12054 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/runit/stone_mod_runit.sh
+++ package/base/runit/stone_mod_runit.sh
@@ -18,9 +18,9 @@ runit_svctrl_menu() {
 
 				actions="$actions 'service: $dir' '' \
 					'  stats: $( $COMMANDDIR/runsvstat $dir/ 2> /dev/null | cut -d' ' -f2- )' ''"
-				actions="$actions 'runsvctrl $name u (up)'   '$COMMANDDIR/runsvctrl u $dir; sleep 1'"
-				actions="$actions 'runsvctrl $name d (down)' '$COMMANDDIR/runsvctrl d $dir; sleep 1'"
-				actions="$actions 'runsvctrl $name h (hup)'  '$COMMANDDIR/runsvctrl h $dir; sleep 1'"
+				actions="$actions 'runsvctrl u $name (up)'   '$COMMANDDIR/runsvctrl u $dir; sleep 1'"
+				actions="$actions 'runsvctrl d $name (down)' '$COMMANDDIR/runsvctrl d $dir; sleep 1'"
+				actions="$actions 'runsvctrl h $name (HUP)'  '$COMMANDDIR/runsvctrl h $dir; sleep 1'"
 			fi
 		done
 		eval "gui_menu runit_sc_menu '$service -> $location' $actions"
@@ -28,19 +28,34 @@ runit_svctrl_menu() {
 	done
 }
 
-main() {
-	local errno=0 dir= service=
-	local stats= logstats=
-	while [ $errno -eq 0 ]; do
-		local installed=
-		for dir in $SERVICEDIR/*; do
-			service=${dir##*/}
-			stats=$( $COMMANDDIR/runsvstat $dir/ 2> /dev/null | cut -d' ' -f2,5- )
-			logstats=$( $COMMANDDIR/runsvstat $dir/log/ 2> /dev/null | cut -d' ' -f2,5- )
-			installed="$installed '$service [$stats]${logstats:+ log/ [$logstats]}' 'runit_svctrl_menu $dir'"
-		done
+runit_svstat() {
+	local dir= stat=
+	local logof= logstat=
+	local service=
 
-        	eval "gui_menu runit 'Simple $SERVICEDIR Manager' $installed"
-		errno=$?
+	$COMMANDDIR/runsvstat $* | sort |
+		sed -n -e 's,^\(.*\): \([^(]*\)\( (.*) \)\?\([^)]*\)$,dir="\1" stats="\2 \4",p' |
+		while read line; do
+			dir= stats=
+			eval "$line"
+
+			if [[ $dir = */log ]] ; then
+				logof=${dir%/log}
+				logstat=${stats}
+			else
+				service=${dir##*/}
+				if [ "$dir" == "$logof" ]; then
+					echo "'$service [$stats] log: [$logstat]' 'runit_svctrl_menu $dir'"
+				else
+					echo "'$service [$stats]' 'runit_svctrl_menu $dir'"
+				fi
+				logof=
+			fi
 	done
+}
+
+main() {
+	while eval "gui_menu runit 'Simple $SERVICEDIR Manager' \
+		$( runit_svstat /service/*{,/log} | tr '\n' ' ' )"; do
+		:; done
 }
