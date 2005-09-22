d388568729dac0a56c15af32cc2036421dcd5667
René Rebe <rene@t2-project.org>
2005-09-22 15:00:29 +0000

    * fixed archivista/db-slave-normal.sh to display normal, not master mode
    git-svn-id: http://svn.exactcode.de/t2/trunk@12073 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/db-slave-master.sh
+++ target/archivista/rootfs/home/archivista/db-slave-master.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 if [ "$UID" -ne 0 ]; then
-	exec gnomesu -t "Database slave -> master mode" \
+	exec gnomesu -t "Database slave -> normal mode" \
 	-m "Please enter the system password (root user)^\
-in order to bring the database from slave into master mode." \
+in order to bring the database from slave into normal mode." \
 	-c $0
 fi
 
 # PATH and co
 . /etc/profile
 
-
 rc mysql stop
 
-# bring into slave mode
+# bring into normal mode
 sed -i -e "s/.*server-id.*/server-id = 1/" \
        -e "s/.*log-bin$/#log-bin/" /etc/my.cnf
 
