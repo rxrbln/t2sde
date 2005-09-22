0660e39faa5c6e91b515549e7347c84fdbd9f605
Alejandro Mery Pellegrini <amery@geeks.cl>
2005-09-22 18:25:38 +0000

    * reworked runit/stone_mod_runit.sh to use arrays 	* improved stone mod_runit to show services available for 'installation'
    git-svn-id: https://svn.exactcode.de/t2/trunk@12080 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/runit/stone_mod_runit.sh
+++ package/base/runit/stone_mod_runit.sh
@@ -4,8 +4,40 @@ SYSCONFDIR=/etc
 SERVICEDIR=/service
 COMMANDDIR=/command
 
+declare -a runit_installed
+declare -a runit_available
+
+runit_audit() {
+	local entry= available= count=
+
+	entry=0
+	while read service; do
+		runit_available[$entry*3+0]="${service##*/}" # service name
+		runit_available[$entry*3+1]="$service" # service's real location
+		runit_available[$entry*3+2]= # installed
+		(( entry++ ))
+	done < <( find $SYSCONFDIR -type f -name run | sort | sed -e '/\/log\/run$/d;' -e 's,/run$,,g' )
+	entry=0
+	(( count=${#runit_available[@]}/3 ))
+	while read service; do
+		runit_installed[$entry*3+0]="${service##*/}" # service name
+		runit_installed[$entry*3+1]="$( readlink -f "$service" )" # real location
+		runit_installed[$entry*3+2]=
+		available=0
+		while [ $available -lt $count ]; do
+			if [ ${runit_available[$available*3+1]} = ${runit_installed[$entry*3+1]} ]; then
+				runit_available[$available*3+2]=$entry
+				runit_installed[$entry*3+2]=$available
+				break
+			fi
+			(( available++ ))
+		done
+		(( entry++ ))
+	done < <( find $SERVICEDIR -maxdepth 1 -type l | sort )
+}
+
 # u|d|o|p|c|h|a|i|q|1|2|t|k|x|e
-runit_svctrl_menu() {
+runit_svc() {
 	local service=$1 errno=0 name= dir=
 	local prefix=${service%/*} location=$(readlink -f $service)
 
@@ -28,34 +60,52 @@ runit_svctrl_menu() {
 	done
 }
 
-runit_svstat() {
-	local dir= stat=
-	local logof= logstat=
-	local service=
-
-	$COMMANDDIR/runsvstat $* | sort |
-		sed -n -e 's,^\(.*\): \([^(]*\)\( (.*) \)\?\([^)]*\)$,dir="\1" stats="\2 \4",p' |
-		while read line; do
-			dir= stats=
-			eval "$line"
-
-			if [[ $dir = */log ]] ; then
-				logof=${dir%/log}
-				logstat=${stats}
-			else
-				service=${dir##*/}
-				if [ "$dir" == "$logof" ]; then
-					echo "'$service [$stats] log: [$logstat]' 'runit_svctrl_menu $dir'"
-				else
-					echo "'$service [$stats]' 'runit_svctrl_menu $dir'"
-				fi
-				logof=
-			fi
-	done
+runit_install() {
+	true
 }
 
 main() {
-	while eval "gui_menu runit 'Simple $SERVICEDIR Manager' \
-		$( runit_svstat /service/*{,/log} | tr '\n' ' ' )"; do
-		:; done
+	local errno=0
+	local entry= count=
+
+	runit_audit
+	while [ $errno -eq 0 ]; do
+		local available= installed=
+		local text= action=
+		local i= stats=
+		declare -a stats
+		(( count=${#runit_installed[@]}/3 )); entry=0
+		while [ $entry -lt $count ]; do
+			text="${runit_installed[$entry*3+0]}"
+			action="runit_svc $SERVICEDIR/${runit_installed[$entry*3+0]} ${runit_installed[$entry*3+1]}"
+			stats[0]=; stats[1]=; i=0
+			while read stats[i++]; do :; done < <( \
+				$COMMANDDIR/runsvstat \
+				"${runit_installed[$entry*3+1]}" \
+				"${runit_installed[$entry*3+1]}"/log 2> /dev/null | \
+				sed -n -e 's,^.*: \([^(]*\)\( (.*) \)\?\([^)]*\)$,\1 \3,p' )
+			[ "${stats[0]}" ] && text="$text [${stats[0]}]" || text="$text [ERROR]"
+			[ "${stats[1]}" ] && text="$text log: [${stats[1]}]"
+			[ "${runit_installed[$entry*3+2]}" ] || text="$text [FOREIGN]"
+			installed="$installed '$text' '$action'"
+			(( entry++ ))
+		done
+		(( count=${#runit_available[@]}/3 )); entry=0
+		while [ $entry -lt $count ]; do
+			if [ -z "${runit_available[$entry*3+2]}" ]; then
+				text="${runit_available[$entry*3+0]} (${runit_available[$entry*3+1]})"
+				if [ -e "$SERVICEDIR/${runit_available[$entry*3+0]}" ]; then
+					text="$text [BLOCKED]"
+					action=
+				else
+					action="runit_install ${runit_available[$entry*3+1]}"
+				fi
+				available="$available '$text' '$action'"
+			fi
+			(( entry++ ))
+		done
+		
+		eval "gui_menu runit 'Simple $SERVICEDIR Manager' $installed '' '' $available"
+		errno=$?
+	done
 }
