710a4d2a440371c5ce5603a492cd9b0e2f2062aa
René Rebe <rene@t2-project.org>
2005-10-09 18:05:04 +0000

    * mostly functional archivista/net-backup.sh - setup is mostly missing
    git-svn-id: https://svn.exactcode.de/t2/trunk@12440 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/net-backup-restore.sh
+++ target/archivista/rootfs/home/archivista/net-backup-restore.sh
@@ -6,23 +6,36 @@ if [ "$UID" -ne 0 ]; then
 in order to restore the network backup." -c $0
 fi
 
-mkdir -p /mnt/net
+# PATH and co
+. /etc/profile
+
+# Xdialog and friends
+export DISPLAY=:0
+
+# include shared code
+. ${0%/*}/net-backup.in
 
 log=`mktemp`
 (
-	rc mysql stop
+	# shared function, included on top
+	mount_net /mnt/net || exit
+
+	rc mysql stop > /dev/null
+	sleep 5 # TODO: fix mysql init stop
+
 	mkdir -p /home/data
-	rm -rf /home/data/*
+	# rm -rf /home/data/* # we skip this due rsync -
+	                      # maybe someone wants this?
 	cd /home/data
 
-	# mount -t $type $from /mnt/net $options
-
 	# no -a since we can not store user/group on most CIFS shares
-	# rsync -rv /home/data /mnt/net/
+	rsync -rvt --delete /mnt/net/data/ /home/data/
 
-	# umount /mnt/net
+	# TODO: permission fixup, since not backed up
 
-	rc mysql start
+	umount /mnt/net
+
+	rc mysql start > /dev/null
 ) > $log 2>&1
 
 Xdialog --no-cancel --log - 20 60 < $log
