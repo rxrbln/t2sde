a59b659c8739928eb4a464256911ac14d789b450
René Rebe <rene@t2-project.org>
2005-10-09 20:32:41 +0000

    * implemented the missing pieces of archivista/net-backup-setup.sh and 	  adapted the shared code in net-backup.in for a syntax change
    git-svn-id: http://svn.exactcode.de/t2/trunk@12444 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/net-backup-setup.sh
+++ target/archivista/rootfs/home/archivista/net-backup-setup.sh
@@ -9,8 +9,6 @@ fi
 # PATH and co
 . /etc/profile
 
-set -x
-
 # include shared code
 . ${0%/*}/backup-setup.in /home/archivista/net-backup.sh
 
@@ -24,10 +22,40 @@ fi
 
 get_time
 
+# maybe previous values ...
+[ -e /etc/net-backup.conf ] && . /etc/net-backup.conf
+
 # type: cifs / nfs / ...
+
+type=`Xdialog --stdout --combobox "Protocol used to access the remote server.
+CIFS stands for Common Internet Filesystem (formerly
+known as SMB) is used on Microsoft servers, where
+NFS is usually used in Unix environments." 0 0 CIFS NFS` || exit
 
 # server / user / pass / ...
 
+server=`Xdialog --stdout --inputbox "Server IP (or hostname):" 0 0 $server` || exit
+
+if [ $type = CIFS ]; then
+	share=`Xdialog --stdout --inputbox "Share:" 0 0 $share` || exit
+	user=`Xdialog --stdout --inputbox "User account:" 0 0 $user` || exit
+	passwd=`Xdialog --stdout --passwordbox "Password:" 0 0 $passwd` || exit
+	domain=`Xdialog --stdout --cancel-label=None --inputbox "Domain:" 0 0 $domain`
+# else # NFS specific stuff
+fi
+
+# save config for the net-backup.sh script
+(
+  echo "type=$type"
+  echo "server=$server"
+  if [ $type = CIFS ]; then
+	[ "$share" ] && echo "share=$share"
+	[ "$user" ] && echo "user=$user"
+	[ "$passwd" ] && echo "passwd=$passwd"
+	[ "$domain" ] && echo "domain=$domain"
+  fi
+) > /etc/net-backup.conf
+chmod 400 /etc/net-backup.conf # hide passwords from users
+
 crontab_add /home/archivista/net-backup.sh $h $m $days
-rc cron restart
 
