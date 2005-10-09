18909a8da71c44ae033904828dac1e30f13b9856
René Rebe <rene@t2-project.org>
2005-10-09 19:32:52 +0000

    * factored out common code to configure the backup (date, time, adapt 	  crontab) - unteseted - target/archivista
    git-svn-id: http://svn.exactcode.de/t2/trunk@12441 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/net-backup-setup.sh
+++ target/archivista/rootfs/home/archivista/net-backup-setup.sh
@@ -9,55 +9,23 @@ fi
 # PATH and co
 . /etc/profile
 
-# read previous settings
-line=`grep "archivista.*/net-backup.sh" /etc/crontab`
-tdays=`echo "$line" | cut -d ' ' -f 5`
-read m h < <(echo "$line" | cut -d ' ' -f 1-2)
-ttime="$h:$m"
+# include shared code
+. ${0%/*}/backup-setup.in
 
-[ "$tdays" ] || tdays="2-6"
-[ "$ttime" = ":" ] && ttime="2:00"
-
-until [ "$days" ]; do
-	tdays=`Xdialog --stdout --inputbox "Days the backup should be run on.
-Allowed are ranges from 1 (Monday) to
-7 (Sunday) (e.g. 2-6) and 0 to disable the backup:" 10 48 "$tdays"` || exit
-	# remove spaces ...
-        tdays="${tdays// /}"
-        # remove invalid content
-        if echo "$tdays" | grep -q "^\([1-7]\+-[1-7]\+\|[0-7]\+\)$" ; then
-		days=$tdays
-	else
-		Xdialog --infobox "Range not valid!" 8 28
-	fi
-done
+get_days
 
 # disable?
 if [ "$tdays" = 0 ]; then
-	# remove previous line ...
-	sed -i "/archivista.*\/backup.sh/d" /etc/crontab
-	rc cron restart
+	crontab_remove /home/archivista/net-backup.sh
 	exit
 fi
 
-until [ "$time" ]; do
-	ttime=`Xdialog --stdout --inputbox "Time the backup should be run on
-the specified days (e.g. 2:30):" 10 40 "$ttime"` || exit
-	read h m < <( echo ${ttime/:/ } )
-	echo "'$h' '$m'"
-	if [ $h -le 24 -a $m -le 60 ]; then
-		time=$ttime
-	else
-		Xdialog --infobox "Time not valid!" 8 28
-	fi
-done
+get_time
 
 # type: cifs / nfs / ...
 
 # server / user / pass / ...
 
-# remove previous line and add new entry ...
-sed -i "/archivista.*\/net-backup.sh/d" /etc/crontab
-echo "$m $h * * $days root /home/archivista/net-backup.sh" >> /etc/crontab
+crontab_add /home/archivista/net-backup.sh $h $m $days
 rc cron restart
 
