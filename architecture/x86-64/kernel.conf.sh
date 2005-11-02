f9ddaec8326ee0c8aa9e2b89d55056ce57cd339f
René Rebe <rene@t2-project.org>
2005-11-02 13:30:10 +0000

    * fixed x86-64 kernel configuraiton to not be a x86 copy but set 	  x86-64 options instead - added Intel EM64T optimization options 	  (untested)
    git-svn-id: https://svn.exactcode.de/t2/trunk@13099 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- architecture/x86-64/kernel.conf.sh
+++ architecture/x86-64/kernel.conf.sh
@@ -20,41 +20,19 @@
 		dnl
 	EOT
 
-	linux_arch=386
-	for x in "i386		386"		\
-		 "i486		486"		\
-		 "via-c3	MCYRIXIII"	\
-		 "via-c3-2	MVIAC3_2"	\
-		 "pentium	586"		\
-		 "pentium-mmx	586MMX"		\
-		 "pentiumpro	686"		\
-		 "pentium2	PENTIUMII"	\
-		 "pentium3	PENTIUMIII"	\
-		 "pentium4	PENTIUM4"	\
-		 "k6		K6"		\
-		 "k6-2		K6"		\
-		 "k6-3		K6"		\
-		 "athlon	K7"		\
-		 "athlon-tbird	K7"		\
-		 "athlon4	K7"		\
-		 "athlon-xp	K7"		\
-		 "athlon-mp	K7"
+	linux_arch=MK8
+	for x in "generic	GENERIC_CPU"	\
+		 "em64t		MPSC"
 	do
 		set $x
-		[ "$1" = "$SDECFG_X86_OPT" ] && linux_arch=$2
+		[ "$1" = "$SDECFG_X8664_OPT" ] && linux_arch=$2
 	done
 
-	# echo `grep -A 20 'Processor family' \
-	#	/usr/src/linux/arch/i386/config.in | expand | \
-	#	cut -c 57- | cut -f1 -d' ' | tr -d '"'`
-	#
-	for x in 386 486 586 586TSC 586MMX 686 PENTIUMIII PENTIUM4 \
-	         K6 K7 K8 ELAN CRUSOE WINCHIPC6 WINCHIP2 WINCHIP3D \
-	         CYRIXIII VIAC3_2
+	for x in GENERIC_CPU MK8 MPSC
 	do
 		if [ "$linux_arch" != "$x" ]
-		then echo "# CONFIG_M$x is not set"
-		else echo "CONFIG_M$x=y" ; fi
+		then echo "# CONFIG_$x is not set"
+		else echo "CONFIG_$x=y" ; fi
 	done
 
 	echo
@@ -63,11 +41,6 @@
 		dnl (improvements in graphic speed ...)
 		dnl
 		CONFIG_MTRR=y
-
-		dnl Some AGP support not enabled by default
-		dnl
-		CONFIG_AGP_AMD_8151=y
-		CONFIG_AGP_SWORKS=y
 
 		dnl PC Speaker for 2.5/6 kernel
 		CONFIG_INPUT_PCSPKR=y
