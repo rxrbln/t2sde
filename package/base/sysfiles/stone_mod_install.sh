939d3cb94ad39708f465b8265fc3bba17818abfa
René Rebe <rene@t2-project.org>
2005-12-23 22:41:28 +0000

    * implemented skipping of cdroms in the new u/dev aware stone install 	  module (untested)
    git-svn-id: https://svn.exactcode.de/t2/trunk@14154 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sysfiles/stone_mod_install.sh
+++ package/base/sysfiles/stone_mod_install.sh
@@ -171,6 +171,7 @@ This dialog allows you to modify your discs parition layout and to create filesy
 		# protect for the case no discs are present ...
 		found=0
 		for x in $( cd /dev/; ls hd[a-z] sd[a-z] 2> /dev/null ); do
+			grep -q cdrom /proc/ide/$x/media 2>/dev/null && continue
 			disk_add $x
 			found=1
 		done
