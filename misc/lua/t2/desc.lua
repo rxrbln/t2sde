8c6f1b40c1a161781409d2b3f0354c8c208cb50b
Juergen "George" Sawinski <jsaw@gmx.net>
2005-12-23 22:30:32 +0000

    * add some more notes to lua files 	* only take a line-iterator for the pkgdb parser 	  (no use of zlib directly, 'cause we may use lzo later?)
    git-svn-id: https://svn.exactcode.de/t2/trunk@14151 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/lua/t2/desc.lua
+++ misc/lua/t2/desc.lua
@@ -1,16 +1,23 @@
 -- Copyright (C) 2005 Juergen "George" Sawinski
 -- Licensed under the GPL, see end of file
 
--- Description:
---   FIXME
+-- TODO:
+--   -in desc.parse, check if required fields were set
 
--- package object
-t2 = t2 or {}
-t2.desc = t2.desc or {}
-t2.desc.__format__ = {}
+-- DESCRIPTION:
+--   t = desc.parse(line-iterator)
+--     Parse ".desc" file format by passing a line iterator and
+--     return a table, where lower-case of last entry in
+--       PKG-DESC-FORMAT
+--     is the key. Example:
+--     Parsing "[I] Title" returns `{ title = "Title" }'
+
+-- package object structure
+desc = desc or {}
+desc.__format__ = {}
 
 -- parse .desc text ; expects line iterator function as argument
-local function parse(desc)
+function desc.parse(desc)
 	local retval = {}
 
 	for line in desc do
@@ -18,7 +25,7 @@ local function parse(desc)
 
 		_,_,tag,cnt = string.find(line, "([[][^]]*[]])[ ]+(.*)")
 		if tag then
-			local fmt = t2.desc.__format__[tag]
+			local fmt = desc.__format__[tag]
 			if fmt then
 				retval[fmt.name] = retval[fmt.name] or {}
 				table.insert(retval[fmt.name], cnt)
@@ -46,13 +53,10 @@ for line in io.open("misc/share/PKG-DESC-FORMAT"):lines() do
 
 		-- sort into __format__
 		for t in string.gfind(line,"([[][^]]*[]])") do 
-			t2.desc.__format__[t] = { name = tag; required = required }
+			desc.__format__[t] = { name = tag; required = required }
 		end
 	end
 end
-
--- insert methods
-t2.desc.parse = parse;
 
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -66,4 +70,4 @@ t2.desc.parse = parse;
 
 -- You should have received a copy of the GNU General Public License
 -- along with this program; if not, write to the Free Software
--- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
