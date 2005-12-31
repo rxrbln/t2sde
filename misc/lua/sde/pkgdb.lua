fb865c46a4e84397cb5d012383bee54e8b631cba
Juergen "George" Sawinski <jsaw@gmx.net>
2005-12-31 18:09:59 +0000

    * WIP (need to transfer to different computer): 	  cleanup, reindentation, commenting
    git-svn-id: http://svn.exactcode.de/t2/trunk@14368 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- misc/lua/sde/pkgdb.lua
+++ misc/lua/sde/pkgdb.lua
@@ -10,70 +10,77 @@
 --   p = pkgdb.parse(line-iterator)
 --     Parse the package.db (takes a line iterator as input)
 
-require "t2/desc"
+require "sde/desc"
 
 -- parse all packages.db information into tables
 -- filelist saving commented out (eats another 30M)
 
 local function block_lines()
-   local line = lines();
-   if (line == nil) or (line=="\023") then
-      return nil;
-   end
-   return line;
+	local line = lines()
+
+	if (line == nil) or (line=="\023") then
+		return nil
+	end
+
+	return line
 end
 
 local function read_deps()
-   local deps={}
-   for line in block_lines do
-      _,_,dependency = string.find(line, "[^ ]* (.*)");
-      table.insert (deps, dependency);
-   end
-   return deps;
+	local deps={}
+
+	for line in block_lines do
+		_,_,dependency = string.find(line, "[^ ]* (.*)")
+		table.insert (deps, dependency)
+	end
+
+	return deps
 end
 
 local function read_flist()
-   local files={};
-   local cksums={};
-   local sizes={};
-   local usage=0;
-   for line in block_lines do
-      _,_,cksum,size,file = string.find(line, "^([0-9]+) ([0-9]+) (.*)");
---    uncomment theese lines if you want to save complete file list
---      table.insert (files, file);
---      table.insert (cksums, 1 * cksum);
---      table.insert (sizes, 1 * size);
-      usage = usage + size;
-   end
-   return usage,files,cksums,sizes;
+	local files={}
+	local cksums={}
+	local sizes={}
+	local usage=0
+
+	for line in block_lines do
+		_,_,cksum,size,file = string.find(line, "^([0-9]+) ([0-9]+) (.*)")
+		-- uncomment these lines if you want to save complete file list
+		--      table.insert (files, file);
+		--      table.insert (cksums, 1 * cksum);
+		--      table.insert (sizes, 1 * size);
+		usage = usage + size
+	end
+
+	return usage,files,cksums,sizes
 end
 
-local parse(lines)
-   packages = {};
+local function parse(lines)
+	packages = {}
 
-   repeat                  -- parse packages
-      pkgname = lines();
-      if pkgname then
-	 print(pkgname);
-	 local pkg_data = {};
+	repeat                  -- parse packages
+		pkgname = lines()
 
-	 if lines() ~= "\023" then -- separator line
-	    print ("terminating line missing\n");
-	 end
+		if pkgname then
+			print(pkgname)
+			local pkg_data = {}
 
-	 pkg_data.desc=desc.parse (block_lines);
-	 pkg_data.deps=read_deps ();
-	 pkg_data.usage = read_flist ();
+			if lines() ~= "\023" then -- separator line
+				print ("terminating line missing\n")
+			end
 
-	 if lines() ~= "\004" then -- separator line
-	    print ("terminating line missing\n");
-	 end
-	 
-	 packages[pkgname] = pkg_data;
-      end
-   until pkgname == nil;
+			pkg_data.desc  = desc.parse (block_lines)
+			pkg_data.deps  = read_deps ()
+			pkg_data.usage = read_flist ()
 
-   return packages
+			if lines() ~= "\004" then -- separator line
+				print ("terminating line missing\n")
+			end
+
+			packages[pkgname] = pkg_data
+		end
+	until pkgname == nil
+
+	return packages
 end
 
 -- This program is free software; you can redistribute it and/or modify
