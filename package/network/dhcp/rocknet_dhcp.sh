b7d297f009881e4b08f365a675f5268c44bf250e
René Rebe <rene@t2-project.org>
2006-01-04 14:41:30 +0000

    * fixed dhcp rocknet module to properly flush the ip address when the 	  interface goes down, so it does not interfere with a new one when it is 	  takes up again (reported by Archivista)
    git-svn-id: https://svn.exactcode.de/t2/trunk@14476 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/network/dhcp/rocknet_dhcp.sh
+++ package/network/dhcp/rocknet_dhcp.sh
@@ -15,6 +15,6 @@
 public_dhcp() {
 	addcode up   5 5 "/sbin/dhclient -q $if"
 	addcode down 5 5 "killall -TERM dhclient"
-	addcode down 5 6 "sleep 2 ; ip link set $if down || ifconfig $if down"
+	addcode down 5 6 "sleep 2 ; ip addr flush $if && ip link set $if down || ifconfig $if down"
 }
 
