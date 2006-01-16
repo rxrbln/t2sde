f14b8de0e66b6f11dbf215ccf5ea4ba30986f3f7
Urs Pfister <bluemlisalp@gmx.ch>
2006-01-16 10:29:55 +0000

    VNC remote access modified -- enabling/disabling/starting other box
    git-svn-id: http://svn.exactcode.de/t2/trunk@14842 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/vnc-enable.sh
+++ target/archivista/rootfs/home/archivista/vnc-enable.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if [ "$UID" -ne 0 ]; then
-	exec gnomesu -t "Enable VNC" \
-	-m "Please enter the current system password (root user)^\
-in order to allow graphical remote access (VNC)." -p -c $0
+PASSWD=`Xdialog --password --stdout --inputbox "Password for
+graphical remote access (VNC)" 0 0 $PASSWD` || exit
+
+if [ -z "$PASSWD" ]; then 
+  exit
 fi
 
 . /etc/profile
 
-# -forever does segfault for me after the first iteration
-# later we could loop here ...
-nohup x11vnc -passwd $PASSWD > /var/log/vnc.log 2>&1 &
+x11vnc -forever -passwd $PASSWD &
 
-Xdialog --title "" --msgbox "Graphical remote access (VNC) enabled." 0 0
+
+Xdialog --stdout --msgbox "Graphical remote access (VNC) is enabled" 0 0
+
 
