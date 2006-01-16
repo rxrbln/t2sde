d9c254d8d8ab3063af20831bb4ba56a9b14f63b3
René Rebe <rene@t2-project.org>
2006-01-16 12:43:06 +0000

    * added ifswitch [ interface profile ] | [ profile ], a rocknet fast profile switcher
    git-svn-id: https://svn.exactcode.de/t2/trunk@14846 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ package/network/rocknet/ifswitch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+active=$( cat /var/run/rocknet/active-interfaces )
+
+if [ $# -eq 0 ]; then
+	echo "Usage $0 [ interface profile ] | [ profile ]"
+	exit
+fi
+
+i=0
+for x in ${active//,/ }; do
+	[ $(( i++ )) -eq 0 ] && echo "Deactivating current interfaces ..."
+	if=${x/(*)/}
+	profile=${x/*(/}; profile=${profile%)}
+	ifdown $if $profile
+done
+
+echo "Activating specified profile/interfaces ..."
+
+if [ $# -eq 1 ]; then
+	rocknet $1 auto up
+else
+	ifup $1 $2
+fi
