e89dff42eebf7a6123097d8ca6bfbb13a221ea1a
René Rebe <rene@t2-project.org>
2006-02-18 16:48:14 +0000

    * renamed the archivista button setup variable for readability and 	  implemented toggling the state from the usual button-setup script as well 	  as fixed it to properly exit when cancel is clicked
    git-svn-id: https://svn.exactcode.de/t2/trunk@15556 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/button-setup.sh
+++ target/archivista/rootfs/home/archivista/button-setup.sh
@@ -15,18 +15,36 @@ fi
 # include shared code
 . ${0%/*}/perl-var.in
 
+[ -f /etc/av-button.conf ] && . /etc/av-button.conf
+def=; [ "$av_button" != 1 ] && def="--default-no"
+Xdialog $def --yesno "Enable the scan button?" 0 0 &&
+av_button=1 || av_button=0
+
+if [ "$av_button" != 1 ]; then
+	cat > /etc/av-button.conf <<-EOT
+		av_button=$av_button
+EOT
+	exit
+fi
+
+
 host=`get_perl_var '\$val{host1}' /home/cvs/archivista/jobs/sane-button.pl`
 db=`get_perl_var '\$val{db1}' /home/cvs/archivista/jobs/sane-button.pl`
 user=`get_perl_var '\$val{user1}' /home/cvs/archivista/jobs/sane-button.pl`
 pw=`get_perl_var '\$val{pw1}' /home/cvs/archivista/jobs/sane-button.pl`
 
-host=`Xdialog --stdout --inputbox "Host:" 0 0 $host`
-db=`Xdialog --stdout --inputbox "Database:" 0 0 $db`
-user=`Xdialog --stdout --inputbox "Default user:" 0 0 $user`
-pw=`Xdialog --stdout --passwordbox "Password:" 0 0 $pw`
+host=`Xdialog --stdout --inputbox "Host:" 0 0 $host` || exit
+db=`Xdialog --stdout --inputbox "Database:" 0 0 $db` || exit
+user=`Xdialog --stdout --inputbox "Default user:" 0 0 $user` || exit
+pw=`Xdialog --stdout --passwordbox "Password:" 0 0 $pw` || exit
 
 set_perl_var '\$val{host1}' /home/cvs/archivista/jobs/sane-button.pl "$host"
 set_perl_var '\$val{db1}' /home/cvs/archivista/jobs/sane-button.pl "$db"
 set_perl_var '\$val{user1}' /home/cvs/archivista/jobs/sane-button.pl "$user"
 set_perl_var '\$val{pw1}' /home/cvs/archivista/jobs/sane-button.pl "$pw"
+
+# only enable it when no cancel was hit above
+cat > /etc/av-button.conf <<-EOT
+av_button=$av_button
+EOT
 
