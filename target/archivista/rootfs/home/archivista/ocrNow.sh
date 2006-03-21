7ef34f799f88913be1c931b9dd61b1e017f7ef44
René Rebe <rene@t2-project.org>
2006-03-21 10:57:46 +0000

    Restart ocr and exit check (batch jobs)
    git-svn-id: https://svn.exactcode.de/t2/trunk@16039 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/ocrNow.sh
+++ target/archivista/rootfs/home/archivista/ocrNow.sh
@@ -7,6 +7,50 @@ in order to start the ocr batch job right now." -c $0
 fi
 
 # PATH and co
-# . /etc/profile
+. /etc/profile
 
-/home/cvs/archivista/jobs/ocrNow.pl $PASSWD
+msg="To restart ocr batch job"
+
+get_range ()
+{
+  local x
+  until [ "$x" ]; do
+	tx=`Xdialog --stdout --cancel-label=All --inputbox "$1" 10 50 "$tx"` \
+	   || return
+	# remove spaces ...
+	tx="${tx// /}"
+	# remove invalid content
+	if echo "$tx" | grep -q "^\([0-9]\+-[0-9]\+\|[0-9]\+\)$" ; then
+		x=$tx
+	else
+		Xdialog --infobox "Range not valid!" 8 28
+	fi
+  done
+  echo $x
+}
+
+
+
+get_db()
+{
+  db="archivista"
+  while
+	db=`Xdialog --stdout --no-cancel --inputbox "Database to use:" \
+	    10 40 "$db"`
+	[ -z "$db" ]
+  do
+	:
+  done
+  echo $db
+}
+
+
+
+db=`get_db`
+
+range=`get_range "Range of documents for ocr job (e.g. 2-4 or just 5):"`
+
+Xdialog --yesno "Really restart ocr batch job with range $range " 8 50 || exit
+
+/home/cvs/archivista/jobs/ocrNow.pl $db $range 
+
