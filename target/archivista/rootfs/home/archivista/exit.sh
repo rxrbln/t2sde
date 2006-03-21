2036d3c522f7de703f8426a3d0e720de0532fbe1
René Rebe <rene@t2-project.org>
2006-03-21 10:57:46 +0000

    Restart ocr and exit check (batch jobs)
    git-svn-id: https://svn.exactcode.de/t2/trunk@16039 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/exit.sh
+++ target/archivista/rootfs/home/archivista/exit.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# PATH and co
+. /etc/profile
+
+# Xdialog and friends
+export DISPLAY=:0
+
 if [ "$UID" -ne 0 ]; then
         exec gnomesu -t "Exit" \
         -m "Please enter the system password (root user)^\
 in order to exit." -c $0
 fi
 
-killall fluxbox
+if [ -e /home/archivista/.wine/drive_c/Programs/Av5e/AV5AUTO.WRK ]; then
+  Xdialog --title "" --yesno "There are batch jobes running. \
+  Do you want to stop them?" 0 0 || exit
+	Xdialog --title "Stop ArchivistaBox" --no-buttons --infobox "Please wait some seconds..." 0 0 20000 &
+  /usr/bin/perl /home/cvs/archivista/jobs/stopJobs.pl 20
+  error=$?
+fi
+
+if [ $error -eq 0 ]; then
+  killall fluxbox
+else
+  Xdialog --msgbox "Jobs can't be stopped. Please try it later again." 0 0
+fi
 
