f1625aa54e15b8b26698a04fda8d80765276cad1
Juergen "George" Sawinski <jsaw@gmx.net>
2006-03-24 18:52:50 +0000

    * move sam.lua etal from jsaw/ one level up
    git-svn-id: https://svn.exactcode.de/t2/trunk@16272 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/sam/sam.lua
+++ package/base/sam/sam.lua
@@ -13,63 +13,360 @@
 -- GNU General Public License can be found in the file COPYING.
 -- --- T2-COPYRIGHT-NOTE-END ---
 
-local VERSION='0.0-devel'
-local COPYRIGHT='SAM v.'..VERSION..' Copyright (C) 2006 The T2 SDE Project'
+-- identification
+local _NAME        = "SAM"
+local _VERSION     = "0.0-devel"
+local _COPYRIGHT   = "Copyright (C) 2006 The T2 SDE Project"
+local _DESCRIPTION = "System Administration Manager for systems based on T2"
 
--- get_modules( pattern ) returns the list on modules available at
---                        SAM_MODULES dir for certain pattern
---
-local function get_modules( pattern )
-	local lfs = require('lfs')
-	-- get MODULESDIR from the SAM_MODULES envvar, or use /usr/lib/sam as default
-	local MODULESDIR = os.getenv('SAM_MODULES') or '/usr/lib/sam'
+-- SAM namespace
+sam = sam or {
+	-- logging (callable via metatable, see DESCRIPTION)
+	log = {
+		ERROR  = 0,
+		WARN   = 1,
+		NOTICE = 2,
+		INFO   = 3,
+		DEBUG  = 4,
+	},
+	error  = function(ident,...) sam.log(sam.log.ERROR, ident, unpack(arg)) end,
+	warn   = function(ident,...) sam.log(sam.log.WARN, ident, unpack(arg)) end,
+	notice = function(ident,...) sam.log(sam.log.NOTICE, ident, unpack(arg)) end,
+	info   = function(ident,...) sam.log(sam.log.INFO, ident, unpack(arg)) end,
+	dbg    = function(ident,...) sam.log(sam.log.DEBUG, ident, unpack(arg)) end,
 
-	local list = {}
-	for file in lfs.dir( MODULESDIR ) do
-		module = string.gsub( file, '%..*','' )
-		file   = MODULESDIR .. '/' .. file
-		attrib = lfs.attributes( file )
+	-- command list (extended by modules)
+	command = {} 
+}	
 
-		if attrib.mode == 'file' then
-			if string.match( module, pattern ) then
-				list[ module ] = file
-			end		
+-- default options
+sam.opt = sam.opt or {
+	loglevel = sam.log.DEBUG, -- sam.log.WARN
+}
+
+--[[ DESCRIPTION ] ----------------------------------------------------------
+
+Provided functions:
+
+* sam.log(required-verbosity-level, identification, printf-format...)
+
+    Print information about SAM processing. The logging level (sam.opt.loglevel)
+    has to be equal or higher than "required-verbosity-level". The
+    "identification" is printed in square brackets in front of the message.
+
+    Example:
+      sam.log(sam.log.ERROR, "Config", "Config file incosistency (%s)",
+	          filename)
+
+
+* sam.error(identification, format...)
+* sam.warn(identification, format...)
+* sam.notice(identification, format...)
+* sam.info(identification, format...)
+* sam.dbg(identification, format...)
+
+    Short form for the logging function.
+  
+
+* sam.command["command-name"](args...)
+* sam.command["command-name"].main(args...)
+
+    Execute a command (extended by modulues) with given arguments.
+	The only built-in command currently is "help".
+
+* cli = sam.cli(def)
+
+    Define a CLI command set. The table "def" consist of key->function
+	mappings, e.g. sam.cli { help = cli_cmd_help }. The return value is
+	a CLI object with the following methods:
+
+	cli:run()
+	cli()
+
+	  Start the event loop.
+
+	cli:finish()
+
+	  Leave the event loop.
+
+	cli:send(...)
+
+      Send a message (printf format). A trailing newline is appended
+	  automatically.
+
+	cli:get()
+
+	  Used in the event loop: read (and tokenize) input
+
+* sam.load(ext)
+  
+  Load a SAM specific extension.
+
+  Example:
+    sam.load("tokenize")
+
+--]] ------------------------------------------------------------------------
+
+require "sam/tokenize"
+
+-- printf helpers -----------------------------------------------------------
+
+local function fprintf(stream, ...)
+	stream:write(string.format(unpack(arg)))
+end
+
+-- LOGGING ------------------------------------------------------------------
+
+-- log_stdout(required-log-level, identification, fmt...)
+--   The default logging method is to log to stderr.
+local function log_stdout(reqlvl, ident, ...)
+	if sam.opt.loglevel >= reqlvl then
+		fprintf(io.stderr, "[%s] ", ident)
+		fprintf(io.stderr, unpack(arg))
+	end
+end
+
+-- sam.log(required-log-level, identification, fmt...)
+--   sam.log can be called as function via a metatable,
+--   default: log_stdout
+setmetatable(sam.log, {
+	__call = function(self, reqlvl, ident,  ...) 
+			log_stdout(reqlvl, ident, unpack(arg))
+		 end
+})
+
+-- MODULES ------------------------------------------------------------------
+
+-- load_module(name)
+--   Load the previously detected module.
+local function load_module(name)
+	sam.info(_NAME, "Loading module %s (from %s)\n", name, sam.command[name]._module._FILE)
+
+	-- sanity check for module info
+	if not sam.command[name] or not sam.command[name]._module then
+		sam.error(_NAME, "No such command module '%s', giving up.\n", name)
+		return
+	end
+
+	-- load and execute the module
+	local module = loadfile(sam.command[name]._module._FILE)
+	module = module()
+
+	-- module sanity check
+	if not module.main or not module._NAME then
+		sam.error(_NAME, "Command module '%s' is probably not a SAM module.\n", name)
+		return
+	end
+
+	-- copy module data
+	sam.command[name]._NAME         = module._NAME
+	sam.command[name]._DESCRIPTION  = module._DESCRIPTION
+	sam.command[name]._module.usage = module.usage
+	sam.command[name]._module.main  = module.main
+
+	-- set real methods
+	sam.command[name].help  = function(self) fprintf(io.stdout, "  %16s    %s\n", self._NAME, self._DESCRIPTION) end
+	sam.command[name].usage = function(self) self._module.usage() end
+	sam.command[name].main  = function(self,...) return self._module.main(unpack(arg)) end
+
+	-- set correct metatable
+	setmetatable(sam.command[name], {
+		__call = function(self, ...) return self._module.main(unpack(arg)) end,
+	})
+end
+
+-- detect_modules()
+--   Detect all SAM modules
+local function detect_modules()
+	local lfs = require("lfs")
+	local moddir = os.getenv("SAM_MODULES") or "/usr/lib/sam"
+
+	for file in lfs.dir( moddir ) do
+		local name
+		local path
+
+		_,_,name = string.find(file, "^sam_([%a][_%w%a]*).lua")
+		path = moddir .. "/" .. file
+
+		if name and lfs.attributes(path).mode == "file" and "sam_" .. name .. ".lua" == file then
+			sam.dbg(_NAME, "Found '%s' (%s)\n", name, path)			
+
+			-- preset the module structure of the detected module
+			-- for auto-loading
+			sam.command[name] = {
+				_module = {
+					_NAME = name,
+					_FILE = path,
+				},
+
+				_NAME = name,
+				_DESCRIPTION = "-",
+
+				help  = function(self)
+						load_module(self._module._NAME)
+						self:help()
+					end,
+				usage = function(self)
+						load_module(self._module._NAME)
+						self:usage()
+					end,
+				main  = function(self,...)
+						load_module(self._module._NAME)
+						return self:main(unpack(arg))
+					end,
+			}
+
+			-- add a metatable so the commands can be used, however,
+			-- it is anly a intermediate metatable, as the module is not
+			-- loaded yet. The module gets loaded (dynamic linker alike)
+			-- once it is called
+
+			setmetatable(sam.command[name], {
+				__call = function(self, ...)
+						load_module(self._module._NAME)
+						return self:main(unpack(arg))
+					end,
+			})
+						
 		end
 	end
-
-	return list
 end
 
+-- COMMANDS -----------------------------------------------------------------
+-- helper functions
+local function create_command(name, desc, usage_func, func)
+	sam.dbg(_NAME, "Creating global SAM command (%s)\n", name)
+
+	-- command structure
+	sam.command[name] = {
+		_NAME = name,
+		_DESCRIPTION = desc,
+
+		_command = {
+			usage = usage_func,
+			main = func
+		},
+
+		help  = function(self) fprintf(io.stdout, "  %16s    %s\n", self._NAME, self._DESCRIPTION) end,
+		usage = function(self) self._command.usage() end,
+		main  = function(self, ...) return self._command.main(unpack(arg)) end,
+	}
+
+	setmetatable(sam.command[name], {
+		__call = function(self, ...) return self._command.main(unpack(arg)) end
+	})
+end
+
+-- commands
 local function usage()
-	print('usage: sam <module> [arguments]')
-	print('\navailable modules are:')
-	for k,_ in pairs(get_modules('^sam_.*')) do
-		print('\t'..string.gsub(k,'sam_',''))
+	print([[Usage: sam [commands]
+
+Commands:]])
+
+	for k,_ in pairs(sam.command) do
+		sam.command[k]:help()
 	end
 end
 
-local function print_table( t )
-	for k,v in pairs(t) do print(k,type(v),v) end
-end
-
--- show the copyrights
-print(COPYRIGHT)
-
-if not arg[ 1 ] then
-	usage()
-else
-	local module=get_modules('sam_'..arg[1])
-	if module[ 'sam_'..arg[1] ] then
-		print('loading module '..module[ 'sam_'..arg[1] ]..'...')
-		module=loadfile( module[ 'sam_'..arg[1] ] )
-		module=module()
-		if module[ 'main' ] then
-			return module.main()
+function help(...)
+	fprintf(io.stderr, "%s v%s %s\n%s\n\n", _NAME, _VERSION, _COPYRIGHT, _DESCRIPTION)
+	if #arg == 1 then
+		if sam.command[ arg[1] ] then
+			sam.command[ arg[1] ]:help()
 		else
-			print("ERROR: this module doesn't contain a main function")
+			usage()
 		end
 	else
-		print("ERROR: module '"..arg[1].."' not found.")
 		usage()
 	end
 end
+
+create_command("help", "Show command reference", usage, help)
+
+
+-- CLI ----------------------------------------------------------------------
+
+-- __cli 
+--   structure behind CLI definitions
+local __cli = {
+	ok = true,
+	command = {
+		-- default wildcard command
+		["*"] = function(self,cmd,...) 
+				self:send("[ERROR] unknown command: %s", cmd or "<none>")
+			end,
+		-- default "exit" command
+		exit = function(self,...) self:finish() end,
+	}
+}
+
+function __cli:finish()
+	self.ok = false
+end
+
+function __cli:get()
+	local line = io.stdin:read("*line")
+	return sam.tokenize(line)
+end
+
+function __cli:send(...)
+	fprintf(io.stdout, unpack(arg))
+	fprintf(io.stdout, "\n")
+end
+
+function __cli:run()
+	-- event loop
+	while self.ok do
+		-- wait for input
+		local args = self:get()
+		local cmd = args[1] ; table.remove(args, 1)
+	
+		-- check command
+		if self.command[cmd] then
+			self.command[cmd](self, unpack(args or {}))
+		else
+			self.command['*'](self, cmd, unpack(args or {}))
+		end
+	end
+end
+
+function sam.cli(def)
+	sam.info(_NAME, "sam.cli(%s,%d)\n", tostring(def), #def)
+	local retval = __cli
+
+	-- install commands
+	for k,v in pairs(def) do
+		sam.dbg(_NAME, "   installing command '%s'\n", k)
+		retval.command[k] = v
+	end
+
+	-- make retval executable
+	setmetatable(retval, { __call = function(self) self:run() end })
+
+	return retval
+end
+
+-- --------------------------------------------------------------------------
+-- INITIALIZE SAM
+-- --------------------------------------------------------------------------
+detect_modules()
+
+-- --------------------------------------------------------------------------
+-- TEST
+-- --------------------------------------------------------------------------
+
+if arg[1] then
+	-- check for help as second argument, must be first
+	if arg[2] and arg[2] == "help" then
+		arg[2] = arg[1]
+		arg[1] = "help"
+	end
+
+	-- split command and command arguments
+	local cmd = arg[1]
+	local args = arg ; table.remove(args, 1)
+
+	-- execute
+	sam.command[cmd](unpack(args or {}))
+end
+
