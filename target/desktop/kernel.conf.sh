6aceca99bb6018178615a40fbc0dae38c3aa8e13
René Rebe <rene@t2-project.org>
2006-04-10 13:42:19 +0000

    * removed PREEMPTION disabling in the desktop target
    git-svn-id: https://svn.exactcode.de/t2/trunk@16846 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/desktop/kernel.conf.sh
+++ target/desktop/kernel.conf.sh
@@ -18,10 +18,3 @@ echo "desktop target -> disabling oss sound modules ..."
 sed -i -e "s/CONFIG_SOUND_OSS=./# CONFIG_SOUND_OSS is not set/" \
        -e"s/CONFIG_SOUND_PRIME=./# CONFIG_SOUND_PRIME is not set/" $1
 
-# preemtion is not stable on PowerPC - so only enable it for x86 for now
-if [ $SDECFG_ARCH = x86 ] ; then
-	sed -i "s/# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/" $1
-else
-	sed -i "s/CONFIG_PREEMPT=y/# CONFIG_PREEMPT is not set/" $1
-fi
-
