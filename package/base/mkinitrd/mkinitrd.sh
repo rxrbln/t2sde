fb5265a7faf56a7e591cafc4d50b459842971ec4
mtr <michael@tross.org>
2006-05-29 08:02:46 +0000

    * added Fusion-MPT and I2O drivers to initrd
    git-svn-id: https://svn.exactcode.de/t2/trunk@17662 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/mkinitrd/mkinitrd.sh
+++ package/base/mkinitrd/mkinitrd.sh
@@ -79,7 +79,7 @@ echo "Copying kernel modules ..."
   find $moddir/kernel -type f | grep \
 	-e reiserfs -e reiser4 -e ext2 -e ext3 -e /jfs -e /xfs \
 	-e isofs -e udf -e /unionfs -e ntfs -e fat -e dm-mod \
-	-e /ide/ -e /scsi/ -e hci -e usb-storage -e sbp2 |
+	-e /ide/ -e /scsi/ -e /message/ -e hci -e usb-storage -e sbp2 |
   while read fn ; do
 
 	for x in $fn `modinfo $fn | grep depends |
