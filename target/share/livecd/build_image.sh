9c800f7a034147488ed5839b6c6fe2b5888434bb
Minto van der Sluis <Minto@MultiMach.com>
2006-06-30 17:32:53 +0000

    * merged r18246 from 2.2 tree 	* fixed livecd by removing reference to none existing output dir.
    git-svn-id: https://svn.exactcode.de/t2/trunk@18250 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/share/livecd/build_image.sh
+++ target/share/livecd/build_image.sh
@@ -58,7 +58,7 @@ time rsync -artH $v --delete --exclude-from ../files-exclude \
 rm ../files-{wanted,all,exclude}
 
 echo "Overlaying root file-system with target defined files ..."
-copy_and_parse_from_source $base/target/share/output/livecd/rootfs $imagelocation
+copy_and_parse_from_source $base/target/share/livecd/rootfs $imagelocation
 copy_and_parse_from_source $base/target/$target/rootfs $imagelocation
 
 [ "$inject_hook" ] && "$inject_hook"
