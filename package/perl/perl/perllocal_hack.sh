5cf90de48e4d152e5010b362c4cb8f7d99014b62
René Rebe <rene@t2-project.org>
2006-07-05 08:38:49 +0000

    * only run the perllocal hack on native builds
    git-svn-id: https://svn.exactcode.de/t2/trunk@18337 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/perl/perl/perllocal_hack.sh
+++ package/perl/perl/perllocal_hack.sh
@@ -12,6 +12,8 @@
 # GNU General Public License can be found in the file COPYING.
 # --- T2-COPYRIGHT-NOTE-END ---
 
+if atstage native; then
+
 # Quick & Dirty hack for the perllocal problem
 # .. to be included by packages which do 'share' the perllocal.pod file
 #
@@ -21,3 +23,4 @@ hook_add postmake 9 "( cd $archlib; mv -v perllocal.pod $pkg.pod || true;
                        mv -v perllocal.pod.sik perllocal.pod || true; )"
 var_append flist''del '|' "${archlib#/}/perllocal.pod"
 
+fi
