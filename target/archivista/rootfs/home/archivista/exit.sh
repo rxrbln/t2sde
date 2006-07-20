c44ce939a80bc4eb0158611bbfac45a05386d0a5
René Rebe <rene@t2-project.org>
2006-07-20 13:06:20 +0000

    * fixed a typo in archivista exit check
    git-svn-id: https://svn.exactcode.de/t2/trunk@18670 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/exit.sh
+++ target/archivista/rootfs/home/archivista/exit.sh
@@ -14,7 +14,7 @@ fi
 
 error=0
 if [ -e /home/archivista/.wine/drive_c/Programs/Av5e/AV5AUTO.WRK ]; then
-  Xdialog --title "" --yesno "There are batch jobes running. \
+  Xdialog --title "" --yesno "There are batch jobs running. \
   Do you want to stop them?" 0 0 || exit
 	Xdialog --title "Stop ArchivistaBox" --no-buttons --infobox "Please wait some seconds..." 0 0 20000 &
   /usr/bin/perl /home/cvs/archivista/jobs/stopJobs.pl 20
