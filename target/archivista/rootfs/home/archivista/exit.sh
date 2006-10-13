3262fa8585387e93227861168c40fa9c3d3e7624
René Rebe <rene@t2-project.org>
2006-10-13 16:11:46 +0000

    * added control about shutdown/reset to the archivista exit script 	  (and not fall back into gdm - this way the machien can be shut down 	   or rebooted via VNC ...)
    git-svn-id: https://svn.exactcode.de/t2/trunk@20163 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/exit.sh
+++ target/archivista/rootfs/home/archivista/exit.sh
@@ -16,13 +16,21 @@ error=0
 if [ -e /home/archivista/.wine/drive_c/Programs/Av5e/AV5AUTO.WRK ]; then
   Xdialog --title "" --yesno "There are batch jobs running. \
   Do you want to stop them?" 0 0 || exit
-	Xdialog --title "Stop ArchivistaBox" --no-buttons --infobox "Please wait some seconds..." 0 0 20000 &
+  Xdialog --title "Stop ArchivistaBox" --no-buttons --infobox "Please wait some seconds..." 0 0 20000 &
   /usr/bin/perl /home/cvs/archivista/jobs/stopJobs.pl 20
   error=$?
 fi
 
 if [ $error -eq 0 ]; then
-  killall fluxbox
+  mode=`Xdialog --title 'Exit' --stdout --no-tags --separator ' ' --radiolist \
+	"Choose one option" 0 0 3 \
+	1 'Shutdown' off \
+	2 'Reboot'   off` || exit
+  if [ $mode -eq 1 ];then
+    shutdown -h now
+  elif  [ $mode -eq 2 ];then
+    reboot
+  fi
 else
   Xdialog --msgbox "Jobs can't be stopped. Please try again later." 0 0
 fi
