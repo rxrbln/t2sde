f51712188fbf0d1f40c9c855efe31c69a9380eb3
René Rebe <rene@t2-project.org>
2006-10-25 12:41:03 +0000

    * added work-in-progress writing of optical disc media to the archivista 	  target (for backup reasons)
    git-svn-id: https://svn.exactcode.de/t2/trunk@20464 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ target/archivista/rootfs/home/archivista/wodim-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$UID" -ne 0 ]; then
+	exec gnomesu -t "Setup backup" \
+	-m "Please enter the system password (root user)^\
+in order to setup the the optical disc archiving." -c $0
+fi
+
+# PATH and co
+. /etc/profile
+
+# read config if existing
+set -x
+[ -e /etc/wodim.conf ] && . /etc/wodim.conf
+[ "$copies" ] || copies=1
+[ "$format" ] || format='CD'
+
+# get requested format
+cdonoff='off'
+dvdonoff='off'
+case $format in
+	CD) cdonoff='on' ;;
+	DVD) dvdonoff='on' ;;
+esac
+
+format=`Xdialog --title "Optical disc archiving" --stdout --no-tags --separator ' ' \
+                --radiolist  "Format allowed for archiving:" 0 0 3 \
+                'CD' 'CD' $cdonoff  'DVD' 'DVD' $dvdonoff` || exit
+
+# get no. of writers
+copies=`Xdialog --stdout --title "Optical disc archiving" \
+        --spinbox "Number of optical disc copies to
+be burned on different writers." 0 0 1 9 $copies "# of writer"` || exit
+
+echo "format=\"$format\"
+copies=\"$copies\"" > /etc/wodim.conf
