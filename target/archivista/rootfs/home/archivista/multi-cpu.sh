ea408b5076fe41b7f592181548d9fcb1619723f8
René Rebe <rene@t2-project.org>
2006-10-25 16:15:47 +0000

    * fixed archivista target max_cpu setting, it is max_cpus (changed 	  function names accordingly)
    git-svn-id: http://svn.exactcode.de/t2/trunk@20473 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/multi-cpu.sh
+++ target/archivista/rootfs/home/archivista/multi-cpu.sh
@@ -12,17 +12,17 @@ fi
 # include shared code
 . ${0%/*}/multi-cpu.in
 
-maxcpu=`get_max_cpu /boot/grub/menu.lst`
+maxcpus=`get_max_cpus /boot/grub/menu.lst`
 
-if [ $maxcpu = 0 ]; then
+if [ $maxcpus = 0 ]; then
 	Xdialog --title "Multi CPU" --yesno "Enable multi CPU (SMP) support?" 0 0 || exit
-	maxcpu=8
+	maxcpus=8
 else
 	Xdialog --title "Multi CPU" --yesno "Disable multi CPU (SMP) support?" 0 0 || exit
-	maxcpu=0
+	maxcpus=0
 fi
 
-set_max_cpu /boot/grub/menu.lst $maxcpu
+set_max_cpus /boot/grub/menu.lst $maxcpus
 
 Xdialog --title "Multi CPU" --msgbox "The setting was saved and will
 have effect on the next reboot." 0 0
