f00591f541cbf99fc463bae872f826a6adc91ec1
René Rebe <rene@t2-project.org>
2006-11-01 06:16:24 +0000

    * improved archivista to permanently allow to enable SSH
    git-svn-id: https://svn.exactcode.de/t2/trunk@20587 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/ssh-enable.sh
+++ target/archivista/rootfs/home/archivista/ssh-enable.sh
@@ -14,7 +14,7 @@ if ! test -e /etc/ssh/ssh_host_key -o \
           -e /etc/ssh/ssh_host_dsa_key -o \
           -e /etc/ssh/ssh_host_rsa_key
 then
-	Xdialog --stdout --yesno "No host keys present.
+	Xdialog --title "SSH setup" --yesno "No host keys present.
 Generate private keys now?" 8 28 || exit
 	# create keys - reuse stone code - don't care about details
 	stone -text sshd <<-EOT
@@ -25,5 +25,14 @@ fi
 
 rc sshd start
 
-Xdialog --title "" --msgbox "Remote access (SSH) enabled." 0 0
+if Xdialog --title "SSH setup" --default-no --yesno \
+           "Enable remote access (SSH) permanently?" \
+           0 0; then
+	ln -sf ../init.d/sshd /etc/rc.d/rc5.d/S25sshd
+	ln -sf ../init.d/sshd /etc/rc.d/rc5.d/K75sshd
 
+	Xdialog --title "SSH setup" --msgbox \
+	        "Remote access (SSH) enabled permanently." 0 0
+else
+	Xdialog --title "SSH setup" --msgbox "Remote access (SSH) enabled." 0 0
+fi
