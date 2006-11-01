2318fc9ae27f0a0cafb2586d91ba6556eecf1b4b
René Rebe <rene@t2-project.org>
2006-11-01 06:49:26 +0000

    * implemented permanent VNC setup for the archivista target, likewise
    git-svn-id: https://svn.exactcode.de/t2/trunk@20589 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/vnc-disable.sh
+++ target/archivista/rootfs/home/archivista/vnc-disable.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 killall x11vnc
+rm -f /etc/vnc.conf
 
-Xdialog --stdout --msgbox "Graphicial remote access (VNC) is disabled" 0 0
-
-
+Xdialog --title "VNC setup" --msgbox "Graphicial remote access (VNC) is disabled" 0 0
