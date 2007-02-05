9d61ff595819b8f01472726232d4225aaec132ac
René Rebe <rene@t2-project.org>
2007-02-05 09:45:52 +0000

    * resort archivista kernels regarding some custom criteria
    git-svn-id: https://svn.exactcode.de/t2/trunk@22382 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/build_initrd.sh
+++ target/archivista/build_initrd.sh
@@ -3,3 +3,7 @@
 boot_title="Archivista Box - $build_date"
 . $base/target/share/$SDECFG_IMAGE/build_initrd.sh
 
+# sort the kernel images according our wishes
+awk -f $base/target/archivista/rootfs/home/archivista/sort-kernel.awk < \
+       $isofsdir/boot/grub/menu.lst > $isofsdir/boot/grub/menu.lst.new
+mv -f $isofsdir/boot/grub/menu.lst{.new,}
