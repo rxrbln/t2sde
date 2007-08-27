f322452ef6995982f771d7ff46b1ac5fe4db05ed
René Rebe <rene@t2-project.org>
2007-08-27 07:36:33 +0000

    * merged rtai branch
    git-svn-id: https://svn.exactcode.de/t2/trunk@25792 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/rtai/build_image.sh
+++ target/rtai/build_image.sh
@@ -75,7 +75,7 @@ copy_with_list_from_file $build_root . $PWD/tar.input
 rm tar.input
 
 echo "Preparing root filesystem image from target defined files ..."
-rm -f sbin/init ; ln -s minit sbin/init
+# rm -f sbin/init ; ln -s minit sbin/init
 copy_from_source $base/target/$target/rootfs .
 
 echo "Creating links for identical files ..."
