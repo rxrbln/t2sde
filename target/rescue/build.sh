8e44274bcca6360474514f745ef7336020f8d195
René Rebe <rene@t2-project.org>
2008-08-15 15:16:59 +0000

    * added an emacs -> zile symlink and avoid bision in the rescue image
    git-svn-id: https://svn.exactcode.de/t2/trunk@30082 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/rescue/build.sh
+++ target/rescue/build.sh
@@ -32,7 +32,7 @@ rescue_rootfs_filter ()
 filter_hook=rescue_rootfs_filter
 
 # do not include some devel packages
-var_append pkg_filter ' ' 'dietlibc flex make cmake scons python perl autoconf automake libtool m4 ccache distcc texinfo binutils gcc'
+var_append pkg_filter ' ' 'dietlibc flex bison make cmake scons python perl autoconf automake libtool m4 ccache distcc texinfo binutils gcc'
 
 . target/generic/build.sh
 
