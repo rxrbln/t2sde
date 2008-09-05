0c57378d1f7b33ba9762bfa62ddee6c8fc3d5b84
mtr <michael@tross.org>
2008-09-05 11:06:30 +0000

    * fixed rocknet dhcp script to enable $if before starting dhclient
    git-svn-id: http://svn.exactcode.de/t2/trunk@30351 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/network/dhcp/rocknet_dhcp.sh
+++ package/network/dhcp/rocknet_dhcp.sh
@@ -13,7 +13,7 @@
 # --- T2-COPYRIGHT-NOTE-END ---
 
 public_dhcp() {
-	addcode up   5 5 "/sbin/dhclient -q $if"
+	addcode up   5 5 "ip link set $if up ; /sbin/dhclient -q $if"
 	addcode down 5 5 "killall -TERM dhclient"
 	addcode down 5 6 "sleep 2 ; ip addr flush $if && ip link set $if down || ifconfig $if down"
 }
