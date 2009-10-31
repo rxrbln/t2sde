79783056634789fa59f7fd46b771e8f18e322c41
René Rebe <rene@t2-project.org>
2009-10-31 20:44:42 +0000

    * optimized initrd, do not include netfilter and wireless modules 	  (beside space optimization this fixes disfunctional wireless 	   modules such as ath9k, due lack of encryption and other support 	   modules, despite potentially preventing NFS roots over the air)
    git-svn-id: https://svn.exactcode.de/t2/trunk@34768 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/mkinitrd/mkinitrd.sh
+++ package/base/mkinitrd/mkinitrd.sh
@@ -75,13 +75,14 @@ mknod $tmpdir/dev/console c 5 1
 echo "Copying kernel modules ..."
 
 (
-  find $moddir/kernel -type f | grep \
-	-e reiserfs -e reiser4 -e ext2 -e ext3 -e ext4 -e /jfs -e /xfs \
+  find $moddir/kernel -type f |
+  grep -v -e /wireless/ -e netfilter |
+  grep  -e reiserfs -e reiser4 -e ext2 -e ext3 -e ext4 -e /jfs -e /xfs \
 	-e isofs -e udf -e /unionfs -e ntfs -e fat -e dm-mod \
 	-e /ide/ -e /ata/ -e /scsi/ -e /message/ \
 	-e cciss -e ips -e virtio \
 	-e hci -e usb-storage -e sbp2 \
-	-e drivers/net/ -e drivers/md/ -e '/ipv6\.' -e usbhid |
+	-e /net/ -e drivers/md/ -e '/ipv6\.' -e usbhid |
   while read fn ; do
 
 	for x in $fn `modinfo $fn | grep depends |
