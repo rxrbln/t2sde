rxrbln/t2sde

commit 2fba9c3533a9936cdd230c9de90506bb929e653a

* allow choosing whether to build the rescue target as livecd or initrd
git-svn-id: https://svn.exactcode.de/t2/trunk@30172 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

target/rescue/build.sh
@@ -1,8 +1,8 @@
 # --- T2-COPYRIGHT-NOTE-BEGIN ---
 # This copyright note is auto-generated by ./scripts/Create-CopyPatch.
 # 
-# T2 SDE: target/desktop/build.sh
-# Copyright (C) 2006 The T2 SDE Project
+# T2 SDE: target/rescue/build.sh
+# Copyright (C) 2006 - 2008 The T2 SDE Project
 # 
 # More information can be found in the files COPYING and README.
 # 
@@ -39,7 +39,8 @@ var_append pkg_filter ' ' 'scons python perl perl-xml-parser'
 . target/generic/build.sh
 
 # now this is a hack - and x86 specific anyway :-(
-if [[ $arch = x86* ]]; then
+if [ "SDECFG_TARGET_RESCUE_STYLE" = "livecd" ]; then
+  if [[ $arch = x86* ]]; then
 	case "$SDECFG_X86_CD_LOADER" in
 	grub)
 		sed -i 's/kernel.*/& vga=0x317/' $isofsdir/boot/grub/menu.lst ;;
@@ -48,4 +49,5 @@ if [[ $arch = x86* ]]; then
 	*)
 		echo "Adapt target/rescue/build.sh for unknown boot loader: $SDECFG_X86_CD_LOADER" ;;
 	esac
+  fi
 fi

target/rescue/config.in
@@ -1,8 +1,8 @@
 # --- T2-COPYRIGHT-NOTE-BEGIN ---
 # This copyright note is auto-generated by ./scripts/Create-CopyPatch.
 # 
-# T2 SDE: target/desktop/config.in
-# Copyright (C) 2006 The T2 SDE Project
+# T2 SDE: target/rescue/config.in
+# Copyright (C) 2006 - 2008 The T2 SDE Project
 # 
 # More information can be found in the files COPYING and README.
 # 
@@ -14,8 +14,12 @@
 
 #Description: T2 Rescue
 
+choice SDECFG_TARGET_RESCUE_STYLE livecd \
+	livecd	'A bootable Live CD/DVD .iso' \
+	initrd	'A network bootable initrd'
+
 SDECFGSET_PKGFILE_TYPE='none'
-SDECFGSET_IMAGE='livecd'
+SDECFGSET_IMAGE=$SDECFG_TARGET_RESCUE_STYLE
 
 SDECFGSET_DO_REBUILD_STAGE=0
 SDECFGSET_DISABLE_NLS=1

commit 176e5b533f4782d57d7772c8b59bba69d0d9c4f8

* fixed typo in rescue target style selection, introduced in r30172
git-svn-id: https://svn.exactcode.de/t2/trunk@35493 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

target/rescue/build.sh
@@ -39,7 +39,7 @@ var_append pkg_filter ' ' 'scons python perl perl-xml-parser intltool'
 . target/generic/build.sh
 
 # now this is a hack - and x86 specific anyway :-(
-if [ "SDECFG_TARGET_RESCUE_STYLE" = "livecd" ]; then
+if [ "$SDECFG_TARGET_RESCUE_STYLE" = "livecd" ]; then
   if [[ $arch = x86* ]]; then
 	case "$SDECFG_X86_CD_LOADER" in
 	grub)