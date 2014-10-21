dad1668c7b7904459b259dc09530ba348d9a8803
René Rebe <rene@t2-project.org>
2014-10-21 14:01:36 +0000

    * fixed mips endian selection, broken in i2007 (r23542), ..., sigh
    git-svn-id: https://svn.exactcode.de/t2/trunk@43314 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- architecture/mips/archtest.sh
+++ architecture/mips/archtest.sh
@@ -13,10 +13,10 @@
 # --- T2-COPYRIGHT-NOTE-END ---
 
 case "$SDECFG_MIPS_ENDIANESS" in
-	el)
+    EL)
 	arch_bigendian=no
 	arch_target="mipsel-t2-linux-gnu" ;;
-	eb)
+    EB)
 	arch_bigendian=yes
 	arch_target="mips-t2-linux-gnu" ;;
 esac
