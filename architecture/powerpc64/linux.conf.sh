ba6154ed2d3b9073fbb952b70c518ebacc20ac1b
René Rebe <rene@t2-project.org>
2018-01-22 22:00:05 +0000

    * added powerpc64 cell optimization option 	* wire up recently added power8 kernel optimization, while at it
    git-svn-id: https://svn.exactcode.de/t2/trunk@46434 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- architecture/powerpc64/linux.conf.sh
+++ architecture/powerpc64/linux.conf.sh
@@ -17,16 +17,19 @@
 	for x in "generic	GENERIC_CPU"	\
 		 "power3	POWER3_CPU"	\
 		 "power4	POWER4_CPU"	\
+		 "cell		CELL_CPU"	\
 		 "G5		POWER4_CPU"	\
 		 "power5	POWER5_CPU"	\
 		 "power6	POWER6_CPU"	\
-		 "power7	POWER7_CPU"
+		 "power7	POWER7_CPU"	\
+		 "power8	POWER8_CPU"
 	do
 		set $x
 		[[ "$SDECFG_POWERPC64_OPT" = $1 ]] && linux_arch=$2
 	done
 
-	for x in GENERIC_CPU POWER3_CPU POWER4_CPU POWER5_CPU POWER6_CPU POWER7_CPU
+	for x in GENERIC_CPU POWER3_CPU POWER4_CPU CELL_CPU \
+		 POWER5_CPU POWER6_CPU POWER7_CPU POWER8_CPU
 	do
 		if [ "$linux_arch" != "$x" ]
 		then echo "# CONFIG_$x is not set"
