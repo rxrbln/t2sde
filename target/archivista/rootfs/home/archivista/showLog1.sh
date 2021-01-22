2632e2d11788058e9e2e826fb257e6cf2c99f82c
René Rebe <rene@t2-project.org>
2021-01-22 11:09:49 +0000

    * removed unmaintained archivista target
    git-svn-id: http://svn.exactcode.de/t2/trunk@51431 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/archivista/rootfs/home/archivista/showLog1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$UID" -ne 0 ]; then
-	exec gnomesu -p -t "View Archivista log file" \
-	-m "Please enter the system password (root user)^\
-in order to view the Archivista log file." -c $0
-fi
-
-Xdialog --tailbox /home/archivista/.wine/drive_c/Programs/Av5e/AV5AUTO.LOG 20	100
--- target/archivista/rootfs/home/archivista/vnc-viewer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-vncviewer --PreferredEncoding=raw
-
