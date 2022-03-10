rxrbln/t2sde

commit 131aed022bd3ae06d796c4da89d9f8a4fc081fb1

* further improved grub2 stone module to selectively include 	  architecture specific modules and include part_sun for sparc*
git-svn-id: https://svn.exactcode.de/t2/trunk@54761 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

package/boot/grub2/stone_mod_grub2.sh
@@ -93,10 +93,16 @@ $( cat /boot/grub/grub.cfg )"
 	unset bootpath
 }
 
-grubmods="part_gpt part_msdos ntfs ntfscomp hfsplus fat ext2 iso9660 \
-          boot configfile linux btrfs all_video reiserfs xfs jfs lvm \
-          normal crypto cryptodisk luks sleep reboot \
-          search search_fs_file search_label search_fs_uuid"
+grubmods="normal boot configfile linux part_msdos part_gpt \
+	  fat ext2 iso9660 reiserfs xfs jfs lvm \
+	  crypto cryptodisk luks all_video sleep reboot \
+	  search search_fs_file search_label search_fs_uuid"
+
+case "$arch" in
+	ppc*)	grubmods="$grubmods part_apple hfs hfsplus suspend" ;;
+	sparc*)	grubmods="$grubmods part_sun" ;;
+	x86*)	grubmods="$grubmods ntfs ntfscomp" ;;
+esac
 
 grub_inst() {
     if [[ $arch != ppc* ]]; then
@@ -165,7 +171,7 @@ EOT
 	    fi
 	    grub-mkimage -O powerpc-ieee1275 -p /boot/grub \
 		-o /mnt/boot/grub.elf -d /usr/lib*/grub/powerpc-ieee1275 \
-		$grubmods part_apple hfs suspend
+		$grubmods
 
 	    cat > /mnt/ofboot.b <<-EOT
 <CHRP-BOOT>

commit b8ec75939dd04e44bf673d136c895eb0392031de

* fixed grub2 to support grub2 again, lost w/ regression in r54761 	* added luks2 module while at it
git-svn-id: https://svn.exactcode.de/t2/trunk@55806 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

package/boot/grub2/stone_mod_grub2.sh
@@ -1,6 +1,6 @@
 # --- T2-COPYRIGHT-NOTE-BEGIN ---
 # T2 SDE: package/*/grub2/stone_mod_grub2.sh
-# Copyright (C) 2004 - 2021 The T2 SDE Project
+# Copyright (C) 2004 - 2022 The T2 SDE Project
 # Copyright (C) 1998 - 2003 ROCK Linux Project
 # 
 # This Copyright note is generated by scripts/Create-CopyPatch,
@@ -94,8 +94,8 @@ $( cat /boot/grub/grub.cfg )"
 }
 
 grubmods="normal boot configfile linux part_msdos part_gpt \
-	  fat ext2 iso9660 reiserfs xfs jfs lvm \
-	  crypto cryptodisk luks all_video sleep reboot \
+	  fat ext2 iso9660 reiserfs btrfs xfs jfs lvm \
+	  crypto cryptodisk luks luks2 all_video sleep reboot \
 	  search search_fs_file search_label search_fs_uuid"
 
 case "$arch" in