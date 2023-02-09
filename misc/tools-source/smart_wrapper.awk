rxrbln/t2sde

commit 05a45ba83ecf3e7d494bd0bfb2c5a7a3133deb6a

* simplified smart wrapper to only evaluate the hot match argument 	* moved default to gcc/parse-config propper, and use -O2 as smart 	  base for c++ as otherwise gcc and clang become too slow
git-svn-id: https://svn.exactcode.de/t2/trunk@52541 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

misc/tools-source/smart_wrapper.awk
@@ -3,7 +3,7 @@
 # This copyright note is auto-generated by scripts/Create-CopyPatch.
 # 
 # T2 SDE: misc/tools-source/smart_wrapper.awk
-# Copyright (C) 2020 The T2 SDE Project
+# Copyright (C) 2020 - 2021 The T2 SDE Project
 # Copyright (C) 1998 - 2003 ROCK Linux Project
 # 
 # More information can be found in the files COPYING and README.
@@ -27,19 +27,18 @@ ARGIND == 1 {
 
 # argument 2, read command list from stdin
 ARGIND == 2 {
-	if (gsub("^-SPEED", "") == 1)
+	if (gsub("^-HOT", "") == 1)
 		speedarg[speedargidx++] = $0;
-	else if (gsub("^-SIZE", "") == 1)
-		sizearg[sizeargidx++] = $0;
 	else {
 		speedarg[speedargidx++] = $0;
 		sizearg[sizeargidx++] = $0;
 
 		# remove path prefix, and lookup hot file
-		gsub(".*/", "");
-		if (hotlist[$0] == 1) {
+		if (gsub("^[^-].*/", "") > 0) {
+		    if (hotlist[$0] == 1) {
 			# print("hot file: ", $0) > "/dev/stderr"
 			ishot = 1;
+		    }
 		}
 	}
 }

package/develop/gcc/parse-config
@@ -84,12 +84,18 @@ case "$SDECFG_OPT" in
 		[ -f "$f" ] || f="$confdir/../${pkg%%[0-9]*}/${pkg%%[0-9]*}.prof"
 		# TODO: or fix awk script for non existing file?
 		[ -f "$f" ] || f="/dev/null"
-		[ "$SDECFG_OPT" = "smarter" ] && opt="O3" || opt="O2"
-		var_insert GCC_WRAPPER_INSERT " " "-SPEED-$opt -SIZE-Os"
+
+		[ "$SDECFG_OPT" = "smarter" ] && opt="-O3" || opt="-O2"
+
+		var_insert CC_WRAPPER_INSERT " " "-Os"
+		var_insert CXX_WRAPPER_INSERT " " "-O2"
+		var_insert GCC_WRAPPER_INSERT " " "-HOT$opt"
 		var_insert GCC_WRAPPER_FILTER "|" "gawk -f $base/misc/tools-source/smart_wrapper.awk $f -"
-		var_insert KCC_WRAPPER_INSERT " " "-SPEED-$opt -SIZE-Os"
+		var_insert KCC_WRAPPER_INSERT " " "-Os -HOT$opt"
 		var_insert KCC_WRAPPER_FILTER "|" "gawk -f $base/misc/tools-source/smart_wrapper.awk $f -"
-		var_insert SYSGCC_WRAPPER_INSERT " " "-SPEED-$opt -SIZE-Os"
+		var_insert SYSCC_WRAPPER_INSERT " " "-Os"
+		var_insert SYSCXX_WRAPPER_INSERT " " "-O2"
+		var_insert SYSGCC_WRAPPER_INSERT " " "-HOT$opt"
 		var_insert SYSGCC_WRAPPER_FILTER "|" "gawk -f $base/misc/tools-source/smart_wrapper.awk $f -"
 		;;
 	speed)

commit 2cfe3bb67dc839545242db9c7a9b158e61057ef7

* fixed smart-wrapper.awk regression from r52541, 	  arguments w/o a path directory separator would not match anymore
git-svn-id: http://svn.exactcode.de/t2/trunk@60847 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

misc/tools-source/smart_wrapper.awk
@@ -1,17 +1,14 @@
 #!/usr/bin/gawk -f
 # --- T2-COPYRIGHT-NOTE-BEGIN ---
-# This copyright note is auto-generated by scripts/Create-CopyPatch.
-# 
 # T2 SDE: misc/tools-source/smart_wrapper.awk
-# Copyright (C) 2020 - 2021 The T2 SDE Project
+# Copyright (C) 2020 - 2023 The T2 SDE Project
 # Copyright (C) 1998 - 2003 ROCK Linux Project
 # 
-# More information can be found in the files COPYING and README.
+# This Copyright note is generated by scripts/Create-CopyPatch,
+# more information can be found in the files COPYING and README.
 # 
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License. A copy of the
-# GNU General Public License can be found in the file COPYING.
+# it under the terms of the GNU General Public License version 2.
 # --- T2-COPYRIGHT-NOTE-END ---
 
 BEGIN {
@@ -23,6 +20,7 @@ BEGIN {
 # argument 1, read in smart profile db, 2nd form for POSIXLY_CORRECT
 ARGIND == 1 || (!ARGIND && FILENAME != "-") {
 	hotlist[$2] = 1;
+	# printf("hot db: '%s'\n", $2) > "/dev/stderr"
 }
 
 # argument 2, read command list from stdin, 2nd form for POSIXLY_CORRECT
@@ -34,9 +32,11 @@ ARGIND == 2 || (!ARGIND && FILENAME == "-") {
 		sizearg[sizeargidx++] = $0;
 
 		# remove path prefix, and lookup hot file
-		if (gsub("^[^-].*/", "") > 0) {
+		if (match($0, "^[^-]")) {
+		    gsub(".*/", "")
+		    #printf("arg: %s\n", $1) > "/dev/stderr"
 		    if (hotlist[$0] == 1) {
-			# print("hot file: ", $0) > "/dev/stderr"
+			# printf("hot file: '%s'\n", $0) > "/dev/stderr"
 			ishot = 1;
 		    }
 		}