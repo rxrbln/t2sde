1a33610a420cdc03ba909f967f25e34ef07a94fa
René Rebe <rene@t2-project.org>
2023-04-08 12:56:18 +0000

    * include virtio-pci in initrd, too
    git-svn-id: http://svn.exactcode.de/t2/trunk@62429 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/mkinitrd/mkinitrd.sh
+++ package/base/mkinitrd/mkinitrd.sh
@@ -58,7 +58,7 @@ done
 [ "$minimal" != 1 ] && filter="$filter -e reiserfs -e btrfs -e /jfs -e /xfs -e jffs2
 -e /udf -e /unionfs -e ntfs -e /fat -e /hfs -e floppy -e efivarfs
 -e /ata/ -e /scsi/ -e /fusion/ -e /sdhci/ -e nvme/host -e /mmc/
--e virtio.\(blk\|scsi\|net\|console\|input\|gpu\)
+-e virtio.\(blk\|scsi\|net\|console\|input\|gpu\|pci\)
 -e /ast/ -e ps3fb -e ps3disk -e drivers/pcmcia
 -e dm-mod -e dm-raid -e md/raid -e dm/mirror -e dm/linear -e dm-crypt -e dm-cache
 -e /rtc/ -e /aes -e /sha -e /blake -e /cbc -e /ecb -e xts
