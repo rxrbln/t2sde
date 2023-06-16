f55f758fc6556f6f89fdb6fa9f2e11973b057448
NoTag <notag@maneto.fr>
2023-06-16 20:52:25 +0000

    * added pci/controller in initrd to allow booting laptops where you can not 	  disable intel's rapid storage (software raid ahci remapped nvme), 	  no need modprobe the vmd.ko by hand anymore
    git-svn-id: http://svn.exactcode.de/t2/trunk@63707 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/mkinitrd/mkinitrd.sh
+++ package/base/mkinitrd/mkinitrd.sh
@@ -67,7 +67,7 @@ done
 -e cciss -e ips -e nls_cp437 -e nls_iso8859-1 -e nls_utf8
 -e /.hci -e usb-common -e usb-storage -e sbp2 -e uas
 -e usbhid -e i2c-hid -e hid-generic -e hid-multitouch
--e hid-apple -e hid-microsoft -e hyperv-keyboard"
+-e hid-apple -e hid-microsoft -e hyperv-keyboard -e pci/controller"
 
 [ "$network" = 1 ] && filter="$filter -e /ipv4/ -e '/ipv6\.' -e ethernet -e nfsv4"
 
