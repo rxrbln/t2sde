rxrbln/t2sde

commit 5d1875170ae7bc37ada28e6bccd0c711965c6191

* improved install-wrapper to support combined singled dash args
git-svn-id: https://svn.exactcode.de/t2/trunk@60005 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

misc/tools-source/install_wrapper.sh
@@ -47,30 +47,33 @@ if [ "${*/--target-directory//}" != "$*" ]; then
 fi
 
 while [ $# -gt 0 ]; do
-	case "$1" in
+    # split combined args
+    for a in `echo $1 | sed '/^-[^-]/ {s/^-//; s/./-& /g}'`; do
+	case "$a" in
 		-g|-m|-o|-S|--group|--mode|--owner|--suffix)
-			newcommand="$newcommand $1 $2"
+			newcommand="$newcommand $a $2"
 			shift 1
 			;;
 		-s|--strip)
 			if [[ $command != *install ]]; then
-				newcommand="$newcommand $1"
+				newcommand="$newcommand $a"
 			fi
 			;;
 		-t)
 			: # skip -t for now, as we generate target filenames
 			;;
 		-*)
-			newcommand="$newcommand $1"
+			newcommand="$newcommand $a"
 			;;
 		*)
 			if [ -n "$destination" ]; then
 				sources[sources_counter++]="$destination"
 			fi
-			destination="$1"
+			destination="$a"
 			;;
 	esac
-	shift 1
+    done
+    shift 1
 done
 
 [ -z "${destination##/*}" ] || destination="$PWD/$destination"

commit 4d104319dcae5c3e4fb2cd5c8335fd3a19be8bd6

* fixed install_wrapper to not break up destination files w/ spaces, 	  regression from r60005
git-svn-id: http://svn.exactcode.de/t2/trunk@64598 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

misc/tools-source/install_wrapper.sh
@@ -46,11 +46,14 @@ fi
 
 while [ $# -gt 0 ]; do
     # split combined args
-    for a in `echo $1 | sed '/^-[^-]/ {s/^-//; s/\([^0-9-]\)/ -\1/g}'`; do
-	case "$a" in
+    case "$1" in
+	-*)
+	    # split combined args, like -m755
+	    for a in `echo $1 | sed '/^-[^-]/ {s/^-//; s/\([^0-9-]\)/ -\1/g}'`; do
+		case "$a" in
 		-g|-m|-o|-S|--group|--mode|--owner|--suffix)
 			newcommand="$newcommand $a $2"
-			shift 1
+			shift
 			;;
 		-s|--strip)
 			if [[ $command != *install ]]; then
@@ -63,15 +66,18 @@ while [ $# -gt 0 ]; do
 		-*)
 			newcommand="$newcommand $a"
 			;;
-		*)
-			if [ -n "$destination" ]; then
-				sources[sources_counter++]="$destination"
-			fi
-			destination="$a"
-			;;
-	esac
-    done
-    shift 1
+		esac
+	    done
+	    ;;
+
+	*)
+		if [ -n "$destination" ]; then
+			sources[sources_counter++]="$destination"
+		fi
+		destination="$1"
+		;;
+    esac
+    shift
 done
 
 [ -z "${destination##/*}" ] || destination="$PWD/$destination"