119b71d153cadd06f2b7274abb6f5b24535cd4c8
René Rebe <rene@t2-project.org>
2023-08-16 09:42:51 +0000

    * enable XEN support by default, too
    git-svn-id: http://svn.exactcode.de/t2/trunk@64681 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- architecture/x86-64/linux.conf.sh
+++ architecture/x86-64/linux.conf.sh
@@ -75,6 +75,7 @@
 		CONFIG_INTEL_IOMMU=y
 		CONFIG_INTEL_IOMMU_SVM=y
 		CONFIG_HYPERV_IOMMU_SVM=y
+		CONFIG_XEN_VIRTIO=y
 		CONFIG_IOMMU_DEFAULT_PASSTHROUGH=y
 
 		dnl Support for latest low level clocks, gpio, and i2c glue
