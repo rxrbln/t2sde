74bb1106769f384b01c594ae5782be82a8ed93c9
René Rebe <rene@t2-project.org>
2023-12-19 14:28:19 +0000

    * include bochs tiny drm module in initrd for virtualization, including qemu-system-sparc64
    git-svn-id: https://svn.exactcode.de/t2/trunk@66093 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/mkinitrd/mkinitrd.sh
+++ package/base/mkinitrd/mkinitrd.sh
@@ -32,7 +32,7 @@ vitalmods[xhci-pci.ko]=1 # probably every modern machine
 # TODO: defauls for vintage vs. latest, usb, pata, etc.
 filter="-e ext4 -e isofs -e pata_legacy -e pata_.*platform -e sym53c8xx
 -e s[rd]_mod -e /ahci.ko -e /nvme.ko -e [uoex]hci-pci -e usbhid -e zram
--e /offb -e ps3fb"
+-e /offb -e /bochs -e ps3fb"
 
 declare -A added
 
