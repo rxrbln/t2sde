4eff30d50e4e77980bf3f580f3174d93dcf90e1e
René Rebe <rene@t2-project.org>
2025-02-02 18:51:00 +0000

    * updated tests a bit to work, inst no symlink, firefox version
    git-svn-id: https://svn.exactcode.de/t2/trunk@73572 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- tests/912-inst-minird.cmd
+++ tests/912-inst-minird.cmd
@@ -3,5 +3,5 @@
 source ${0%/*}/qemu.in
 
 cd $SYSROOT
-expect -f ${0%.cmd}.exp $QEMUSYS boot/$vmlinux TOOLCHAIN/isofs/boot/minird \
+expect -f ${0%.cmd}.exp $QEMUSYS boot/$vmlinux TOOLCHAIN/isofs/boot/minird-* \
 	"$append" $qemuargs -m $((lowmem + 16)) ${extstorage}$ISO # mem=512m
--- tests/921-inst-kexec.cmd
+++ tests/921-inst-kexec.cmd
@@ -5,5 +5,5 @@ source ${0%/*}/qemu.in
 cd $SYSROOT
 [ -e usr/sbin/kexec ] || exit 43
 
-expect -f ${0%.cmd}.exp $QEMUSYS boot/$vmlinux TOOLCHAIN/isofs/boot/initrd \
+expect -f ${0%.cmd}.exp $QEMUSYS boot/$vmlinux-* TOOLCHAIN/isofs/boot/initrd-* \
 	"$append" $qemuargs -m $normmem ${extstorage}$ISO
