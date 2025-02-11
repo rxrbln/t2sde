b7b5a0881ce72bd1249439b7d36977c0bbc24943
NoTag <vs@salvatorenco.fr>
2025-02-11 10:53:51 +0000

    * added a misc/archive/hello.py example
    git-svn-id: https://svn.exactcode.de/t2/trunk@73998 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ misc/archive/hello.py
@@ -0,0 +1,11 @@
+# --- T2-COPYRIGHT-BEGIN ---
+# t2/misc/archive/hello.py
+# Copyright (C) 2025 The T2 SDE Project
+# SPDX-License-Identifier: GPL-2.0
+# --- T2-COPYRIGHT-END ---
+
+def main():
+    print("Hello from Python")
+
+if __name__ == "__main__":
+    main()
