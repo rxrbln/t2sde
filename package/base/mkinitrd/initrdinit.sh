rxrbln/t2sde

commit dfbb27b09830730b4c741d40478266564f67b752

* remove more old-school shell function overlooked previously
git-svn-id: https://svn.exactcode.de/t2/trunk@81812 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

package/base/mkinitrd/initrdinit.sh
@@ -4,7 +4,7 @@ PATH=/sbin:/bin:/usr/bin:/usr/sbin
 
 echo "T2 SDE early userspace (c)2005-2025 Rene Rebe, ExactCODE GmbH; Germany."
 
-function mapper2lvm {
+mapper2lvm {
 	# support both, direct vg/lv or mapper/...
 	x=${1#mapper/}
 	if [ "$x" != "$1" -a "${x#*-}" != "$x" ]; then
@@ -13,7 +13,7 @@ function mapper2lvm {
 	echo $x
 }
 
-function boot {
+boot {
 	mount -t none -o move {,/mnt}/dev
 	mount -t none -o move {,/mnt}/proc
 	mount -t none -o move {,/mnt}/sys

architecture/share/boot/grub.in
@@ -1,15 +1,10 @@
-# --- T2-COPYRIGHT-NOTE-BEGIN ---
-# T2 SDE: architecture/share/boot/grub.in
-# Copyright (C) 2021 The T2 SDE Project
-# 
-# This Copyright note is generated by scripts/Create-CopyPatch,
-# more information can be found in the files COPYING and README.
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2.
-# --- T2-COPYRIGHT-NOTE-END ---
+# --- T2-COPYRIGHT-BEGIN ---
+# t2/architecture/share/boot/grub.in
+# Copyright (C) 2021 - 2025 The T2 SDE Project
+# SPDX-License-Identifier: GPL-2.0
+# --- T2-COPYRIGHT-END ---
 
-function grub_create_efi_img() {
+grub_create_efi_img() {
 	local exe="$1"
 
 	dd if=/dev/zero bs=1024 count=1440 of=$isofsdir/efi.img
@@ -19,7 +14,7 @@ function grub_create_efi_img() {
 	mcopy -i $isofsdir/efi.img $exe ::/EFI/BOOT
 }
 
-function grub_create_boot_entry() {
+grub_create_boot_entry() {
 	cat <<- EOT
 BOOTx	-partition_offset 16 -append_partition 2 0xef $isofsdir/efi.img
 BOOTx	-e --interval:appended_partition_2:all::

package/base/dietlibc/parse-config-9
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-2.0
 # --- T2-COPYRIGHT-END ---
 
-function set_wrapper_and_misc () {
+set_wrapper_and_misc () {
 	export DIETHOME=$root/usr/dietlibc
 	if ! atstage native || [ "$SDECFG_LIBC" != "dietlibc" ]; then
 		var_append CC_WRAPPER_OTHERS ':' $dietbin
@@ -18,15 +18,15 @@ function set_wrapper_and_misc () {
 	var_append GCC_WRAPPER_INSERT ' ' -D__dietlibc__
 }
 
-function add_lib () {
+add_lib () {
 	var_append CC_WRAPPER_APPEND ' ' -c?:$1
 }
 
-function remove_lib () {
+remove_lib () {
 	var_append CC_WRAPPER_REMOVE ' ' $1
 }
 
-function pkg_dietlibc_rebuild () {
+pkg_dietlibc_rebuild () {
 	patchfiles=
 	. $base/package/*/dietlibc/parse-config-9
 	var_append patchfiles ' ' `ls $confdir/*.patch.dietlibc.2nd 2>/dev/null`

package/base/stone/stone_mod_install.sh
@@ -278,7 +278,7 @@ part_add() {
 	cmd="$cmd '`printf "%-6s %-24s %-10s" $dev "$location" "$size"` ${type//_/ }' 'part_${action}_action $1 $2'"
 }
 
-function join() {
+join() {
 	local IFS="$1"; shift
 	echo "$*"
 }

package/develop/gcc/parse-config
@@ -199,7 +199,7 @@ var_insert GCC_WRAPPER_REMOVE ' ' "-I/usr/include -I/usr/local/include"
 
 # pkg-config and other output or hardcoded configure assumption
 # do not include the "DESTDIR" we need to adapt the pathes accordingly
-function gcc_sysroot_filter() {
+gcc_sysroot_filter() {
 	# merge multiple path separators
 	var_append GCC_WRAPPER_FILTER '|' "sed -e 's,//*,/,g'"
 

package/filesystem/autofs/autofs.init
@@ -1,14 +1,9 @@
-# --- T2-COPYRIGHT-NOTE-BEGIN ---
-# T2 SDE: package/*/autofs/autofs.init
-# Copyright (C) 2004 - 2022 The T2 SDE Project
+# --- T2-COPYRIGHT-BEGIN ---
+# t2/package/*/autofs/autofs.init
+# Copyright (C) 2004 - 2025 The T2 SDE Project
 # Copyright (C) 1998 - 2003 ROCK Linux Project
-# 
-# This Copyright note is generated by scripts/Create-CopyPatch,
-# more information can be found in the files COPYING and README.
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2.
-# --- T2-COPYRIGHT-NOTE-END ---
+# SPDX-License-Identifier: GPL-2.0
+# --- T2-COPYRIGHT-END ---
 #
 # Desc: Kernel Automounter Daemon
 # Runlevel: 74 rcX
@@ -22,8 +17,7 @@ export PATH
 #
 localoptions=''
 
-function getmounts()
-{
+getmounts() {
   if [ -f /etc/auto.master ]
   then
     cat /etc/auto.master | sed -e '/^#/d' -e '/^$/d'| (
@@ -47,8 +41,7 @@ function getmounts()
   fi
 }
 
-function status()
-{
+status() {
 	echo "Configured Mount Points:"
 	echo "------------------------"
 	getmounts

package/network/iptables/rocknet_iptables.sh
@@ -1,20 +1,12 @@
-# --- T2-COPYRIGHT-NOTE-BEGIN ---
-# This copyright note is auto-generated by scripts/Create-CopyPatch.
-# 
-# T2 SDE: package/.../iptables/rocknet_iptables.sh
-# Copyright (C) 2004 - 2023 The T2 SDE Project
-# 
-# More information can be found in the files COPYING and README.
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License. A copy of the
-# GNU General Public License can be found in the file COPYING.
-# --- T2-COPYRIGHT-NOTE-END ---
+# --- T2-COPYRIGHT-BEGIN ---
+# t2/package/*/iptables/rocknet_iptables.sh
+# Copyright (C) 2004 - 2025 The T2 SDE Project
+# SPDX-License-Identifier: GPL-2.0
+# --- T2-COPYRIGHT-END ---
 
 ipv=
 
-function ipt_addcode() {
+ipt_addcode() {
 	code="$4"
 	ipv6code="${code/iptables/ip6tables}"
 	ipv6code="${ipv6code//icmp/icmpv6}"

scripts/Create-FlatPriPatch
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
-# --- T2-COPYRIGHT-NOTE-BEGIN ---
-# T2 SDE: scripts/Create-FlatPriPatch
-# Copyright (C) 2004 - 2023 The T2 SDE Project
-# 
-# This Copyright note is generated by scripts/Create-CopyPatch,
-# more information can be found in the files COPYING and README.
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2.
-# --- T2-COPYRIGHT-NOTE-END ---
+# --- T2-COPYRIGHT-BEGIN ---
+# t2/scripts/Create-FlatPriPatch
+# Copyright (C) 2004 - 2025 The T2 SDE Project
+# SPDX-License-Identifier: GPL-2.0
+# --- T2-COPYRIGHT-END ---
 
 tempfile=`mktemp`
 
-function create_pkg_patch() {
+create_pkg_patch() {
 	local pkg=$1
 	local priority=$2
 	sed -e 's,^\(\[P\] .* \)[0-9\.][0-9\.]*$,\1'$priority',' \

commit 998daefd5a95ea8cd91d85c46877957cdf286d4e

* fix mkinitrdinit syntax error regression from r81812 "remove more old-school shell function"
git-svn-id: https://svn.exactcode.de/t2/trunk@81904 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

package/base/mkinitrd/initrdinit.sh
@@ -4,7 +4,7 @@ PATH=/sbin:/bin:/usr/bin:/usr/sbin
 
 echo "T2 SDE early userspace (c)2005-2025 Rene Rebe, ExactCODE GmbH; Germany."
 
-mapper2lvm {
+mapper2lvm() {
 	# support both, direct vg/lv or mapper/...
 	x=${1#mapper/}
 	if [ "$x" != "$1" -a "${x#*-}" != "$x" ]; then
@@ -13,7 +13,7 @@ mapper2lvm {
 	echo $x
 }
 
-boot {
+boot() {
 	mount -t none -o move {,/mnt}/dev
 	mount -t none -o move {,/mnt}/proc
 	mount -t none -o move {,/mnt}/sys