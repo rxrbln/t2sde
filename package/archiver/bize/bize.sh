a7fa259a4eb95aa8a3db47af9b91c3960128029e
René Rebe <rene@t2-project.org>
2025-12-31 15:02:58 +0000

    * clean up bize a bit
    git-svn-id: https://svn.exactcode.de/t2/trunk@86030 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/archiver/bize/bize.sh
+++ package/archiver/bize/bize.sh
@@ -5,29 +5,27 @@
 # SPDX-License-Identifier: GPL-2.0
 # --- T2-COPYRIGHT-END ---
 
-bize_usage()
-{
-	echo "usage: bize -i [-t] [-v] [-f] [-R root] package1.tar.bz2 ..." 1>&2
-	echo "       bize -r [-t] [-v] [-f] [-R root] package1 ..." 1>&2
+bize_usage() {
+	echo "usage: bize -i [-t] [-v] [-f] [-R root] package1.tar.bz2 ..." >&2
+	echo "       bize -r [-t] [-v] [-f] [-R root] package1 ..." >&2
 }
 
-bize_remove()
-{
+bize_remove() {
 	local line base tag md5s="$adm/md5sums/$pkg"
 
 	if [ "$keep" ]; then
 		if [ ! -f "$md5s" ]; then
-			echo "$0: $md5s: no such file, skipping remove" 1>&2
+			echo "$0: $md5s: no such file, skipping remove" >&2
 			return
 		fi
 
 		(cd "$root/" && md5sum -c "var/adm/md5sums/$pkg" 2> /dev/null) |
-		while read line ; do
+		while read line; do
 			base="${line%: *}"
 			stat="${line##*: }"
 			file="$root/$base"
 			if [ -z "$base" -o -z "$stat" ]; then
-				echo "$0: invalid md5sum output '$line'" 1>&2
+				echo "$0: invalid md5sum output '$line'" >&2
 			elif [ -f "$file" -a ! -L "$file" ]; then
 				if [ "$stat" = OK ]; then
 					$unlink "$file"
@@ -40,12 +38,12 @@ bize_remove()
 		done
 	fi
 
-	sort -r "$list" | while read tag base ; do
+	sort -r "$list" | while read tag base; do
 		file="$root/$base"
 		if [ "$tag" != "$pkg:" ]; then
-			echo "$0: invalid tag '$tag' in $list" 1>&2
+			echo "$0: invalid tag '$tag' in $list" >&2
 		elif [ -z "$base" ]; then
-			echo "$0: missing file name in $list" 1>&2
+			echo "$0: missing file name in $list" >&2
 		elif [ -L "$file" ]; then
 			$unlink "$file"
 		elif [ -d "$file" ]; then
@@ -60,23 +58,22 @@ bize_remove()
 	done
 }
 
-bize_install()
-{
+bize_install() {
 	if [ ! -f "$arch" ]; then
-		echo "$0: $arch: no such file, skipping install" 1>&2
+		echo "$0: $arch: no such file, skipping install" >&2
 		return
 	fi
 
 	pkg="${arch%.tar.bz2}"
 	if [ "$arch" = "$pkg" ]; then
-		echo "$0: $arch: not a .tar.bz2 file" 1>&2
+		echo "$0: $arch: not a .tar.bz2 file" >&2
 		return
 	fi
 	pkg="${pkg%-[0-9]*}"
 	pkg="${pkg##*/}"
 
 	if [ -z "$pkg" ]; then
-		echo "$0: $arch: missing package name" 1>&2
+		echo "$0: $arch: missing package name" >&2
 		return
 	fi
 	[ "${arch#-}" = "$arch" ] || arch="./$arch"
@@ -97,23 +94,21 @@ bize_install()
 	fi
 }
 
-bize_uninstall()
-{
+bize_uninstall() {
 	[ "$verbose" ] && echo "removing $pkg"
 	list="$adm/flists/$pkg"
 	if [ -f "$list" ]; then
 		bize_remove
 	else
-		echo "$0: $list: no such file, skipping remove" 1>&2
+		echo "$0: $list: no such file, skipping remove" >&2
 	fi
 }
 
-bize_bundle()
-{
+bize_bundle() {
 	[ -z "${root}" ] && root='/'
 
 	[ ! -f "${root}var/adm/flists/$pkg" ] && \
-		echo "$0: $pkg: no such package" 1>&2
+		echo "$0: $pkg: no such package" >&2
 
 	local ver=$(head -1 ${root}/var/adm/packages/$pkg | cut -d' ' -f6)
 	local compressor="zstd -T0 -19"
@@ -128,12 +123,11 @@ bize_bundle()
 	) > ./$pkg-$ver.tar.$ext
 }
 
-bize_main()
-{
+bize_main() {
 	local which=which file arch list="sort rm rmdir mkdir tar bzip2"
 	local install remove bundle test verbose voption keep=k root=/ taropt
 
-	while [ "$1" ] ; do
+	while [ "$1" ]; do
 		case "$1" in
 			-i) install=1 ;;
 			-r) remove=1 ;;
@@ -153,14 +147,14 @@ bize_main()
 	if type sh > /dev/null 2>&1; then
 		which=type
 	elif ! which sh > /dev/null; then
-		echo "$0: unable to find 'type' or 'which'" 1>&2
+		echo "$0: unable to find 'type' or 'which'" >&2
 		return 1
 	fi
 
 	[ "$keep" ] && list="$list md5sum"
-	for file in $list ; do
+	for file in $list; do
 		if ! $which $file > /dev/null; then
-			echo "$0: unable to find '$file'" 1>&2
+			echo "$0: unable to find '$file'" >&2
 			return 1
 		fi
 	done
