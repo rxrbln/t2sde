rxrbln/t2sde

commit ec53356e22236fa263ac6aa84d63a4e7fe3b5dbb

* compact all remaining historic copyright notes
git-svn-id: https://svn.exactcode.de/t2/trunk@89447 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

package/base/sysfiles/btee.c
@@ -1,17 +1,9 @@
 /*
- * --- T2-COPYRIGHT-NOTE-BEGIN ---
- * This copyright note is auto-generated by ./scripts/Create-CopyPatch.
- * 
- * T2 SDE: package/.../sysfiles/btee.c
- * Copyright (C) 2004 - 2005 The T2 SDE Project
- * 
- * More information can be found in the files COPYING and README.
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License. A copy of the
- * GNU General Public License can be found in the file COPYING.
- * --- T2-COPYRIGHT-NOTE-END ---
+ * --- T2-COPYRIGHT-BEGIN ---
+ * t2/package/*/sysfiles/btee.c
+ * Copyright (C) 2004 - 2026 The T2 SDE Project
+ * SPDX-License-Identifier: GPL-2.0
+ * --- T2-COPYRIGHT-END ---
  */
 /*  btee.c, a buffered tee clone   -   written for ROCK Linux
 

package/network/net-tools/config.h
@@ -1,19 +1,11 @@
 /*
 *
-* --- T2-COPYRIGHT-NOTE-BEGIN ---
-* This copyright note is auto-generated by ./scripts/Create-CopyPatch.
-* 
-* T2 SDE: package/.../net-tools/config.h
-* Copyright (C) 2004 - 2005 The T2 SDE Project
+* --- T2-COPYRIGHT-BEGIN ---
+* t2/package/*/net-tools/config.h
+* Copyright (C) 2004 - 2026 The T2 SDE Project
 * Copyright (C) 1998 - 2003 ROCK Linux Project
-* 
-* More information can be found in the files COPYING and README.
-* 
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; version 2 of the License. A copy of the
-* GNU General Public License can be found in the file COPYING.
-* --- T2-COPYRIGHT-NOTE-END ---
+* SPDX-License-Identifier: GPL-2.0
+* --- T2-COPYRIGHT-END ---
 *
 * config.h	Automatically generated configuration includefile
 *

commit 0ede48ee1ba4cedceec6a097fdac3d9f2cce50f8

* fix sysfiles/btee.c and net-tools/config.h comment regressions 	  from r89447 "compact all remaining historic copyright notes"
git-svn-id: https://svn.exactcode.de/t2/trunk@89550 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

package/base/sysfiles/btee.c
@@ -1,6 +1,6 @@
 /*
  * --- T2-COPYRIGHT-BEGIN ---
- * t2/package/*/sysfiles/btee.c
+ * t2/package/.../sysfiles/btee.c
  * Copyright (C) 2004 - 2026 The T2 SDE Project
  * SPDX-License-Identifier: GPL-2.0
  * --- T2-COPYRIGHT-END ---

package/network/net-tools/config.h
@@ -1,7 +1,7 @@
 /*
 *
 * --- T2-COPYRIGHT-BEGIN ---
-* t2/package/*/net-tools/config.h
+* t2/package/...//net-tools/config.h
 * Copyright (C) 2004 - 2026 The T2 SDE Project
 * Copyright (C) 1998 - 2003 ROCK Linux Project
 * SPDX-License-Identifier: GPL-2.0