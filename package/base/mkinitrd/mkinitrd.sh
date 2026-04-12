9f861b8e1255fc568b0274ab46a854c6a899ea66
René Rebe <rene@t2-project.org>
2026-04-12 12:12:32 +0000

    * include zfs if available in mkinitrd, too
    git-svn-id: https://svn.exactcode.de/t2/trunk@90052 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/mkinitrd/mkinitrd.sh
+++ package/base/mkinitrd/mkinitrd.sh
@@ -62,7 +62,7 @@ while [ "$1" ]; do
 done
 
 # -e ps3vram -e net/phy
-[ -z "$minimal" ] && filter="$filter -e reiserfs -e btrfs -e /jfs -e /xfs -e jffs2
+[ -z "$minimal" ] && filter="$filter -e reiserfs -e btrfs -e /jfs -e /zfs -e jffs2
 -e /udf -e overlayfs -e ntfs -e /fat -e /exfat -e /hfs -e floppy -e efivarfs -e watchdog
 -e pci/controller -e /ata/ -e /scsi/ -e /fusion/ -e nvme/host -e mmc/host -e mmc_block
 -e virtio.\(blk\|scsi\|net\|console\|input\|gpu\|pci\) -e ps3disk -e drivers/pcmcia
