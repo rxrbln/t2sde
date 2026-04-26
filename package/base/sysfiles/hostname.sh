af9c43fa041f29695f707b28719c39de52cbf585
René Rebe <rene@t2-project.org>
2026-04-26 16:12:59 +0000

    * move sysvinit/system.init auto-hostname to sysfiles/hostname and re-use in new systemd/hostname.service
    git-svn-id: https://svn.exactcode.de/t2/trunk@90630 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ package/base/sysfiles/hostname.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# --- T2-COPYRIGHT-BEGIN ---
+# t2/package/*/sysfiles/hostname.sh
+# Copyright (C) 2026 The T2 SDE Project
+# SPDX-License-Identifier: GPL-2.0
+# --- T2-COPYRIGHT-END ---
+
+hostname="hostname= $(cat /proc/cmdline 2>/dev/null)"
+hostname=${hostname##*hostname=} hostname=${hostname%% *}
+[ "$hostname" ] || hostname=$(cat /etc/hostname 2>/dev/null)
+
+if [ ! "$hostname" ]; then
+    # try DMI first
+    [ -e /sys/devices/virtual/dmi/id ] &&
+    for f in product_family product_name board_name; do
+	hostname=$(cat /sys/devices/virtual/dmi/id/$f 2>/dev/null)
+	case "$hostname" in
+	*"To Be Filled"*|*Default*|"N/A"|NULL)
+		hostname= ;;
+	esac
+	[ "${hostname// /}" ] && break
+    done
+    # OF second
+    if [ ! "$hostname" -a -e /proc/device-tree ]; then
+	hostname=$(sed "s/\x0//" /proc/device-tree/name 2>/dev/null)
+	[ "$hostname" ] || hostname=$(sed "s/\x0//" /proc/device-tree/model 2>/dev/null)
+	[ "$hostname" ] || hostname=$(sed "s/\x0//" /proc/device-tree/banner-name 2>/dev/null)
+    fi
+    # try various /proc as fallback
+    [ "$hostname" ] || hostname=$(sed -n "/\(system type\)[^:]*:[[:space:]]/{s///p;q}" /proc/cpuinfo)
+    [ "$hostname" ] || hostname=$(sed -n "/\(model\)[^:]*:[[:space:]]/{s///p;q}" /proc/cpuinfo)
+    hostname="${hostname%%(*}"
+    hostname="${hostname%${hostname##*[![:space:]]}}"
+    hostname="${hostname//[[:space:][:punct:]]/_}"
+    hostname="${hostname//__/_}"
+fi
+
+[ "$hostname" ] && hostname "$hostname" 
